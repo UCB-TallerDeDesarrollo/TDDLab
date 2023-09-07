c673c4b15a98a2312bdd880b39500a260c3a6588
Christian Rivero <christianriveroarnez@gmail.com>
2023-09-07 11:01:58 -0400

    organized the repo into WebUi folder

---
+++ Web Ui/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import CardsView from "./application/views/view-1/components/cardsView";
+function App() {
+
+  return (
+    <div>
+      <div></div>
+      <h1>TDDLAB</h1>
+      <div>cards Example</div>
+
+      <CardsView></CardsView>
+    </div>
+  );
+}
+
+export default App;
