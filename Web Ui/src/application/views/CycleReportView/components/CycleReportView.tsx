666794db4f6e667da3628a0e18aa8db42b080bd4
Christian Rivero <christianriveroarnez@gmail.com>
2023-09-12 21:36:08 -0400

    created interfaces for github API, also linked the tddCyclesPort and githubadapter

--- Web Ui/src/application/views/CycleReportView/components/CycleReportView.tsx
+++ Web Ui/src/application/views/CycleReportView/components/CycleReportView.tsx
@@ -1,17 +1,28 @@
 // src/App.tsx
-import {useEffect} from "react"
+import {useEffect,useState} from "react"
 import { TDDCyclesPort } from "../useCases/tddCycles.port";
+import { CommitDataObject } from "../../../../domain/models/githubInterfaces";
 function CycleReportView() {
   const port=new TDDCyclesPort()
+  const [commits,setCommits]=useState<CommitDataObject[]|null>(null)
   useEffect(() => {
-    console.log("testing github API");
-    port.obtainCommitsOfRepo("DwijanX","astronomyPage")
+    const APICalls = async () => {
+      try {
+        const data = await port.obtainCommitsOfRepo('DaddyLipi', 'Cactus-Topia');
+        setCommits(data);
+      } catch (error) {
+        // Handle API call errors here
+        console.error('Error obtaining commits:', error);
+      }
+    };
 
-  }, [])
+    APICalls();
+  }, []);
   
   return (
     <>
       Grafica TDD
+      {commits && JSON.stringify(commits[0])}
     </>
   );
 }
