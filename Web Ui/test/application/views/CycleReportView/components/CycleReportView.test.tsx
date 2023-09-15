07b607ac4a4e4dd980979f16c098ce8c0b4600c8
Christian Rivero <christianriveroarnez@gmail.com>
2023-09-15 18:10:05 -0400

    added tests

--- Web Ui/test/application/views/CycleReportView/components/CycleReportView.test.tsx
+++ Web Ui/test/application/views/CycleReportView/components/CycleReportView.test.tsx
@@ -1,20 +1,28 @@
-import {  render } from '@testing-library/react';
+import {  render,waitFor } from '@testing-library/react';
 import CycleReportView from '../../../../../src/application/views/CycleReportView/components/CycleReportView';
 import { mockCommitData } from '../../../../__ mocks __/dataTypeMocks/commitData';
 
 
 class MockTDDCyclesPort {
-    obtainCommitsOfRepo = jest.fn(async () => {
+  obtainCommitsOfRepo = jest.fn(async () => {
       return [mockCommitData];
+    });
+    obtainJobsData=jest.fn(async () => {
+      return {};
     });
   }
 describe('CycleReport View tests', () => {
 
-  
   test('checks if Port is being used', async () => {
     const mockPort = new MockTDDCyclesPort();
+
     render(<CycleReportView port={mockPort} />);
-    expect(mockPort.obtainCommitsOfRepo).toHaveBeenCalled();
+
+    await waitFor(() => {
+      expect(mockPort.obtainJobsData).toHaveBeenCalled();
+      expect(mockPort.obtainCommitsOfRepo).toHaveBeenCalled();
+    });
+    
   });
   
   
