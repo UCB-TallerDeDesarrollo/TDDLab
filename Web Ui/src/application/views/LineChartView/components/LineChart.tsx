76bda18988d302ac0586eb154a7a985ef85797a4
Rene <ddcrene@gmail.com>
2023-09-25 13:09:14 -0400

    Example of line chart

---
+++ Web Ui/src/application/views/LineChartView/components/LineChart.tsx
@@ -0,0 +1,63 @@
+import { Line } from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler,
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler
+);
+
+
+var beneficios = [0, 56, 20, 36, 80, 40, 30, -20, 25, 30, 12, 60];
+var meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+var midata = {
+    labels: meses,
+    datasets: [ // Cada una de las líneas del gráfico
+        {
+            label: 'Beneficios',
+            data: beneficios,
+            tension: 0.5,
+            fill : true,
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            pointRadius: 5,
+            pointBorderColor: 'rgba(255, 99, 132)',
+            pointBackgroundColor: 'rgba(255, 99, 132)',
+        },
+        {
+            label: 'Otra línea',
+            data: [20, 25, 60, 65, 45, 10, 0, 25, 35, 7, 20, 25]
+        },
+    ],
+};
+
+var misoptions = {
+    scales : {
+        y : {
+            min : 0
+        },
+        x: {
+            ticks: { color: 'rgb(255, 99, 132)'}
+        }
+    }
+};
+
+export default function LinesChart() {
+    return <Line data={midata} options={misoptions}/>
+}
