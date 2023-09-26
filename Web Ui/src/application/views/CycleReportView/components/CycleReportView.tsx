0f655fcd17fca2fe9ec216c1bd568395e01a2bc6
BMB <bruno.marquez@ucb.edu.bo>
2023-09-25 22:09:46 -0400

    Errors

--- Web Ui/src/application/views/CycleReportView/components/CycleReportView.tsx
+++ Web Ui/src/application/views/CycleReportView/components/CycleReportView.tsx
@@ -1,50 +1,62 @@
-import {useEffect,useState} from "react"
+import { useEffect, useState } from "react";
 import { TDDCyclesPort } from "../useCases/tddCycles.port";
-import { CommitDataObject, CommitInformationDataObject } from "../../../../domain/models/githubCommitInterfaces";
 import CycleCard from "./CycleCard";
 import { JobDataObject } from "../../../../domain/models/jobInterfaces";
+import { CommitDataObject, CommitInformationDataObject } from "../../../../domain/models/githubCommitInterfaces";
+
 interface CycleReportViewProps {
-  port: TDDCyclesPort|any;
+  port: TDDCyclesPort | any;
 }
+
 function CycleReportView({ port }: CycleReportViewProps) {
-  const [commits, setCommits] = useState<CommitDataObject[] >();
-  const [commitsInfo, setCommitsInfo] = useState<CommitInformationDataObject[] >();
-  const [jobsByCommit, setJobsByCommit] = useState<Record<string, JobDataObject> >();
+  const repoOwner = "bmb0";
+  const repoName = "pacman-calidad-software";
 
-  const repoOwner = "DwijanX";
-  const repoName = "Bulls-and-Cows";
+  const [commits, setCommits] = useState<CommitDataObject[] | null>(null);
+  const [commitsInfo, setCommitsInfo] = useState<CommitInformationDataObject[] | null>(null);
+  const [jobsByCommit, setJobsByCommit] = useState<Record<string, JobDataObject> | null>(null);
 
-  const obtainJobsData=async()=>{
-    const jobsData:Record<string, JobDataObject> = await port.obtainJobsData(repoOwner, repoName);
-    setJobsByCommit(jobsData);
+  const obtainJobsData = async () => {
+    try {
+      const jobsData: Record<string, JobDataObject> = await port.obtainJobsData(repoOwner, repoName);
+      setJobsByCommit(jobsData);
+    } catch (error) {
+      console.error('Error obtaining jobs:', error);
+    }
+  };
 
-  }
-  const obtainCommitsData=async()=>{
-    const commitsData = await port.obtainCommitsOfRepo(repoOwner, repoName);
-    setCommits(commitsData);
-  }
-  const obtainCommitsInfoData=async(commits: any)=>{
-    const commitsInfoData = commits.map((commit: any) => await port.obtainCommitsFromSha(repoOwner, repoName, commit.sha)) // Verificar type any
-    setCommitsInfo(commitsInfoData);
-  }
+  const obtainCommitsData = async () => {
+    try {
+      const commitsData = await port.obtainCommitsOfRepo(repoOwner, repoName);
+      setCommits(commitsData);
+    } catch (error) {
+      console.error('Error obtaining commits:', error);
+    }
+  };
+
+  const obtainCommitsInfoData = async (commits: CommitDataObject[] | null) => {
+    if (commits) {
+      const commitsInfoData = await Promise.all(
+        commits.map((commit) => port.obtainCommitInformation(repoOwner, repoName, commit.sha))
+      );
+      setCommitsInfo(commitsInfoData);
+    }
+  };
 
   useEffect(() => {
-    const fetchData=async ()=>{
-      try {
-        await obtainJobsData()
-        await obtainCommitsData()
-        await obtainCommitsInfoData(commits)
-      } catch (error) {
-        console.error('Error obtaining commits:', error);
-      }
-    }
-    fetchData()
+    const fetchData = async () => {
+      await obtainJobsData();
+      await obtainCommitsData();
+      // await obtainCommitsInfoData(commits);
+    };
+    fetchData();
   }, []);
+
   return (
     <>
-      <h1>Repository:{repoName} </h1>
-      {jobsByCommit != null && commits!=null && commits.map((commit) => (
-        <CycleCard key={commit.sha} commit={commit} jobs={jobsByCommit[commit.sha]}></CycleCard>
+      <h1>Repository: {repoName}</h1>
+      {jobsByCommit != null && commits != null && commits.map((commit) => (
+        <CycleCard key={commit.sha} commit={commit} jobs={jobsByCommit[commit.sha]} />
       ))}
     </>
   );
