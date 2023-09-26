6aa91a5d865bb3db4520262cd6cca5a3eda52837
Gabriel Ichiro <gabriel582084@gmail.com>
2023-09-26 15:39:52 -0400

    added routing for get assignment by id

--- Web Ui/src/application/utils/dateUtils.ts
+++ Web Ui/src/application/utils/dateUtils.ts
@@ -1,9 +1,9 @@
-// dateUtils.ts
+// Function to format a Date object as MM/DD/YYYY
+export const formatDate = (date: Date | undefined | null) => {
+  if (!date) {
+    return ''; // Return an empty string if the date is undefined or null
+  }
 
-  // Function to format a Date object as MM/DD/YYYY
-  export const formatDate = (date: Date) => {
-    const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    return date.toLocaleDateString(undefined, options);
-  };
-  
-
+  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  return date.toLocaleDateString(undefined, options);
+};
