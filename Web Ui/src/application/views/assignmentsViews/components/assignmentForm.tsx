d782b12f6b831a32616ef33cc6baef23010df324
Anaelise Huaniri Romero <aanesita@gmail.com>
2023-09-25 23:52:42 -0400

    Cree el formulario de crear tarea, tambien el boton de crear tarea, mas la tabla de tareas

---
+++ Web Ui/src/application/views/assignmentsViews/components/assignmentForm.tsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import SaveIcon from '@mui/icons-material/Save';
+import Stack from '@mui/material/Stack';
+import { Box, Container, TextField, Typography } from "@mui/material";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import Filter from "./datePicker";
+
+function Formulario2() {
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+
+  const handleGuardarClick = () => {
+    setSuccessMessage('Cambios guardados con éxito');
+  };
+
+  return (
+    <>
+      <Container maxWidth="sm">
+        <Box
+          sx={{ display: "grid", gap: 2 }}
+          component="form"
+          autoComplete="off"
+        >
+          <TextField
+            id="city"
+            label="Titulo"
+            variant="outlined"
+            size="small"
+            required
+          />
+          <TextField
+            id="city"
+            label="Descripcion"
+            variant="outlined"
+            size="small"
+            required
+            sx={{
+              '& label.Mui-focused': {
+                color: '#001F3F',
+              },
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  borderColor: '#001F3F',
+                },
+              },
+            }}
+          />
+          <section>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <Filter />
+                </LocalizationProvider>
+            </section>
+          <Stack direction="row" spacing={2}>
+            <Button
+              variant="contained"
+              endIcon={<SaveIcon />}
+              style={{
+                backgroundColor: '#001F3F',
+                textTransform: 'uppercase',
+              }}
+              onClick={handleGuardarClick}  // Agregamos un manejador para el clic
+            >
+              Guardar cambios
+            </Button>
+          </Stack>
+
+          {successMessage && (
+            <Typography sx={{ marginTop: 2, color: 'green' }}>
+              {successMessage}
+            </Typography>
+          )}
+        </Box>
+            
+      </Container>
+    </>
+  );
+}
+
+export default Formulario2;
---
+++ Web Ui/src/application/views/assignmentsViews/components/assignmentManager.tsx
@@ -0,0 +1,35 @@
+import Formulario2 from "./assignmentForm";
+import Tareas from "./assignmentTable";
+import { styled } from '@mui/system'; 
+
+const GestionTareasContainer = styled('section')({
+    display: 'flex',
+    width: '100%',
+});
+
+  const TareasContainer = styled('div')({
+    flex: '1', 
+   
+  });
+  
+  const FormularioContainer = styled('div')({
+    flex: '1',
+  });
+
+
+  function GestionTareas() {
+    return (
+      <GestionTareasContainer>
+        <TareasContainer>
+          <Tareas />
+        </TareasContainer>
+        <FormularioContainer>
+          <Formulario2 />
+        </FormularioContainer>
+      </GestionTareasContainer>
+    
+    );
+  }
+  
+  export default GestionTareas;
+  
