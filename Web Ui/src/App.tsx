bfca60ef252abea4c0b4e33e130b0ca1c7c7ff7d
Gabriel Ichiro <gabriel582084@gmail.com>
2023-09-26 11:55:08 -0400

    added route for graph

--- Web Ui/src/App.tsx
+++ Web Ui/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GestionTareas from "./application/views/assignmentsViews/components/assignmentManager";
 import AssignmentDetail from "./application/views/assignmentDetailView/components/assignmentDetail";
+import CycleReportView from "./application/views/CycleReportView/components/CycleReportView";
+import { TDDCyclesPort } from "./application/views/CycleReportView/useCases/tddCycles.port";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/TDDLab/" element={<GestionTareas />} />
-        <Route path="/TDDLab/:id" element={<AssignmentDetail id={1} />} />
-
+        <Route path="/" element={<GestionTareas />} />
+        <Route path="/assignment/:id" element={<AssignmentDetail id={1} />} />
+        <Route path="/graph" element={<CycleReportView port={new TDDCyclesPort()} />} />
 
 
       </Routes>
