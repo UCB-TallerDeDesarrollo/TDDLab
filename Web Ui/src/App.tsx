5d3b938a1ceb5a8cb6ffc35505a7292e74ab8f62
Gabriel Ichiro <gabriel582084@gmail.com>
2023-09-26 10:57:25 -0400

    added routes

--- Web Ui/src/App.tsx
+++ Web Ui/src/App.tsx
@@ -1,15 +1,18 @@
-import CycleReportView from "./application/views/CycleReportView/components/CycleReportView";
-import { TDDCyclesPort } from "./application/views/CycleReportView/useCases/tddCycles.port";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GestionTareas from "./application/views/assignmentsViews/components/assignmentManager";
+import AssignmentDetail from "./application/views/assignmentDetailView/components/assignmentDetail";
 
 function App() {
   return (
-    <div>
-      <div></div>
-      <GestionTareas />
-      
-     {/* <CycleReportView port={new TDDCyclesPort()}></CycleReportView>*/}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/TDDLab/" element={<GestionTareas />} />
+        <Route path="/TDDLab/:id" element={<AssignmentDetail id={1} />} />
+
+
+
+      </Routes>
+    </Router>
   );
 }
 
