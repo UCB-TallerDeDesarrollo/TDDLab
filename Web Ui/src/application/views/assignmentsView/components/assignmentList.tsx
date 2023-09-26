ab93eeb77159c48b637cc53f9d8a3f8bc205b700
Gabriel Ichiro <gabriel582084@gmail.com>
2023-09-26 00:18:28 -0400

    added files for assignment view

--- Web Ui/src/application/views/assignmentsView/components/assignmentList.tsx
+++ Web Ui/src/application/views/assignmentsView/components/assignmentList.tsx
@@ -1,7 +1,7 @@
 // application/components/AssignmentList.tsx
 import React from 'react';
-import { AssignmentDataObject } from '../../domain/models/assignmentInterfaces'; // Import your assignment model
-
+import { AssignmentDataObject } from '../../../../domain/models/assignmentInterfaces'; // Import your assignment model
+import { formatDate } from '../../../utils/dateUtils';
 interface AssignmentListProps {
   assignments: AssignmentDataObject[];
 }
@@ -13,9 +13,9 @@ const AssignmentList: React.FC<AssignmentListProps> = ({ assignments }) => {
         <div key={assignment.id} className="assignment-card">
           <h2>{assignment.title}</h2>
           <p>Description: {assignment.description}</p>
-          <p>Start Date: {assignment.start_date}</p>
-          <p>End Date: {assignment.end_date}</p>
           <p>State: {assignment.state}</p>
+         {/* <p>Start Date: {formatDate(assignment.start_date)}</p>  Format start_date */}
+         {/* <p>End Date: {formatDate(assignment.end_date)}</p>  Format end_date */}
         </div>
       ))}
     </div>
---
+++ Web Ui/src/application/views/assignmentsView/components/assignmentsView.tsx
@@ -0,0 +1,11 @@
+import AssignmentList from "./assignmentList";
+
+function AssignmentsView() {
+  return (
+    <div className="App">
+      <AssignmentList assignments={[]}/>
+    </div>
+  );
+}
+
+export default AssignmentsView;
