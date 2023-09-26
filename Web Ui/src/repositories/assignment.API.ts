8a337fd9b6ef2c65fbb0d1356f9d949675f60db2
Gabriel Ichiro <gabriel582084@gmail.com>
2023-09-26 15:10:53 -0400

    added adapter for get assignment

--- Web Ui/src/repositories/assignment.API.ts
+++ Web Ui/src/repositories/assignment.API.ts
@@ -1,25 +1,32 @@
 import axios from 'axios'; // Import Axios or your preferred HTTP library
 import { AssignmentDataObject } from '../domain/models/assignmentInterfaces'; // Import your assignment model
 
-const API_URL = '/api/assignment'; // Adjust this URL according to your backend's URL structure
+const API_URL = 'http://localhost:3000/api/assignments';
+
+// Define a function to fetch assignments
+export const fetchAssignments = async () => {
+  try {
+    // Send a GET request to fetch assignments from the backend
+    const response = await axios.get(API_URL);
+
+    // Check if the response status is successful (e.g., 200 OK)
+    if (response.status === 200) {
+      // Return the assignments data from the response
+      return response.data;
+    } else {
+      // Handle other response status codes or errors here if needed
+      throw new Error('Failed to fetch assignments');
+    }
+  } catch (error) {
+    // Handle any network errors or exceptions that may occur
+    console.error('Error fetching assignments:', error);
+    throw error;
+  }
+};
 
 export const createAssignment = async (assignmentData: AssignmentDataObject): Promise<void> => {
   // Send a POST request to create a new assignment
   await axios.post(API_URL, assignmentData);
-};
-
-export const fetchAssignments = async (): Promise<AssignmentDataObject[]> => {
-  // Send a GET request to fetch assignments
-  const response = await axios.get(API_URL);
-
-  // Check if the response status is successful (e.g., 200 OK)
-  if (response.status === 200) {
-    // Return the assignments data from the response
-    return response.data;
-  } else {
-    // Handle other response status codes or errors here if needed
-    throw new Error('Failed to fetch assignments');
-  }
 };
 
 export const updateAssignment = async (assignmentData: AssignmentDataObject): Promise<void> => {
