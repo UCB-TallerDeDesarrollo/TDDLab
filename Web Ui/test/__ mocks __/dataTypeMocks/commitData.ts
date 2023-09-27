3cb36133d2e137b1e2c140755eea6e49b46b8ba0
BMB <bruno.marquez@ucb.edu.bo>
2023-09-26 23:13:53 -0400

    Updated port test and refactors

--- Web Ui/test/__ mocks __/dataTypeMocks/commitData.ts
+++ Web Ui/test/__ mocks __/dataTypeMocks/commitData.ts
@@ -1,80 +1,47 @@
-import {  CommitDataObject, CommitInformationDataObject } from "../../../src/domain/models/githubCommitInterfaces";
-
+import { CommitDataObject, CommitInformationDataObject } from "../../../src/domain/models/githubCommitInterfaces";
 
 export const mockCommitData: CommitDataObject = {
-    sha: 'mockSha',
-    node_id: 'mockNodeId',
-    commit: {
-      author: {
-        name: 'Mock Author',
-        email: 'author@example.com',
-        date: new Date('2023-09-13T12:00:00Z'),
-      },
-      committer: {
-        name: 'Mock Committer',
-        email: 'committer@example.com',
-        date: new Date('2023-09-13T12:00:00Z'),
-      },
-      message: 'Mock commit message',
-      tree: {
-        sha: 'mockTreeSha',
-        url: 'mockTreeUrl',
-      },
-      url: 'mockCommitUrl',
-      comment_count: 0,
-      verification: {
-        verified: false,
-        reason: 'unsigned',
-        signature: null,
-        payload: null,
-      },
+  sha: 'mockSha',
+  node_id: 'mockNodeId',
+  commit: {
+    author: {
+      name: 'Mock Author',
+      email: 'author@example.com',
+      date: new Date('2023-09-13T12:00:00Z'),
     },
-    url: 'mockUrl',
-    html_url: 'mockHtmlUrl',
-    comments_url: 'mockCommentsUrl',
-    author: null, 
-    committer: null,
-    parents: [],
-  };
+    committer: {
+      name: 'Mock Committer',
+      email: 'committer@example.com',
+      date: new Date('2023-09-13T12:00:00Z'),
+    },
+    message: 'Mock commit message',
+    tree: {
+      sha: 'mockTreeSha',
+      url: 'mockTreeUrl',
+    },
+    url: 'mockCommitUrl',
+    comment_count: 0,
+    verification: {
+      verified: false,
+      reason: 'unsigned',
+      signature: null,
+      payload: null,
+    },
+  },
+  url: 'mockUrl',
+  html_url: 'mockHtmlUrl',
+  comments_url: 'mockCommentsUrl',
+  author: null,
+  committer: null,
+  parents: [],
+};
 
-  export const mockCommitInfoData: CommitInformationDataObject = {
-    sha: 'mockSha',
-    node_id: 'mockNodeId',
-    commit: {
-      author: {
-        name: 'Mock Author',
-        email: 'author@example.com',
-        date: new Date('2023-09-13T12:00:00Z'),
-      },
-      committer: {
-        name: 'Mock Committer',
-        email: 'committer@example.com',
-        date: new Date('2023-09-13T12:00:00Z'),
-      },
-      message: 'Mock commit message',
-      tree: {
-        sha: 'mockTreeSha',
-        url: 'mockTreeUrl',
-      },
-      url: 'mockCommitUrl',
-      comment_count: 0,
-      verification: {
-        verified: false,
-        reason: 'unsigned',
-        signature: null,
-        payload: null,
-      },
-    },
-    url: 'mockUrl',
-    html_url: 'mockHtmlUrl',
-    comments_url: 'mockCommentsUrl',
-    author: null, 
-    committer: null,
-    parents: [],
-    stats: {
-      total: 5,
-      additions: 4,
-      deletions: 1
-    },
-    files: [],
-  };
+export const mockCommitInfoData: CommitInformationDataObject = {
+  ...mockCommitData,
+  stats: {
+    total: 5,
+    additions: 4,
+    deletions: 1,
+  },
+  files: [],
+};
