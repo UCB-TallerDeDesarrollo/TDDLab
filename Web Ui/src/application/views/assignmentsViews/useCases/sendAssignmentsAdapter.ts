2d1a7618f72a8ebfc51f1f6875062ee99cd0b149
E-Doggo <rodrigo.guardia316@gmail.com>
2023-09-27 15:00:57 -0400

    send assignment progress

---
+++ Web Ui/src/application/views/assignmentsViews/useCases/sendAssignmentsAdapter.ts
@@ -0,0 +1,19 @@
+import { AssignmentDataObject } from "../../../../domain/models/assignmentInterfaces";
+import { fetchAssignmentById, updateAssignment } from "../../../../repositories/assignment.API";
+
+export const sendAssignemtUseCase = async (assignmentId: number) => {
+    try{
+    const foundAssignment:AssignmentDataObject | null = await fetchAssignmentById(assignmentId);
+
+    if (foundAssignment !== null){
+        foundAssignment.state = 'delivered'
+        return await updateAssignment(foundAssignment);
+    }
+    else{
+        throw new Error("No se encontro la tarea")
+    }
+    } catch(error){
+        console.error(error);
+        throw error;
+    }
+  }
