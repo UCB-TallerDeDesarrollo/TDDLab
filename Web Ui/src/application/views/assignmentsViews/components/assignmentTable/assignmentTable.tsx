cf52a2369f9068e9f583aef3bb00e9cbcb3d7e60
Gabriel Ichiro <gabriel582084@gmail.com>
2023-09-27 09:03:39 -0400

    mobed deleyte adapter

--- Web Ui/src/application/views/assignmentsViews/components/assignmentTable/assignmentTable.tsx
+++ Web Ui/src/application/views/assignmentsViews/components/assignmentTable/assignmentTable.tsx
@@ -1,38 +1,46 @@
-import  { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import IconButton from '@mui/material/IconButton';
-import EditIcon from '@mui/icons-material/Edit';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import SendIcon from '@mui/icons-material/Send';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { Table, TableHead, TableBody, TableRow, TableCell, Container, Button } from '@mui/material';
-import { styled } from '@mui/system';
-import { AssignmentDataObject } from '../../../../../domain/models/assignmentInterfaces'; // Import your assignment model
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import SendIcon from "@mui/icons-material/Send";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Container,
+  Button,
+} from "@mui/material";
+import { styled } from "@mui/system";
+import { AssignmentDataObject } from "../../../../../domain/models/assignmentInterfaces"; // Import your assignment model
 
-import { fetchAssignmentsUseCase } from '../../useCases/fetchAssignmentsApater'; // Import your fetchAssignments function\
-import { deleteAssignmentUseCase } from '../../../assignmentDeleteView/deleteAssignmentAdapter';
+import { fetchAssignmentsUseCase } from "../../useCases/fetchAssignmentsApater"; // Import your fetchAssignments function\
+import { deleteAssignmentUseCase } from "../../useCases/deleteAssignmentAdapter";
 
-const ButtonContainer = styled('div')({
-  display: 'flex',
-  justifyContent: 'flex-end',
-  gap: '8px',
+const ButtonContainer = styled("div")({
+  display: "flex",
+  justifyContent: "flex-end",
+  gap: "8px",
 });
 
 const CustomTableCell1 = styled(TableCell)({
-  width: '90%',
+  width: "90%",
 });
 
 const CustomTableCell2 = styled(TableCell)({
-  width: '10%',
+  width: "10%",
 });
 interface TareasProps {
-  mostrarFormulario: () => void,
+  mostrarFormulario: () => void;
 }
 function Tareas({ mostrarFormulario }: TareasProps) {
   const navigate = useNavigate();
 
-  const [selectedRow, setSelectedRow] = useState<number | null>(null);
-  const [hoveredRow, setHoveredRow] = useState<number | null>(null);
+  const [, setSelectedRow] = useState<number | null>(null);
+  const [, setHoveredRow] = useState<number | null>(null);
   const [assignments, setAssignments] = useState<AssignmentDataObject[]>([]); // Specify the type as Assignment[]
 
   useEffect(() => {
@@ -42,16 +50,12 @@ function Tareas({ mostrarFormulario }: TareasProps) {
         setAssignments(data); // Set the fetched assignments in state
       })
       .catch((error) => {
-        console.error('Error fetching assignments:', error);
+        console.error("Error fetching assignments:", error);
       });
-
-    
   }, []); // Empty dependency array means this effect runs once when the component mounts
-
 
   const handleClickDetail = (index: number) => {
     setSelectedRow(index);
-
     // Navigate to the assignment detail view with the assignment's ID as a parameter
     navigate(`/assignment/${assignments[index].id}`);
   };
@@ -61,7 +65,6 @@ function Tareas({ mostrarFormulario }: TareasProps) {
     // Navigate to the assignment detail view with the assignment's ID as a parameter
     deleteAssignmentUseCase(assignments[index].id);
     window.location.reload();
-    
   };
   const handleRowHover = (index: number | null) => {
     setHoveredRow(index);
@@ -85,23 +88,27 @@ function Tareas({ mostrarFormulario }: TareasProps) {
           </TableHead>
           <TableBody>
             {assignments.map((assignment, index) => (
-              <TableRow>
+              <TableRow key={assignment.id}>
                 <TableCell>{assignment.title}</TableCell>
                 <TableCell>
                   <ButtonContainer key={assignment.id}>
-                    <IconButton aria-label="see" 
+                    <IconButton
+                      aria-label="see"
                       onClick={() => handleClickDetail(index)}
                       onMouseEnter={() => handleRowHover(index)}
-                      onMouseLeave={() => handleRowHover(null)}>
+                      onMouseLeave={() => handleRowHover(null)}
+                    >
                       <VisibilityIcon />
                     </IconButton>
                     <IconButton aria-label="edit">
                       <EditIcon />
                     </IconButton>
-                    <IconButton aria-label="delete" 
-                    onClick={() => handleClickDelete(index)}
-                    onMouseEnter={() => handleRowHover(index)}
-                    onMouseLeave={() => handleRowHover(null)}>
+                    <IconButton
+                      aria-label="delete"
+                      onClick={() => handleClickDelete(index)}
+                      onMouseEnter={() => handleRowHover(index)}
+                      onMouseLeave={() => handleRowHover(null)}
+                    >
                       <DeleteIcon />
                     </IconButton>
                     <IconButton aria-label="send">
