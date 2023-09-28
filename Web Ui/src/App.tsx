951c431a4943e90862b1b619942ccb8fe74da7de
Christian <christianriveroarnez@gmail.com>
2023-09-28 13:57:20 -0400

    deleted gh token, personal tokens cant be in a public env

--- Web Ui/src/App.tsx
+++ Web Ui/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-//import GestionTareas from "./application/views/assignmentsViews/components/assignmentManager";
+import GestionTareas from "./application/views/assignmentsViews/components/assignmentManager";
 import AssignmentDetail from "./application/views/assignmentDetailView/components/assignmentDetail";
 import CycleReportView from "./application/views/CycleReportView/components/CycleReportView";
 import { TDDCyclesPort } from "./application/views/CycleReportView/useCases/tddCycles.port";
@@ -10,7 +10,7 @@ function App() {
     <Router>
       Root Component Renders
       <Routes>
-        <Route path="/" element={<CycleReportView port={new TDDCyclesPort()} />} />
+        <Route path="/" element={<GestionTareas />} />
         <Route path="/assignment/:id" element={<AssignmentDetail />} />
         <Route
           path="/graph"
