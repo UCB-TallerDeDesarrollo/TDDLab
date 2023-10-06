7e976e180839d5ed393f879aea4d2399e4d4b14c
Christian <christianriveroarnez@gmail.com>
2023-10-06 13:58:21 -0400

    added date to commit int

--- server/src/adapters/dataBaseRepositories/commitRepository.ts
+++ server/src/adapters/dataBaseRepositories/commitRepository.ts
@@ -11,8 +11,10 @@ export class commitRepository{
     async saveCommitInfoOfRepo(owner:String,repoName:String,commit:CommitDTO){
         const client = await this.pool.connect();
 
-        const query = 'INSERT INTO commitsTable (owner, repoName, html_url, sha, total,additions,deletions,message,url,comment_count) VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10)';
-        const values = [owner,repoName,commit.html_url,commit.sha,commit.stats.total,commit.stats.additions,commit.stats.deletions,commit.commit.message,commit.commit.url,commit.commit.comment_count];
+        const query = 'INSERT INTO commitsTable (owner, repoName, html_url, sha, total,additions,deletions,message,url,comment_count,date) VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10,$11)';
+        const values = [owner,repoName,commit.html_url,commit.sha,
+                        commit.stats.total,commit.stats.additions,commit.stats.deletions,
+                        commit.commit.message,commit.commit.url,commit.commit.comment_count,commit.commit.date];
 
         const result = await client.query(query, values);
 
