d1a9a6602ab0d763dbf8018066d78fb1ccb90b7e
Ogreaggressive <nicolascari.rz@gmail.com>
2023-10-07 12:31:33 -0400

    using only best chart now

--- Web Ui/src/sections/TDDCycles-Visualization/TDDCycleList.tsx
+++ Web Ui/src/sections/TDDCycles-Visualization/TDDCycleList.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+
 import TDDCycleCard from "./TDDCycleCard";
 import { JobDataObject } from "../../TDDCycles-Visualization/domain/jobInterfaces";
-import { CommitDataObject, CommitInformationDataObject } from "../../TDDCycles-Visualization/domain/githubCommitInterfaces";
+import { CommitInformationDataObject } from "../../TDDCycles-Visualization/domain/githubCommitInterfaces";
 
 interface CycleReportViewProps {
   commitsInfo: CommitInformationDataObject[] | null;
@@ -9,12 +9,8 @@ interface CycleReportViewProps {
 }
 
 function TDDCycleList({ commitsInfo,jobsByCommit }: CycleReportViewProps) {
-  const repoOwner = "DwijanX";
-  const repoName = "Bulls-and-Cows";
-
   return (
     <>
-      <h1>Repository: {repoName}</h1>
       {jobsByCommit != null && commitsInfo != null && commitsInfo.map((commit) => (
         <TDDCycleCard key={commit.sha} commit={commit} jobs={jobsByCommit[commit.sha]} />
       ))}
