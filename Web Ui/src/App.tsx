2a9237e815da496496ed7f5291ebeaea5d80fb0c
Ogreaggressive <nicolascari.rz@gmail.com>
2023-10-07 12:29:31 -0400

    deleting other routes not used in router

--- Web Ui/src/App.tsx
+++ Web Ui/src/App.tsx
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GestionTareas from "./application/views/assignmentsViews/components/assignmentManager";
 import AssignmentDetail from "./application/views/assignmentDetailView/components/assignmentDetail";
-import TDDCycleList from "./sections/TDDCycles-Visualization/TDDCycleList";
 import {GithubAPIAdapter} from "./TDDCycles-Visualization/repository/GithubAPIAdapter";//Revisar el cambio por puerto
-import TDDChartsView from "./sections/TDDCycles-Visualization/TDDChartsView";
 import TDDChartPage from "./sections/TDDCycles-Visualization/TDDChartPage";
 
 function App() {
@@ -15,10 +13,6 @@ function App() {
         <Route path="/assignment/:id" element={<AssignmentDetail />} />
         <Route
           path="/graph"
-          //element={<TDDCycleList port={new GithubAPIAdapter()} />}
-        />
-        <Route
-          path="/bar"
           element={<TDDChartPage port={new GithubAPIAdapter()} />}
         />
       </Routes>
