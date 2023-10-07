1f6a9b52ccb132d3e634833a75789026072377bd
Ogreaggressive <nicolascari.rz@gmail.com>
2023-10-07 11:48:54 -0400

    modifying CycleCard

--- Web Ui/src/sections/TDDCycles-Visualization/TDDCycleList.tsx
+++ Web Ui/src/sections/TDDCycles-Visualization/TDDCycleList.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import { GetTDDCycles } from "../../TDDCycles-Visualization/application/GetTDDCycles";
-import { GithubAPIAdapter } from "../../TDDCycles-Visualization/repository/GithubAPIAdapter";
 import TDDCycleCard from "./TDDCycleCard";
 import { JobDataObject } from "../../TDDCycles-Visualization/domain/jobInterfaces";
 import { CommitDataObject, CommitInformationDataObject } from "../../TDDCycles-Visualization/domain/githubCommitInterfaces";
@@ -13,45 +11,6 @@ interface CycleReportViewProps {
 function TDDCycleList({ commitsInfo,jobsByCommit }: CycleReportViewProps) {
   const repoOwner = "DwijanX";
   const repoName = "Bulls-and-Cows";
-
-
-  const getTDDCycles = new GetTDDCycles(port);
-
-  const obtainJobsData = async () => {
-    try {
-      console.log("Fetching commits data...");
-      const jobsData: Record<string, JobDataObject> = await getTDDCycles.obtainJobsData(repoOwner, repoName);
-      setJobsByCommit(jobsData);
-    } catch (error) {
-      console.error('Error obtaining jobs:', error);
-    }
-  };
-
-  const obtainCommitsData = async () => {
-    console.log("Fetching commit information...");
-    try {
-      const commits: CommitDataObject[] = await getTDDCycles.obtainCommitsOfRepo(repoOwner, repoName);
-      if (commits) {
-        console.log("Fetching commit information...");
-        const commitsInfoData: CommitInformationDataObject[] = await Promise.all(
-          commits.map((commit) => getTDDCycles.obtainCommitInformation(repoOwner, repoName, commit.sha))
-        );
-        setCommitsInfo(commitsInfoData);
-      }
-    } catch (error) {
-      console.error('Error obtaining commit information:', error);
-    }
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await obtainJobsData();
-      await obtainCommitsData();
-    };
-    fetchData();
-  }, []);
-
-  console.log("Commits Info:", commitsInfo);
 
   return (
     <>
