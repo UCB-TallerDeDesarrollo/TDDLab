175189b92b63ef9d3695da0533dd56292f3784a4
Rene <ddcrene@gmail.com>
2023-10-06 20:01:52 -0400

    GithubAPIAdpater and interfaces

--- Web Ui/src/App.tsx
+++ Web Ui/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GestionTareas from "./application/views/assignmentsViews/components/assignmentManager";
 import AssignmentDetail from "./application/views/assignmentDetailView/components/assignmentDetail";
 import CycleReportView from "./application/views/CycleReportView/components/CycleReportView";
-import { TDDCyclesPort } from "./application/views/CycleReportView/useCases/tddCycles.port";
+import {GithubAPIAdapter} from "./TDDCycles-Visualization/repository/GithubAPIAdapter";
 import ChartsView from "./application/views/CycleReportView/components/ChartsView";
 
 function App() {
@@ -14,11 +14,11 @@ function App() {
         <Route path="/assignment/:id" element={<AssignmentDetail />} />
         <Route
           path="/graph"
-          element={<CycleReportView port={new TDDCyclesPort()} />}
+          element={<CycleReportView port={new GithubAPIAdapter()} />}
         />
         <Route
           path="/bar"
-          element={<ChartsView port={new TDDCyclesPort()} />}
+          element={<ChartsView port={new GithubAPIAdapter()} />}
         />
       </Routes>
     </Router>
