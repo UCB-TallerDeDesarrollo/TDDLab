ab91d11afd8657ededfedf433c5b691aeeb4b9e4
Christian <christianriveroarnez@gmail.com>
2023-10-07 21:06:30 -0400

    fixed build

--- Web Ui/test/sections/TDDCycles-Visualization/__mocks__/MocksGithubAPI.ts
+++ Web Ui/test/sections/TDDCycles-Visualization/__mocks__/MocksGithubAPI.ts
@@ -1,12 +1,12 @@
 import { GithubAPIRepository } from "../../../../src/TDDCycles-Visualization/domain/GithubAPIRepositoryInterface";
-import { mockCommitData, mockCommitInfoData } from "../__mocks__/dataTypeMocks/commitData";
+import { mockCommitData } from "../__mocks__/dataTypeMocks/commitData";
 
 export class MockGithubAPI implements GithubAPIRepository {
   obtainCommitsOfRepo = jest.fn(async () => {
     return [mockCommitData];
   });
   obtainCommitsFromSha = jest.fn(async () => {
-    return mockCommitInfoData;
+    return mockCommitData;
   });
   obtainRunsOfGithubActions = jest.fn(async () => {
     return {};
