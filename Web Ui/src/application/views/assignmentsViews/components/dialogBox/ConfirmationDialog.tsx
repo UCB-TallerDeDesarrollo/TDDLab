508303e6386a4b07b106159dffd8743e59f77ec4
Anaelise Huaniri Romero <aanesita@gmail.com>
2023-10-09 11:48:48 -0400

    Add two popups for delete and link

---
+++ Web Ui/src/application/views/assignmentsViews/components/dialogBox/ConfirmationDialog.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+
+interface ConfirmationDialogProps {
+  open: boolean;
+  title: string;
+  content: string;
+  cancelText: string;
+  deleteText: string;
+  onCancel: () => void;
+  onDelete: () => void;
+}
+
+export const ConfirmationDialog: React.FC<ConfirmationDialogProps> = ({
+  open,
+  title,
+  content,
+  cancelText,
+  deleteText,
+  onCancel,
+  onDelete,
+}) => {
+  const dialogContentStyle = {
+    fontFamily: "Roboto",
+    fontSize: "13px",
+  };
+  const titleStyle = {
+    fontSize: "1rem",
+    fontWeight: "bold",
+    fontFamily: "Roboto",
+  };
+
+  return (
+    <Dialog open={open}>
+      <DialogTitle style={titleStyle}>{title}</DialogTitle>
+      <DialogContent style={dialogContentStyle}>{content}</DialogContent>
+      <DialogActions>
+        <Button
+          onClick={onCancel}
+          color="primary"
+          style={dialogContentStyle}
+        >
+          {cancelText}
+        </Button>
+        <Button
+          onClick={onDelete}
+          color="primary"
+          style={dialogContentStyle}
+        >
+          {deleteText}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
