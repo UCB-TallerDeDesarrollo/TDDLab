38833c723cf09647729497cf4c2a627060df5c43
Gabriel Ichiro <gabriel582084@gmail.com>
2023-10-10 12:00:25 -0400

    added assignment interface

--- server/src/modules/Assignments/domain/Assignment.ts
+++ server/src/modules/Assignments/domain/Assignment.ts
@@ -1,5 +1,8 @@
-interface Assignment {
-    title:
-    description:
-    date
+export interface AssignmentDataObject {
+  id: number;
+  title: string;
+  description: string | null;
+  start_date: Date | null;
+  end_date: Date | null;
+  state: "pending" | "delivered";
 }
