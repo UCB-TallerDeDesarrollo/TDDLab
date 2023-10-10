60cc4cf293eeaad105016605bdb96381613f05d1
Gabriel Ichiro <gabriel582084@gmail.com>
2023-10-10 12:00:25 -0400

    added assignment interface

--- server/src/modules/Assignments/repositories/assignmentsPostgressAdapter.ts
+++ server/src/modules/Assignments/repositories/assignmentsPostgressAdapter.ts
@@ -1,27 +1,41 @@
-class AssignetPostgressAdapter() {
+import { Pool } from "pg";
+import config from "../../../config/db";
+import { AssignmentDataObject } from "../domain/Assignment";
+const pool = new Pool(config);
 
-    obtainAssignments() {
-        try {
-            // Use a pool client to connect to the database
-            const client = await pool.connect();
-        
-            // Query to retrieve all assignments from the 'assignments' table
-            const query = 'SELECT * FROM assignments';
-        
-            // Execute the query
-            const result = await client.query(query);
-        
-            // Release the client back to the pool
-            client.release();
-            // Respond with the fetched assignments as JSON
-            res.status(200).json(result.rows);
-          } catch (error) {
-            console.error('Error fetching assignments:', error);
-            res.status(500).json({ error: 'Server error' });
-          }
+class AssignmentPostgresAdapter {
+  async obtainAssignments(): Promise<AssignmentDataObject[]> {
+    try {
+      // Use a pool client to connect to the database
+      const client = await pool.connect();
 
-          
-          return [Assignment]
+      // Query to retrieve all assignments from the 'assignments' table
+      const query = "SELECT * FROM assignments";
+
+      // Execute the query
+      const result = await client.query(query);
+
+      // Release the client back to the pool
+      client.release();
+
+      // Map the rows to assignment objects
+      const assignments: AssignmentDataObject[] = result.rows.map(
+        (row: any) => ({
+          id: row.id,
+          title: row.title,
+          description: row.description,
+          start_date: row.start_date,
+          end_date: row.end_date,
+          state: row.state,
+        })
+      );
+
+      return assignments;
+    } catch (error) {
+      console.error("Error fetching assignments:", error);
+      throw error; // Rethrow the error to be handled by the caller
     }
+  }
 }
 
+export default AssignmentPostgresAdapter;
