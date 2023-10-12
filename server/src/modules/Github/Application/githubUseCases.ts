2ac436364663d1c0eee5ed5037255eed52514615
BMB <bruno.marquez@ucb.edu.bo>
2023-10-11 23:26:09 -0400

    Added route to test API

--- server/src/modules/Github/Application/githubUseCases.ts
+++ server/src/modules/Github/Application/githubUseCases.ts
@@ -1,16 +1,29 @@
 import { GithubAdapter } from "../Repositories/github.API";
-import { CommitDataObject, CommitInformationDataObject } from "../Domain/commitInterfaces";
+import { CommitInformationDataObject } from "../Domain/commitInterfaces";
 import { JobDataObject } from "../Domain/jobInterfaces";
+import { Request, Response } from 'express';
 
 export class TDDCyclesPort {
-  adapter:GithubAdapter
+  adapter : GithubAdapter
   constructor(){
-    this.adapter=new GithubAdapter()
+    this.adapter = new GithubAdapter()
   }
 
-  async obtainCommitsOfRepo(owner: string, repoName: string): Promise<CommitDataObject[]> {
-    return await this.adapter.obtainCommitsOfRepo(owner, repoName);
-  }
+async obtainCommitsOfRepo(req: Request, res: Response): Promise<void> {
+    try {
+        const owner = String(req.query.owner);
+        const repoName = String(req.query.repoName);
+        
+        if (!owner || !repoName) {
+            res.status(400).json({ error: 'Bad request, missing owner or repoName' });
+            return;
+        }
+        const ans = await this.adapter.obtainCommitsOfRepo(owner, repoName);
+        res.status(200).json(ans);
+    } catch (error) {
+        res.status(500).json({ error: 'Server errorr' });
+    }
+}
 
   async obtainCommitInformation(owner: string, repoName: string, sha: string): Promise<CommitInformationDataObject> {
     return await this.adapter.obtainCommitsFromSha(owner, repoName, sha);
