07b69a3efaae6220bcaeb155ecc62c451845f0f7
Christian <christianriveroarnez@gmail.com>
2023-10-12 15:30:20 -0400

    jobs data now works well with api

--- server/src/modules/Jobs/Application/jobUseCases.ts
+++ server/src/modules/Jobs/Application/jobUseCases.ts
@@ -1,13 +1,14 @@
 import { jobRepository } from "../Repositories/jobRepository";
+import { updateJobsTable } from "./updateJobsTable";
 // import { Job } from "../../domain/models/Job";
 
 
 export const getJobs = async (owner: string, repoName: string,Adapter:jobRepository=new jobRepository()) => {
-  const ans = await Adapter.getJobs(owner, repoName);
-  return ans
+  
+  await updateJobsTable(owner,repoName,Adapter)
+  const Jobs = await Adapter.getJobs(owner, repoName);
+  return Jobs
 };
 
-// export const saveJobs = async (req: Request, res: Response) => {
-    
-//   };
+
   
--- server/src/modules/Jobs/Repositories/jobRepository.ts
+++ server/src/modules/Jobs/Repositories/jobRepository.ts
@@ -28,8 +28,44 @@ export class jobRepository {
             }
         }
     }
+    async checkIfJobExistsInDb(owner: string, repo: string,jobId:number) {
+        let client;
+        try {
+            client = await this.pool.connect();
+            const query = 'SELECT * FROM jobsTable WHERE owner = $1 AND reponame = $2 AND id=$3';
+            const values = [owner, repo,jobId];
+            
+            const result = await client.query(query, values);
 
-    // async saveJob(owner: String, repo: String, job: Job) {
+            return result.rows;
+        } catch (error) {
+            console.error('Error en la consulta a la base de datos:', error);
+            throw new Error('Error en la consulta a la base de datos');
+        } finally {
+            if (client) {
+                client.release();
+            }
+        }
+    }
 
-    // }
+    async insertRecordsIntoDatabase(records:{ id: number; sha: string; owner: string; reponame: string; conclusion: string; }[]) {
+        const client = await this.pool.connect();
+        try {
+
+          for (const record of records) {
+            const { id, sha, owner, reponame, conclusion } = record;
+      
+            const query = 'INSERT INTO jobsTable (id, sha, owner, repoName, conclusion) VALUES ($1, $2, $3, $4, $5)';
+            const values = [id, sha, owner, reponame, conclusion];
+      
+            await client.query(query, values);
+          }
+        } catch (error) {
+          console.error('Error inserting records:', error);
+        } finally {
+            if (client) {
+                client.release();
+            }
+        }
+      }
 }
