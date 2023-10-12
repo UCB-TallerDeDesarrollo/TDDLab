f423d2ffae74ced94d568e866146e0900ffdcc3c
BMB <bruno.marquez@ucb.edu.bo>
2023-10-11 23:36:57 -0400

    Added route to get jobs from repo

--- server/src/modules/Github/Application/githubUseCases.ts
+++ server/src/modules/Github/Application/githubUseCases.ts
@@ -39,19 +39,24 @@ async obtainCommitsOfRepo(req: Request, res: Response): Promise<void> {
         res.status(500).json({ error: 'Server errorr' });
     }
   }
-  async obtainJobsData(owner: string, repoName: string):Promise<Record<string, JobDataObject>> {
-    let githubruns=await this.adapter.obtainRunsOfGithubActions(owner,repoName)
-    const commitsWithActions: [number,string][] =githubruns.data.workflow_runs.map((workFlowRun:any) => {
-      return [workFlowRun.id,workFlowRun.head_commit.id]
+async obtainJobsData(req: Request, res: Response): Promise<void> {
+    const owner = String(req.query.owner);
+    const repoName = String(req.query.repoName);
+    if (!owner || !repoName) {
+        res.status(400).json({ error: 'Bad request, missing owner or repoName' });
+        return;
+    }
+    const githubruns = await this.adapter.obtainRunsOfGithubActions(owner, repoName);
+    const commitsWithActions: [number, string][] = githubruns.data.workflow_runs.map((workFlowRun: any) => {
+        return [workFlowRun.id, workFlowRun.head_commit.id];
     });
-    let jobs: Record<string, JobDataObject> = {};
-    commitsWithActions.map(async(workflowInfo)=>{
-      let jobInfo=await this.adapter.obtainJobsOfACommit(owner,repoName,workflowInfo[0],1)
-      jobs[workflowInfo[1]]=jobInfo
-    })
+    const jobs: Record<string, JobDataObject> = {};
+    await Promise.all(commitsWithActions.map(async (workflowInfo) => {
+        const jobInfo = await this.adapter.obtainJobsOfACommit(owner, repoName, workflowInfo[0], 1);
+        jobs[workflowInfo[1]] = jobInfo;
+    }));
     console.log(jobs);
-    
-    return jobs
-  }
+    res.status(200).json(jobs);
+}
 
 }
