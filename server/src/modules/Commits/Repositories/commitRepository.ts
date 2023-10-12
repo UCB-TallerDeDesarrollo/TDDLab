2c7934b5b3a43d28d7e056c3e73e8e355004159c
Gabriel Ichiro <gabriel582084@gmail.com>
2023-10-11 20:55:05 -0400

    moved user crap to new architechture

--- server/src/modules/Commits/Repositories/commitRepository.ts
+++ server/src/modules/Commits/Repositories/commitRepository.ts
@@ -1,35 +1,50 @@
-import { Pool } from 'pg'; // Import the Pool from 'pg'
-import config from '../../../config/db';
-import { CommitDTO } from '../Domain/CommitDTO';
+import { Pool } from "pg"; // Import the Pool from 'pg'
+import config from "../../../config/db";
+import { CommitDTO } from "../Domain/CommitDTO";
 
+export class CommitRepository {
+  pool: Pool;
+  constructor() {
+    this.pool = new Pool(config);
+  }
+  async saveCommitInfoOfRepo(
+    owner: string,
+    repoName: string,
+    commit: CommitDTO
+  ) {
+    const client = await this.pool.connect();
 
-export class commitRepository{
-    pool:Pool
-    constructor(){
-        this.pool=new Pool(config)
-    }
-    async saveCommitInfoOfRepo(owner:String,repoName:String,commit:CommitDTO){
-        const client = await this.pool.connect();
+    const query =
+      "INSERT INTO commitsTable (owner, repoName, html_url, sha, total,additions,deletions,message,url,comment_count,date) VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10,$11)";
+    const values = [
+      owner,
+      repoName,
+      commit.html_url,
+      commit.sha,
+      commit.stats.total,
+      commit.stats.additions,
+      commit.stats.deletions,
+      commit.commit.message,
+      commit.commit.url,
+      commit.commit.comment_count,
+      commit.commit.date,
+    ];
 
-        const query = 'INSERT INTO commitsTable (owner, repoName, html_url, sha, total,additions,deletions,message,url,comment_count,date) VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10,$11)';
-        const values = [owner,repoName,commit.html_url,commit.sha,
-                        commit.stats.total,commit.stats.additions,commit.stats.deletions,
-                        commit.commit.message,commit.commit.url,commit.commit.comment_count,commit.commit.date];
+    const result = await client.query(query, values);
 
-        const result = await client.query(query, values);
+    client.release();
+    return result.rows;
+  }
+  async getCommitsOfRepo(owner: String, repoName: String) {
+    const client = await this.pool.connect();
 
-        client.release();
-        return result.rows
-    }
-    async getCommitsOfRepo(owner:String, repoName:String) {
-        const client = await this.pool.connect();
-        
-        const query = 'SELECT * FROM commitsTable WHERE owner = $1 AND reponame = $2';
-        const values = [owner, repoName];
-      
-        const result = await client.query(query, values);
-        
-        client.release();
-        return result.rows;
-      }
+    const query =
+      "SELECT * FROM commitsTable WHERE owner = $1 AND reponame = $2";
+    const values = [owner, repoName];
+
+    const result = await client.query(query, values);
+
+    client.release();
+    return result.rows;
+  }
 }
