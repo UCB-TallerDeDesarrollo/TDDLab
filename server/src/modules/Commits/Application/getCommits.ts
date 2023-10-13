0cbbe1dac789233587c333209afa34a87b3d1351
BMB <bruno.marquez@ucb.edu.bo>
2023-10-13 00:15:36 -0400

    Added code to save Commits in back from API

--- server/src/modules/Commits/Application/getCommits.ts
+++ server/src/modules/Commits/Application/getCommits.ts
@@ -1,7 +1,11 @@
 import { CommitRepository } from "../Repositories/commitRepository";
+import { updateCommitsTable } from "./updateCommitsTable";
 
 
 export const getCommits = async (owner: string, repoName: string,Adapter:CommitRepository=new CommitRepository()) => {
+  console.log("getCommits")
+  await updateCommitsTable(owner,repoName,Adapter)
+  console.log("getCommits2")
   const Jobs = await Adapter.getCommits(owner, repoName);
   return Jobs
 };
