6923d65e962292f1219b93ab09263433722e095c
Gabriel Ichiro <gabriel582084@gmail.com>
2023-10-14 17:54:11 -0400

    added correct query to update assignment usecase

--- server/src/modules/Assignments/repositories/AssignmentRepository.ts
+++ server/src/modules/Assignments/repositories/AssignmentRepository.ts
@@ -100,11 +100,19 @@ class AssignmentRepository {
   ): Promise<AssignmentDataObject | null> {
     try {
       const client = await pool.connect();
-      const { title, description, start_date, end_date, state } =
+      const { title, description, start_date, end_date, state, link } =
         updatedAssignment;
       const query =
-        "UPDATE assignments SET title = $1, description = $2, start_date = $3, end_date = $4, state = $5 WHERE id = $6 RETURNING *";
-      const values = [title, description, start_date, end_date, state, id];
+        "UPDATE assignments SET title = $1, description = $2, start_date = $3, end_date = $4, state = $5, link = $6 WHERE id = $7 RETURNING *";
+      const values = [
+        title,
+        description,
+        start_date,
+        end_date,
+        state,
+        link,
+        id,
+      ];
       const result = await client.query(query, values);
       client.release();
       if (result.rows.length === 1) {
