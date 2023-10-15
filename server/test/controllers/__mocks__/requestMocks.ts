4e74d4307e379fc327131c58a6578c22a98eeeae
Ricardo <jesus.pelaez@ucb.edu.bo>
2023-10-15 01:36:57 -0600

    Backend unit tests(AssignmentsController): Checks if the request for the create assignment is executed correctly.

--- server/test/controllers/__mocks__/requestMocks.ts
+++ server/test/controllers/__mocks__/requestMocks.ts
@@ -1,4 +1,5 @@
 import { Request } from "express";
+import { AssignmentDataObject } from "../../../src/modules/Assignments/domain/Assignment";
 
 export function createRequest() {
   return {} as Request;
@@ -7,6 +8,12 @@ export function createRequest() {
 export function createRequestWithId(_id: string): Request {
   return {
     params: { id: _id },
+  } as unknown as Request;
+}
+
+export function createRequestWithBody(_body: AssignmentDataObject): Request {
+  return {
+    body: _body,
   } as unknown as Request;
 }
 
