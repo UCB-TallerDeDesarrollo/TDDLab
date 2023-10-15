022296e060f68c3dba9eade7817601af3957998a
Ricardo <jesus.pelaez@ucb.edu.bo>
2023-10-15 00:34:24 -0600

    Backend unit tests(AssignmentsController): checks if the request in the get assignmen by id is executed correctly.

--- server/test/controllers/__mocks__/requestMocks.ts
+++ server/test/controllers/__mocks__/requestMocks.ts
@@ -4,3 +4,9 @@ export function createRequest() {
   return {} as Request;
 }
 
+export function createRequestWithId(_id: string): Request {
+  return {
+    params: { id: _id },
+  } as unknown as Request;
+}
+
--- server/test/controllers/assignmentController.test.ts
+++ server/test/controllers/assignmentController.test.ts
@@ -2,9 +2,11 @@ import AssignmentsController from "../../src/controllers/assignments/assignmentC
 import GetAssignments from "../../src/modules/Assignments/application/AssignmentUseCases/getAssignmentsUseCase";
 import AssignmentRepository from "../../src/modules/Assignments/repositories/AssignmentRepository";
 
-import {assignmentList} from "./__mocks__/dataTypeMocks/assignmentListData"
-import { createRequest } from "./__mocks__/requestMocks";
+import { assignmentList } from "./__mocks__/dataTypeMocks/assignmentListData"
+import { assignment } from "./__mocks__/dataTypeMocks/assignmentData"
+import { createRequest, createRequestWithId } from "./__mocks__/requestMocks";
 import { createResponse } from "./__mocks__/responseMoks";
+import GetAssignmentById from "../../src/modules/Assignments/application/AssignmentUseCases/getAssignmentByIdUseCase";
 
 let controller: AssignmentsController;
 let repository: AssignmentRepository;
@@ -24,11 +26,22 @@ describe('Get assignments', () => {
         expect(res.json).toHaveBeenCalledWith(assignmentList);
       });
       it('should respond with a 500 status and error message when getAssignments fails', async () => {
-        jest.spyOn(GetAssignments.prototype, "execute").mockRejectedValue(new Error("Error creating assignment"));
+        jest.spyOn(GetAssignments.prototype, "execute").mockRejectedValue(new Error);
         const req = createRequest();
         const res = createResponse();
         await controller.getAssignments(req, res);
         expect(res.status).toHaveBeenCalledWith(500);
         expect(res.json).toHaveBeenCalledWith({ error: "Server error" });
       });
+});
+
+describe('Get assignment by id', () => {
+    it('should respond with a status 200 and a list of assignments', async () => {
+      jest.spyOn(GetAssignmentById.prototype, "execute").mockResolvedValue(assignment);
+      const req = createRequestWithId('Tarea 1');
+      const res = createResponse();
+      await controller.getAssignmentById(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith(assignment);
+    });
 });
