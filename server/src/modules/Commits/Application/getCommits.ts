0bd731c5663cdb2df643193277a201a97d195fb6
BMB0 <bruno.marquez@ucb.edu.bo>
2023-10-17 12:47:05 -0400

    Updated use case logic

--- server/src/modules/Commits/Application/getCommits.ts
+++ server/src/modules/Commits/Application/getCommits.ts
@@ -3,19 +3,39 @@ import { CommitRepository } from "../Repositories/commitRepository";
 import { updateCommitsTable } from "./updateCommitsTable";
 
 
+// export const getCommits = async (
+//   owner: string,
+//   repoName: string,
+//   repositoryAdapter: CommitRepository,
+//   githubAdapter: GithubAdapter
+// ) => {
+//   try {
+//     await updateCommitsTable(owner, repoName, repositoryAdapter, githubAdapter);
+//   } catch (error) {
+//     console.error("Error updating commits table:", error);
+//     return { error: "Error updating commits table" };
+//   } finally {
+//     const Jobs = await repositoryAdapter.getCommits(owner, repoName);
+//     return Jobs;
+//   }
+// };
+
+
 export const getCommits = async (
   owner: string,
   repoName: string,
   repositoryAdapter: CommitRepository,
   githubAdapter: GithubAdapter
 ) => {
-  try {
-    await updateCommitsTable(owner, repoName, repositoryAdapter, githubAdapter);
-  } catch (error) {
-    console.error("Error updating commits table:", error);
-    return { error: "Error updating commits table" };
-  } finally {
-    const Jobs = await repositoryAdapter.getCommits(owner, repoName);
-    return Jobs;
+  ()
+  if (!repositoryExistDB(owner, repo)){
+    const commits = githubAdapter.getCommits
+    repositoryAdapter.saveCommitInfo(commits)
+
+  }else {
+    const commits = githubAdapter.getCommits
+    updateCommitsDB
   }
+  const Jobs = await repositoryAdapter.getCommits(owner, repoName);
+    return Jobs;
 };
