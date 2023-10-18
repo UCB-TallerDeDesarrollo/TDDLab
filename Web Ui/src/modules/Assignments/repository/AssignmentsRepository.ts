6c4060a4bd04dbcea4dc98e5c716f7dc7baaf15f
Rodrigo Guardia <rodrigo.guardia316@gmail.com>
2023-10-17 23:00:17 -0400

    added deliver useCase logic

--- Web Ui/src/modules/Assignments/repository/AssignmentsRepository.ts
+++ Web Ui/src/modules/Assignments/repository/AssignmentsRepository.ts
@@ -1,94 +1,100 @@
-import axios from 'axios'; // Import Axios or your preferred HTTP library
-import { AssignmentDataObject } from '../domain/assignmentInterfaces'; // Import your assignment model
-import AssignmentsRepositoryInterface from '../domain/AssignmentsRepositoryInterface';  
+import axios from "axios"; // Import Axios or your preferred HTTP library
+import { AssignmentDataObject } from "../domain/assignmentInterfaces"; // Import your assignment model
+import AssignmentsRepositoryInterface from "../domain/AssignmentsRepositoryInterface";
 
-const API_URL = 'https://tdd-lab-6hbrauuzu-tddlabs-projects.vercel.app/api/assignments';
+const API_URL =
+  "https://tdd-lab-6hbrauuzu-tddlabs-projects.vercel.app/api/assignments";
 
 class AssignmentsRepository implements AssignmentsRepositoryInterface {
+  async getAssignments(): Promise<AssignmentDataObject[]> {
+    try {
+      // Send a GET request to fetch assignments from the backend
+      const response = await axios.get(API_URL);
 
-
-
-async getAssignments(): Promise<AssignmentDataObject[]> {
-  try {
-    // Send a GET request to fetch assignments from the backend
-    const response = await axios.get(API_URL);
-
-    // Check if the response status is successful (e.g., 200 OK)
-    if (response.status === 200) {
-      // Return the assignments data from the response
-      return response.data;
-    } else {
-      // Handle other response status codes or errors here if needed
-      throw new Error('Failed to fetch assignments');
+      // Check if the response status is successful (e.g., 200 OK)
+      if (response.status === 200) {
+        // Return the assignments data from the response
+        return response.data;
+      } else {
+        // Handle other response status codes or errors here if needed
+        throw new Error("Failed to fetch assignments");
+      }
+    } catch (error) {
+      // Handle any network errors or exceptions that may occur
+      console.error("Error fetching assignments:", error);
+      throw error;
     }
-  } catch (error) {
-    // Handle any network errors or exceptions that may occur
-    console.error('Error fetching assignments:', error);
-    throw error;
   }
-}
 
+  // Define a function to fetch an assignment by its ID
+  async getAssignmentById(
+    assignmentId: number
+  ): Promise<AssignmentDataObject | null> {
+    try {
+      // Send a GET request to fetch a specific assignment by ID
+      const response = await axios.get(`${API_URL}/${assignmentId}`);
 
-
-
-// Define a function to fetch an assignment by its ID
-async getAssignmentById(assignmentId: number): Promise<AssignmentDataObject | null> {
-  try {
-    // Send a GET request to fetch a specific assignment by ID
-    const response = await axios.get(`${API_URL}/${assignmentId}`);
-    
-    // Check if the response status is successful (e.g., 200 OK)
-    if (response.status === 200) {
-      // Return the assignment data from the response
-      return response.data;
-    } else if (response.status === 404) {
-      // Return null if the assignment was not found
-      return null;
-    } else {
-      // Handle other response status codes or errors here if needed
-      throw new Error('Failed to fetch assignment by ID');
+      // Check if the response status is successful (e.g., 200 OK)
+      if (response.status === 200) {
+        // Return the assignment data from the response
+        return response.data;
+      } else if (response.status === 404) {
+        // Return null if the assignment was not found
+        return null;
+      } else {
+        // Handle other response status codes or errors here if needed
+        throw new Error("Failed to fetch assignment by ID");
+      }
+    } catch (error) {
+      // Handle any network errors or exceptions that may occur
+      console.error("Error fetching assignment by ID:", error);
+      throw error;
     }
-  } catch (error) {
-    // Handle any network errors or exceptions that may occur
-    console.error('Error fetching assignment by ID:', error);
-    throw error;
   }
-}
 
+  async createAssignment(assignmentData: AssignmentDataObject): Promise<void> {
+    // Send a POST request to create a new assignment
+    await axios.post(API_URL, assignmentData);
+  }
 
-async createAssignment(assignmentData: AssignmentDataObject): Promise<void> {
-  // Send a POST request to create a new assignment
-  await axios.post(API_URL, assignmentData);
-}
+  async updateAssignment(
+    assignmentId: number,
+    assignmentData: AssignmentDataObject
+  ): Promise<void> {
+    // Send a PUT request to update an assignment
+    console.log(API_URL);
+    console.log(assignmentData.id);
+    console.log(`${API_URL}/${assignmentId}`);
 
-async updateAssignment(assignmentId: number, assignmentData: AssignmentDataObject): Promise<void> {
-  // Send a PUT request to update an assignment
-  console.log(API_URL)
-  console.log(assignmentData.id);
-  console.log(`${API_URL}/${assignmentId}`);
-  
-  await axios.put(`${API_URL}/${assignmentId}`, assignmentData);
-}
+    await axios.put(`${API_URL}/${assignmentId}`, assignmentData);
+  }
 
-async deleteAssignment(assignmentId: number): Promise<void> {
-  try {
-    // Send a GET request to fetch a specific assignment by ID
-    const response = await axios.delete(`${API_URL}/${assignmentId}`);
+  async deleteAssignment(assignmentId: number): Promise<void> {
+    try {
+      // Send a GET request to fetch a specific assignment by ID
+      const response = await axios.delete(`${API_URL}/${assignmentId}`);
 
-    // Check if the response status is successful (e.g., 200 OK)
-    if (response.status === 200) {
-      // Return the assignment data from the response
-      return response.data;
-    } else {
-      // Handle other response status codes or errors here if needed
-      throw new Error('Failed to delete assignment by ID');
+      // Check if the response status is successful (e.g., 200 OK)
+      if (response.status === 200) {
+        // Return the assignment data from the response
+        return response.data;
+      } else {
+        // Handle other response status codes or errors here if needed
+        throw new Error("Failed to delete assignment by ID");
+      }
+    } catch (error) {
+      // Handle any network errors or exceptions that may occur
+      console.error("Error deleting assignment by ID:", error);
+      throw error;
     }
-  } catch (error) {
-    // Handle any network errors or exceptions that may occur
-    console.error('Error deleting assignment by ID:', error);
-    throw error;
   }
-};
+
+  async deliverAssignment(
+    assignmentId: number,
+    assignmentData: AssignmentDataObject
+  ): Promise<void> {
+    await axios.put(`${API_URL}/${assignmentId}/deliver`, assignmentData);
+  }
 }
 
 export default AssignmentsRepository;
