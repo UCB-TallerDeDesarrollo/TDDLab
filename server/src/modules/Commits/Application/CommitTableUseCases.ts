e0294f5386c09b199ffb871d7e2475e5ea7ade27
BMB <bruno.marquez@ucb.edu.bo>
2023-10-17 20:43:48 -0400

    Refactored updateCommitsTable into a class named CommitTableUseCases

---
+++ server/src/modules/Commits/Application/CommitTableUseCases.ts
@@ -0,0 +1,73 @@
+import { GithubAdapter } from "../../Github/Repositories/github.API";
+import { CommitDataObject } from "../Domain/CommitDataObject";
+import { CommitRepository } from "../Repositories/commitRepository";
+
+export class CommitTableUseCases {
+  constructor(
+    private repositoryAdapter: CommitRepository,
+    private githubAdapter: GithubAdapter
+  ) {}
+
+  async checkNewCommits(owner: string, repoName: string, commitsData: CommitDataObject[]) {
+    let commitsToAdd = [];
+
+    for (let index = 0; index < commitsData.length; index++) {
+      let currentCommit = commitsData[index];
+      let row = await this.repositoryAdapter.commitExists(owner, repoName, currentCommit.sha);
+      if (row.length != 0) {
+        break;
+      } else {
+        commitsToAdd.push(currentCommit);
+      }
+    }
+    return commitsToAdd;
+  }
+
+  async getCommitsAPI(owner: string, repoName: string) {
+    try{
+      const commits = await this.githubAdapter.obtainCommitsOfRepo(owner, repoName);
+      const commitsFromSha = await Promise.all(
+        commits.map((commit: any) => {
+          return this.githubAdapter.obtainCommitsFromSha(owner, repoName, commit.sha);
+        })
+      );
+      const commitsData: CommitDataObject[] = commitsFromSha.map(
+        (commit: any) => {
+          return {
+            html_url: commit.html_url,
+            stats: {
+              total: commit.stats.total,
+              additions: commit.stats.additions,
+              deletions: commit.stats.deletions,
+            },
+            commit: {
+              date: commit.commit.author.date,
+              message: commit.commit.message,
+              url: commit.commit.url,
+              comment_count: commit.commit.comment_count,
+            },
+            sha: commit.sha,
+          };
+        }
+      );
+      return commitsData
+    } catch (error) {
+      console.error("Error en la obtención de commits:", error);
+      throw { error: "Error en la obtención de commits" };
+    }
+  }
+
+  async saveCommitsDB(owner: string, repoName: string, newCommits: CommitDataObject[]) {
+    try {
+      if (newCommits.length > 0) {
+        await Promise.all(
+          newCommits.map(async (commit: CommitDataObject) => {
+            !(await this.repositoryAdapter.saveCommitInfoOfRepo(owner, repoName, commit));
+          })
+        );
+      }
+    } catch (error) {
+      console.error("Error en la actualización de la tabla de commits:", error);
+    }
+  }
+}
--- server/src/modules/Commits/Application/getCommits.ts
+++ server/src/modules/Commits/Application/getCommits.ts
@@ -1,25 +1,27 @@
 import { GithubAdapter } from "../../Github/Repositories/github.API";
 import { CommitRepository } from "../Repositories/commitRepository";
-import { getCommitsAPI, saveCommitsDB, checkNewCommits } from "./updateCommitsTable";
+import { CommitTableUseCases } from "./CommitTableUseCases";
 
 export class CommitUseCases {
+  private commitTableUseCases: CommitTableUseCases;
   constructor(
     private repositoryAdapter: CommitRepository,
-    private githubAdapter: GithubAdapter
+    private githubAdapter: GithubAdapter,
   ) {
     this.repositoryAdapter = repositoryAdapter;
     this.githubAdapter = githubAdapter;
+    this.commitTableUseCases = new CommitTableUseCases(this.repositoryAdapter, this.githubAdapter);
   }
 
   async getCommits(owner: string, repoName: string) {
     try {
       if (!await this.repositoryAdapter.repositoryExist(owner, repoName)) {
-        const commits = await getCommitsAPI(owner, repoName, this.githubAdapter);
-        saveCommitsDB(owner, repoName, commits, this.repositoryAdapter);
+        const commits = await this.commitTableUseCases.getCommitsAPI(owner, repoName);
+        this.commitTableUseCases.saveCommitsDB(owner, repoName, commits);
       } else {
-        const commits = await getCommitsAPI(owner, repoName, this.githubAdapter); //getCommitsAPI should be changed to getLastCommits once it is implemented
-        const newCommits = await checkNewCommits(owner, repoName, commits);
-        saveCommitsDB(owner, repoName, newCommits, this.repositoryAdapter);
+        const commits = await this.commitTableUseCases.getCommitsAPI(owner, repoName); //getCommitsAPI should be changed to getLastCommits once it is implemented
+        const newCommits = await this.commitTableUseCases.checkNewCommits(owner, repoName, commits);
+        this.commitTableUseCases.saveCommitsDB(owner, repoName, newCommits);
       }
     } catch (error) {
       console.error("Error updating commits table:", error);
