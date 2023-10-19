dc23565401e94cf584519ce4063a02b4319e9969
Ricardo <jesus.pelaez@ucb.edu.bo>
2023-10-19 02:49:51 -0600

    Backend unit tests: Unit tests for the update assignment use case.

---
+++ server/test/modules/assignments/application/updateAssignmentUseCase.test.ts
@@ -0,0 +1,35 @@
+import UpdateAssignment from "../../../../src/modules/Assignments/application/AssignmentUseCases/updateAssignmentUseCase";
+import { getAssignmentMock } from "../../../__mocks__/assignments/dataTypeMocks/assignmentData";
+import { getAssignmentRepositoryMock } from "../../../__mocks__/assignments/repositoryMock";
+
+const assignmentRepositoryMock = getAssignmentRepositoryMock();
+
+let updateAssignment: UpdateAssignment;
+
+  beforeEach(() => {
+    updateAssignment = new UpdateAssignment(assignmentRepositoryMock);
+  });
+
+describe("UpdateAssignment", () => {
+  it("should update an assignment successfully", async () => {
+    const assignmentId = 1;
+    assignmentRepositoryMock.updateAssignment.mockResolvedValueOnce(getAssignmentMock());
+    const result = await updateAssignment.execute(assignmentId, getAssignmentMock());
+    expect(result).toEqual(getAssignmentMock());
+    expect(assignmentRepositoryMock.updateAssignment).toHaveBeenCalledWith(
+      assignmentId,
+      getAssignmentMock()
+    );
+  });
+
+  it("should handle errors when updating an assignment", async () => {
+    const assignmentId = 2;
+    assignmentRepositoryMock.updateAssignment.mockRejectedValueOnce(new Error());
+    await expect(updateAssignment.execute(assignmentId, getAssignmentMock())).rejects.toThrow();
+    expect(assignmentRepositoryMock.updateAssignment).toHaveBeenCalledWith(
+      assignmentId,
+      getAssignmentMock()
+    );
+  });
+});
+
