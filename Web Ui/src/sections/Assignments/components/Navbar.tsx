55f474bc629e486c6c1e591dd85fcf39ce7d714d
Anaelise Huaniri Romero <aanesita@gmail.com>
2023-10-27 19:54:18 -0400

    I added the button login, also the options in the navbar

--- Web Ui/src/sections/Assignments/components/Navbar.tsx
+++ Web Ui/src/sections/Assignments/components/Navbar.tsx
@@ -1,15 +1,49 @@
-import { AppBar,Toolbar, Grid, Typography } from '@mui/material';
+
+import React, { useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { AppBar, Toolbar, Button, Tabs, Tab, Typography } from '@mui/material';
+import WindowIcon from '@mui/icons-material/Window';
+
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#fff' // Cambiar el color secundario a blanco
+    }
+  }
+});
 
 const Navbar = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
-   <AppBar>
+    <ThemeProvider theme={theme}>
+      <AppBar sx={{ background: "#052845" }}>
         <Toolbar>
-            <Typography>
-                TDDLab
-            </Typography>
+          <WindowIcon />
+          <Typography>
+            TDDLab
+          </Typography>
+          <Tabs
+            textColor="inherit"
+            value={value}
+            onChange={handleChange}
+            indicatorColor="secondary"
+          >
+            <Tab label="Grupos" />
+            <Tab label="Tareas" />
+            <Tab label="Usuario" />
+          </Tabs>
+          <Button sx={{ marginLeft: 'auto' }} variant="contained">
+            Iniciar sesión
+          </Button>
         </Toolbar>
-   </AppBar>   
-  )
+      </AppBar>
+    </ThemeProvider>
+  );
 }
 
-export default Navbar
+export default Navbar;
