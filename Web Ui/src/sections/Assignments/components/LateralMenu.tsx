911fe525a41e2814c057ae327d7377281375ab1a
Anaelise Huaniri Romero <aanesita@gmail.com>
2023-10-28 11:00:44 -0400

    I added some elements to make it responsive

--- Web Ui/src/sections/Assignments/components/LateralMenu.tsx
+++ Web Ui/src/sections/Assignments/components/LateralMenu.tsx
@@ -1,21 +1,33 @@
 import React, { useState } from 'react'
 import {Drawer,IconButton,List,ListItemButton,ListItemIcon,ListItemText} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+
+const PAGES=["Iniciar sesión","Grupos","Tareas", "Usuario"];
 const LateralMenu = () => {
   const [openMenu, setOpenMenu] =useState(false);
   return (
     <React.Fragment>
         <Drawer open={openMenu} onClose={()=>setOpenMenu(false)}>
             <List>
-                <ListItemButton>
-                    <ListItemText>Iniciar sesión</ListItemText>
+              {
+                PAGES.map((page,index)=>(
+                  <ListItemButton onClick={()=>setOpenMenu(false)}key={index}>
+                    <ListItemIcon>
+                      <ListItemText>{page}</ListItemText>
+                    </ListItemIcon>
+                    
                 </ListItemButton>
+                ))
+              }
+                
             </List>
         </Drawer>
-        <IconButton onClick={()=>setOpenMenu(!openMenu)}>
-         
+        <IconButton
+        sx={{color: "white", marginLeft:"auto"}}
+         onClick={()=>setOpenMenu(!openMenu)}
+         >
+          <MenuIcon />
         </IconButton>
-
     </React.Fragment>  
   )
   }
