e8da34efd554a8d200df88a3b3a90c3a107fb945
Anaelise Huaniri Romero <aanesita@gmail.com>
2023-10-28 11:00:44 -0400

    I added some elements to make it responsive

--- Web Ui/src/sections/Assignments/components/Navbar.tsx
+++ Web Ui/src/sections/Assignments/components/Navbar.tsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { AppBar, Toolbar, Button, Tabs, Tab, Typography } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Tabs,
+  Tab,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import WindowIcon from "@mui/icons-material/Window";
 import LateralMenu from "./LateralMenu";
 
+const PAGES=["Grupos","Tareas", "Usuario"];
 const theme = createTheme({
   palette: {
     secondary: {
@@ -12,8 +22,13 @@ const theme = createTheme({
   },
 });
 
+
 const Navbar = () => {
   const [value, setValue] = useState(0);
+  const theme2 = useTheme();
+  console.log(theme2);
+  const isMatch = useMediaQuery(theme2.breakpoints.down("md"));
+  console.log(isMatch);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
@@ -25,27 +40,32 @@ const Navbar = () => {
         <Toolbar>
           <WindowIcon sx={{ marginRight: "6px" }} />
           <Typography sx={{ marginRight: "70px" }}>TDDLab</Typography>
-          <div style={{ marginLeft: 'auto' }}> 
-            <Tabs
-              textColor="inherit"
-              value={value}
-              onChange={handleChange}
-              indicatorColor="secondary"
-            >
-              <Tab label="Grupos" />
-              <Tab label="Tareas" />
-              <Tab label="Usuario" />
-            </Tabs>
-          </div>
-          
-          <Button
-            
-            variant="contained"
-          >
-            Iniciar sesión
-          </Button>
+          {isMatch ? (
+            <>
+              <LateralMenu />
+            </>
+          ) : (
+            <>
+              <div style={{ marginLeft: "auto" }}>
+                <Tabs
+                  textColor="inherit"
+                  value={value}
+                  onChange={handleChange}
+                  indicatorColor="secondary"
+                >
+                    {
+                      PAGES.map((page, index)=>(
+                        <Tab key={index} label={page}/>
+                      ))
+                    }
+
+                </Tabs>
+              </div>
+
+              <Button variant="contained" sx={{ marginLeft: "18px" }}>Iniciar sesión</Button>
+            </>
+          )}
         </Toolbar>
-        <LateralMenu/>
       </AppBar>
     </ThemeProvider>
   );
