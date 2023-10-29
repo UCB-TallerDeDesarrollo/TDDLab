da126051475d7b85a0ce779f0d7f6a3827822925
FranAliss <franalissar@gmail.com>
2023-10-29 12:51:23 -0400

    Added feature so that link can be copied

--- Web Ui/src/sections/Assignments/components/EnrollmentLink.tsx
+++ Web Ui/src/sections/Assignments/components/EnrollmentLink.tsx
@@ -8,14 +8,22 @@ const InvitationComponent: React.FC = () => {
     setMostrarLink(true);
   };
 
-
+  const copiarAlPortapapeles = () => {
+    const inputLink = document.getElementById("linkText") as HTMLInputElement;
+    if (inputLink) {
+      inputLink.select();
+      document.execCommand("copy");
+      alert("Link copiado al portapapeles");
+    }
+  };
+  
   return (
     <div>
       <button onClick={generarLink}>Generar Link de Invitación</button>
       {mostrarLink && (
         <div>
           <input type="text" id="linkText" value={link} readOnly />
-
+          <button onClick={copiarAlPortapapeles}>Copiar</button>
         </div>
       )}
     </div>
