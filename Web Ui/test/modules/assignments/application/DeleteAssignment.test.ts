3a1a6700502b688cd082c4539b0e5221d336c0ee
luferchoo <luisfernandogilgareca@gmail.com>
2023-10-30 15:19:52 -0400

    SP3 delete assignment: 'Succesful deletion'

--- Web Ui/test/modules/assignments/application/DeleteAssignment.test.ts
+++ Web Ui/test/modules/assignments/application/DeleteAssignment.test.ts
@@ -1,5 +1,5 @@
 import { DeleteAssignment } from "../../../../src/modules/Assignments/application/DeleteAssignment";
-//import { AssignmentDataObject } from "../../../../src/modules/Assignments/domain/assignmentInterfaces";
+import { AssignmentDataObject } from "../../../../src/modules/Assignments/domain/assignmentInterfaces";
 import { MockAssignmentsRepository } from "../../__mocks__/mockAssignmentsRepository";
 
 let mockRepository: MockAssignmentsRepository;
@@ -8,9 +8,20 @@ beforeEach(() => {
     mockRepository = new MockAssignmentsRepository();
     deleteAssignment = new DeleteAssignment(mockRepository);
 });
-describe('Test', () => {
-    it('Should return true.', () => {
-        expect(true).toBeTruthy;
+describe("Create Assignment", () => {
+    it("Should successfully create a new assignment", async () => {
+        const assignmentId = 1;
+        const assignment: AssignmentDataObject = {
+            id: assignmentId,
+            title: "Tarea Test",
+            description: "Esta es una tarea de prueba",
+            start_date: new Date("2023-10-31"),
+            end_date: new Date("2023-11-05"),
+            state: "inProgress",
+            link: "Enlace",
+        };
+        mockRepository.createAssignment(assignment);
+        const obtainedAssignment = await deleteAssignment.deleteAssignment(1);
+        expect(obtainedAssignment).toEqual('Succesful deletion');
     });
 });
-  
