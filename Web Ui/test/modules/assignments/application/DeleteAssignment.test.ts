8483f30c2a13aec55078f557c55ae2f4eaa75a28
luferchoo <luisfernandogilgareca@gmail.com>
2023-10-30 15:13:56 -0400

    SP3 delete assignment: declared

--- Web Ui/test/modules/assignments/application/DeleteAssignment.test.ts
+++ Web Ui/test/modules/assignments/application/DeleteAssignment.test.ts
@@ -1,3 +1,13 @@
+import { DeleteAssignment } from "../../../../src/modules/Assignments/application/DeleteAssignment";
+//import { AssignmentDataObject } from "../../../../src/modules/Assignments/domain/assignmentInterfaces";
+import { MockAssignmentsRepository } from "../../__mocks__/mockAssignmentsRepository";
+
+let mockRepository: MockAssignmentsRepository;
+let deleteAssignment: DeleteAssignment;
+beforeEach(() => {
+    mockRepository = new MockAssignmentsRepository();
+    deleteAssignment = new DeleteAssignment(mockRepository);
+});
 describe('Test', () => {
     it('Should return true.', () => {
         expect(true).toBeTruthy;
