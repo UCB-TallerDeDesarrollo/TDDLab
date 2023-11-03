9e32097bc58f51c4c4b960c28b3d8117742ab3d8
Rodrigo Guardia <rodrigo.guardia316@gmail.com>
2023-11-03 10:01:09 -0400

    Fixed some code smells on frontend

--- Web Ui/src/sections/Assignments/components/AssignmentsList.tsx
+++ Web Ui/src/sections/Assignments/components/AssignmentsList.tsx
@@ -38,7 +38,7 @@ interface AssignmentsProps {
   mostrarFormulario: () => void;
 }
 
-function Assignments({ mostrarFormulario }: AssignmentsProps) {
+function Assignments({ mostrarFormulario }: Readonly<AssignmentsProps>) {
   const [confirmationOpen, setConfirmationOpen] = useState(false);
   const [githubLinkDialogOpen, setGithubLinkDialogOpen] = useState(false);
   const [selectedAssignmentIndex, setSelectedAssignmentIndex] = useState<
@@ -101,7 +101,6 @@ function Assignments({ mostrarFormulario }: AssignmentsProps) {
     setSelectedRow(index);
     setGithubLinkDialogOpen(true);
     setSelectedAssignmentIndex(index);
-    // submitAssignment.submitAssignment(assignments[index].id);
   };
 
   const handleSendGithubLink = (link: string) => {
