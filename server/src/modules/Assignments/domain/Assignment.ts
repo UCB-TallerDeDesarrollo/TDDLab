11f7144a890fdd648e3541a8ba511243791cb73a
Gabriel Ichiro <gabriel582084@gmail.com>
2023-11-02 23:55:15 -0400

    added addigment comment field in backend

--- server/src/modules/Assignments/domain/Assignment.ts
+++ server/src/modules/Assignments/domain/Assignment.ts
@@ -4,5 +4,6 @@ export interface AssignmentDataObject {
   start_date: Date | null;
   end_date: Date | null;
   state: "pending" | "in progress" | "delivered";
-  link: string | null; // Add the link field
+  link: string | null;
+  comment: string | null;
 }
