945955f06e1493eac7c7e15a5428309c663835e9
Rene <ddcrene@gmail.com>
2023-11-05 17:39:15 -0400

    Refactoring invitation code

--- Web Ui/src/sections/Invitation/InvitationPage.tsx
+++ Web Ui/src/sections/Invitation/InvitationPage.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { getAuth, OAuthProvider, onAuthStateChanged, signInWithPopup, User, signOut } from 'firebase/auth';
 import firebase from '../../firebaseConfig'; // Importa la instancia de Firebase
-import ConfirmationPopUp from './PopUp';
+import SuccessfulSignUpPopUp from './components/SuccessfulSignUpPopUp';
+import SuccessfulEnrollmentPopUp from './components/SuccessfulEnrollmentPopUp';
+import Button from '@mui/material/Button';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const AuthComponent: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const auth = getAuth(firebase); // Utiliza la instancia de Firebase
+    const auth = getAuth(firebase);
     const unsubscribe = onAuthStateChanged(auth, (authUser) => {
       setUser(authUser);
     });
-
     return () => {
       unsubscribe();
     };
@@ -19,7 +21,6 @@ const AuthComponent: React.FC = () => {
 
   const handleSignInWithGitHub = async () => {
     const provider = new OAuthProvider('github.com');
-
     try {
       const auth = getAuth(firebase); // Obtén la instancia de autenticación aquí
       const result = await signInWithPopup(auth, provider);
@@ -38,22 +39,37 @@ const AuthComponent: React.FC = () => {
     }
   };
 
+  const [showPopUp, setShowPopUp] = useState(false);
+
+  const handleAcceptInvitation = () => {
+    setShowPopUp(true);
+  };
 
   return (
     <div>
       {user ? (
         <div>
           <p>Bienvenido, {user.displayName}</p>
-          <img src={user.photoURL ?? 'URL_POR_DEFECTO'} alt={user.displayName || 'Usuario'} />
-          <button onClick={handleSignOut}>Cerrar sesión</button>
+          <img src={user.photoURL ?? 'URL_POR_DEFECTO'} alt={user.displayName ?? 'Usuario'} />
+          <Button onClick={handleSignOut}>Cerrar sesión</Button>
+          <Button onClick={handleAcceptInvitation}>Aceptar invitación al curso</Button>
+          {showPopUp && (
+            <SuccessfulEnrollmentPopUp></SuccessfulEnrollmentPopUp>
+          )}
         </div>
       ) : (
-        <button onClick={handleSignInWithGitHub}>Iniciar sesión con GitHub</button>
+        <Button       
+            color="primary"
+            aria-label="GitHub" 
+            onClick={handleSignInWithGitHub}
+          >
+            <GitHubIcon></GitHubIcon>
+            Iniciar Sesión
+        </Button>
       )}
+
       {user && (
-          <div className="popup">
-            <ConfirmationPopUp photoAccount={user.photoURL} nameAccount={user.displayName}></ConfirmationPopUp>
-          </div>
+        <SuccessfulSignUpPopUp photoAccount={user.photoURL} nameAccount={user.displayName}></SuccessfulSignUpPopUp>
       )}
     </div>
   );
