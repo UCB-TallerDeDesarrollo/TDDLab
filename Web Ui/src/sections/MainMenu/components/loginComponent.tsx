1dbc09e93f7843537ed5f5c82cfd4b7a1982f9f6
Christian <christianriveroarnez@gmail.com>
2023-11-06 23:06:03 -0400

    finished login

--- Web Ui/src/sections/MainMenu/components/loginComponent.tsx
+++ Web Ui/src/sections/MainMenu/components/loginComponent.tsx
@@ -1,16 +1,27 @@
 import { Button } from "@mui/material";
 import { GithubAuthPort } from "../../../modules/Auth/application/GithubAuthPort";
 import { LoginPort } from "../../../modules/Auth/application/LoginPort";
+import {
+  setGlobalState,
+  useGlobalState,
+} from "../../../modules/Auth/domain/authStates";
+import React from "react";
+import "../styles/loginComponentStyles.css";
+export default function LoginComponent() {
+  const authData = useGlobalState("authData");
 
-export default function loginComponent() {
   const handleLogin = async () => {
     const githbAuthPort = new GithubAuthPort();
     let userData = await githbAuthPort.handleSignInWithGitHub();
     if (userData?.email) {
       const loginPort = new LoginPort();
       let userCourse = await loginPort.userHasAnAcount(userData.email);
-      if (userCourse) {
-        console.log("Valid User");
+      if (userCourse && userData.photoURL) {
+        setGlobalState("authData", {
+          userProfilePic: userData.photoURL,
+          userEmail: userData.email,
+          userCourse: userCourse,
+        });
       } else {
         console.log("Invalid User");
       }
@@ -18,12 +29,23 @@ export default function loginComponent() {
   };
 
   return (
-    <Button
-      onClick={handleLogin}
-      variant="contained"
-      sx={{ marginLeft: "18px" }}
-    >
-      Iniciar sesión
-    </Button>
+    <React.Fragment>
+      {!authData[0].userEmail && (
+        <Button
+          onClick={handleLogin}
+          variant="contained"
+          sx={{ marginLeft: "18px" }}
+        >
+          Iniciar sesión
+        </Button>
+      )}
+      {authData[0].userEmail && (
+        <img
+          src={authData[0].userProfilePic}
+          alt="Profile Picture"
+          className="profilePicture"
+        />
+      )}
+    </React.Fragment>
   );
 }
