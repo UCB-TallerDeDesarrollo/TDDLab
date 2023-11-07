a9af310a465107098491b32a312bef5ba017caf3
Christian <christianriveroarnez@gmail.com>
2023-11-06 22:40:04 -0400

    simple login done

--- Web Ui/src/sections/MainMenu/MainMenu.tsx
+++ Web Ui/src/sections/MainMenu/MainMenu.tsx
@@ -12,6 +12,9 @@ import MenuIcon from "@mui/icons-material/Menu";
 import { ReactElement, useState } from "react";
 import { NavLink } from "react-router-dom";
 import WindowIcon from "@mui/icons-material/Window";
+import { LoginPort } from "../../modules/Auth/application/LoginPort";
+import { GithubAuthPort } from "../../modules/Auth/application/GithubAuthPort";
+import UserOnDb from "../../modules/Auth/domain/userOnDb.interface";
 
 type NavLink = {
   title: string;
@@ -29,6 +32,19 @@ export default function MainMenu({ navArrayLinks }: Readonly<NavbarProps>) {
 
   const handleButtonClick = (title: string) => {
     setActiveButton(title);
+  };
+  const handleLogin = async () => {
+    const githbAuthPort = new GithubAuthPort();
+    let userData = await githbAuthPort.handleSignInWithGitHub();
+    if (userData?.email) {
+      const loginPort = new LoginPort();
+      let userCourse = await loginPort.userHasAnAcount(userData.email);
+      if (userCourse) {
+        console.log("Valid User");
+      } else {
+        console.log("Invalid User");
+      }
+    }
   };
 
   return (
@@ -64,8 +80,7 @@ export default function MainMenu({ navArrayLinks }: Readonly<NavbarProps>) {
               </Button>
             ))}
             <Button
-              component={NavLink}
-              to="/login"
+              onClick={handleLogin}
               variant="contained"
               sx={{ marginLeft: "18px" }}
             >
