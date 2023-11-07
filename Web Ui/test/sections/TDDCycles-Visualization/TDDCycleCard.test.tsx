35f2b9fd7e27494544100c44a2cd5703277802f9
Ogreaggressive <nicolascari.rz@gmail.com>
2023-11-06 23:37:57 -0400

    fixing 3 of the 4 test casses broken

--- Web Ui/test/sections/TDDCycles-Visualization/TDDCycleCard.test.tsx
+++ Web Ui/test/sections/TDDCycles-Visualization/TDDCycleCard.test.tsx
@@ -1,41 +1,46 @@
 import { render, fireEvent } from '@testing-library/react';
 import TDDCycleCard from '../../../src/sections/TDDCycles-Visualization/components/TDDCycleCard';
-import {  mockCommitData } from './__mocks__/dataTypeMocks/commitData';
-import { mockSuccessJobData,mockFailedJobData } from './__mocks__/dataTypeMocks/jobData';
+import { mockCommitData } from './__mocks__/dataTypeMocks/commitData';
+import { mockSuccessJobData, mockFailedJobData } from './__mocks__/dataTypeMocks/jobData';
 import '@testing-library/jest-dom';
 
-
 describe('CycleCard component', () => {
-    
   it('renders the commit message', () => {
-    const { getByText } = render(<TDDCycleCard commit={mockCommitData} jobs={null}/>);
+    const { getByText } = render(<TDDCycleCard commit={mockCommitData} jobs={null} />);
     const commitMessageElement = getByText('Commit Commit Mock commit message');
-    
+
     expect(commitMessageElement).toBeInTheDocument();
   });
 
   it('renders the commit stats', () => {
     const { getByText } = render(<TDDCycleCard commit={mockCommitData} jobs={mockSuccessJobData} />);
-    const statsMessageElement = getByText('Total: 5 Adiciones: 4 Sustraccion: 1');
-    expect(statsMessageElement).toBeInTheDocument();
-});
+  
+    const totalText = getByText(/Total: 5/);
+    const additionsText = getByText(/Adiciones: 4/);
+    const deletionsText = getByText(/Sustracción: 1/);
+    const coverageText = getByText(/Cobertura: 80%/);
 
+  expect(totalText).toBeInTheDocument();
+  expect(additionsText).toBeInTheDocument();
+  expect(deletionsText).toBeInTheDocument();
+  expect(coverageText).toBeInTheDocument();
+  });
 
   it('renders the card as success', () => {
-    const { getByText } = render(<TDDCycleCard commit={mockCommitData} jobs={mockSuccessJobData}/>);
-    const commitMessageElement = getByText('Actions:success');
-    
+    const { getByText } = render(<TDDCycleCard commit={mockCommitData} jobs={mockSuccessJobData} />);
+    const commitMessageElement = getByText('Actions: success');
+
     expect(commitMessageElement).toBeInTheDocument();
   });
 
   it('renders the card as failed', () => {
-    const { getByText } = render(<TDDCycleCard commit={mockCommitData} jobs={mockFailedJobData}/>);
-    const commitMessageElement = getByText('Actions:failure');
+    const { getByText } = render(<TDDCycleCard commit={mockCommitData} jobs={mockFailedJobData} />);
+    const commitMessageElement = getByText('Actions: failure');
     expect(commitMessageElement).toBeInTheDocument();
   });
 
   it('renders the card with no actions', () => {
-    const { getByText } = render(<TDDCycleCard commit={mockCommitData} jobs={null}/>);
+    const { getByText } = render(<TDDCycleCard commit={mockCommitData} jobs={null} />);
     const commitMessageElement = getByText('Actions werent Found');
     expect(commitMessageElement).toBeInTheDocument();
   });
@@ -44,18 +49,15 @@ describe('CycleCard component', () => {
     const { getByText } = render(<TDDCycleCard commit={mockCommitData} jobs={mockSuccessJobData} />);
     const verCommitButton = getByText('Ver commit');
     expect(verCommitButton).toBeInTheDocument();
-    
-    const mockWindowOpen = jest.fn();
-    Object.defineProperty(window, 'open', {
-      value: mockWindowOpen,
-      writable: true,
-    });
-  
+
+    const originalOpen = window.open;
+    window.open = jest.fn();
+
     fireEvent.click(verCommitButton);
 
     setTimeout(() => {
-      expect(setTimeout).toHaveBeenCalledWith(expect.any(expect(mockWindowOpen).toHaveBeenCalledWith(mockCommitData.html_url, 'mockHtmlUrl')), 3000);
-      expect(mockWindowOpen).toHaveBeenCalled();
+      expect(window.open).toHaveBeenCalledWith(mockCommitData.html_url, '_blank');
+      window.open = originalOpen; // Restore the original window.open
     }, 3000);
   });
 });
