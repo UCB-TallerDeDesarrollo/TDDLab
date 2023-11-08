a97a8785dfeb7ee627c276a41663d44fb6ba0c25
Rene <ddcrene@gmail.com>
2023-11-08 16:35:52 -0400

    New invitation style

--- Web Ui/src/sections/Invitation/components/SuccessfulSignUpPopUp.tsx
+++ Web Ui/src/sections/Invitation/components/SuccessfulSignUpPopUp.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import DialogContentText from '@mui/material/DialogContentText';
 import PopUp from './PopUp';
+import { Card, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 
 interface GithubAccountCredentials{
     photoAccount:string | null;
@@ -16,12 +16,35 @@ function SuccessfulSignUpPopUp({ photoAccount,nameAccount}: Readonly<GithubAccou
   };
 
   const dialogContent:any = (
-    <>
-        <DialogContentText>
-            {nameAccount ?? 'Usuario'}
-        </DialogContentText>
-        <img src={photoAccount ?? 'URL_POR_DEFECTO'} alt={nameAccount ?? 'Usuario'} />
-    </>
+    <Card sx={{
+        width: 400, // Establece un ancho fijo para el primer Card
+        '&:hover': { boxShadow: 'md', borderColor: 'neutral.outlinedHoverBorder' },
+      }} variant="outlined">
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item xs={4}>
+              <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+                <div style={{ width: 100, height: 100, borderRadius: '10%', overflow: 'hidden' }}>
+                  <CardMedia
+                    component="img"
+                    alt="Imagen"
+                    height="100%"
+                    width="100%"
+                    image={photoAccount ?? 'URL_POR_DEFECTO'} // Reemplaza con la ruta de tu imagen.
+                  />
+                </div>
+              </div>
+            </Grid>
+            <Grid item xs={8} container direction="column" justifyContent="space-between">
+              <Grid item>
+                <Typography variant="h5" sx={{ marginBottom: 1 }}>
+                  {nameAccount ?? 'Usuario'}
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
   );
 
   return (
