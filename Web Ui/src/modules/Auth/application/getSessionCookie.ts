e979ff11c4d445c5f800ff60e23e8fa8e09a1280
Ogreaggressive <nicolascari.rz@gmail.com>
2023-11-19 18:44:16 -0400

    cookies test now work

--- Web Ui/src/modules/Auth/application/getSessionCookie.ts
+++ Web Ui/src/modules/Auth/application/getSessionCookie.ts
@@ -3,8 +3,6 @@ import Cookies from 'js-cookie';
 export const setSessionCookie = (userData: any) => {
   try {
     Cookies.set('userSession', JSON.stringify(userData), { expires: 1 });
-    console.log('Session cookie set successfully.');
-    console.log(userData);
   } catch (error) {
     console.error('Error setting session cookie:', error);
   }
--- Web Ui/test/sections/Auth/GithubCookies.test.tsx
+++ Web Ui/test/sections/Auth/GithubCookies.test.tsx
@@ -1,10 +1,9 @@
-
 import Cookies from 'js-cookie';
 import '@testing-library/jest-dom';
 import { setSessionCookie } from '../../../src/modules/Auth/application/getSessionCookie';
 import { getSessionCookie } from '../../../src/modules/Auth/application/setSessionCookie';
 import { removeSessionCookie } from '../../../src/modules/Auth/application/deleteSessionCookie';
-import {cookieUserData} from './__mocks__/cookieData';
+import { cookieUserData } from './__mocks__/cookieData';
 
 jest.mock('js-cookie', () => ({
   set: jest.fn(),
@@ -14,34 +13,39 @@ jest.mock('js-cookie', () => ({
 
 describe('setSessionCookie', () => {
   it('sets the session cookie successfully', () => {
-    const userData = {cookieUserData};
+    const userData = { cookieUserData };
     setSessionCookie(userData);
 
     expect(Cookies.set).toHaveBeenCalledWith('userSession', JSON.stringify(userData), { expires: 1 });
-    expect(console.log).toHaveBeenCalledWith('Session cookie set successfully.');
-    expect(console.log).toHaveBeenCalledWith(userData);
   });
 
   it('logs an error if setting the session cookie fails', () => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+
     (Cookies.set as jest.Mock).mockImplementationOnce(() => {
       throw new Error('Test error');
     });
 
     setSessionCookie({});
 
-    expect(console.error).toHaveBeenCalledWith('Error setting session cookie:', expect.any(Error));
+    expect(Cookies.set).toHaveBeenCalledWith('userSession', JSON.stringify({}), { expires: 1 });
+    expect(console.error).toHaveBeenCalledTimes(1);
+    expect(console.error).toHaveBeenNthCalledWith(
+        1,
+        expect.stringContaining('Error setting session cookie:'),
+        expect.any(Error)
+      );
   });
 });
 
 describe('getSessionCookie', () => {
   it('retrieves the session cookie successfully', () => {
-    const userData = {cookieUserData};
+    const userData = { cookieUserData };
     (Cookies.get as jest.Mock).mockReturnValueOnce(JSON.stringify(userData));
 
     const result = getSessionCookie();
 
     expect(Cookies.get).toHaveBeenCalledWith('userSession');
-    expect(console.log).toHaveBeenCalledWith('Session cookie retrieved successfully:', userData);
     expect(result).toEqual(userData);
   });
 
@@ -51,19 +55,30 @@ describe('getSessionCookie', () => {
     const result = getSessionCookie();
 
     expect(Cookies.get).toHaveBeenCalledWith('userSession');
-    expect(console.log).toHaveBeenCalledWith('Session cookie retrieved successfully:', null);
     expect(result).toBeNull();
   });
 
   it('logs an error if retrieving the session cookie fails', () => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+
     (Cookies.get as jest.Mock).mockImplementationOnce(() => {
       throw new Error('Test error');
     });
 
     const result = getSessionCookie();
 
-    expect(console.error).toHaveBeenCalledWith('Error retrieving session cookie:', expect.any(Error));
     expect(result).toBeNull();
+    expect(console.error).toHaveBeenCalledTimes(2);
+    expect(console.error).toHaveBeenNthCalledWith(
+        1,
+        expect.stringContaining('Error setting session cookie:'),
+        expect.any(Error)
+      );
+      expect(console.error).toHaveBeenNthCalledWith(
+        2,
+        expect.stringContaining('Error retrieving session cookie:'),
+        expect.any(Error)
+      );
   });
 });
 
@@ -72,16 +87,35 @@ describe('removeSessionCookie', () => {
     removeSessionCookie();
 
     expect(Cookies.remove).toHaveBeenCalledWith('userSession');
-    expect(console.log).toHaveBeenCalledWith('Session cookie removed successfully.');
   });
 
   it('logs an error if removing the session cookie fails', () => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  
     (Cookies.remove as jest.Mock).mockImplementationOnce(() => {
       throw new Error('Test error');
     });
-
+  
     removeSessionCookie();
-
-    expect(console.error).toHaveBeenCalledWith('Error removing session cookie:', expect.any(Error));
+  
+    expect(Cookies.remove).toHaveBeenCalledWith('userSession');
+    expect(console.error).toHaveBeenCalledTimes(3);
+  
+    expect(console.error).toHaveBeenNthCalledWith(
+      1,
+      expect.stringContaining('Error setting session cookie:'),
+      expect.any(Error)
+    );
+    expect(console.error).toHaveBeenNthCalledWith(
+      2,
+      expect.stringContaining('Error retrieving session cookie:'),
+      expect.any(Error)
+    );
+    expect(console.error).toHaveBeenNthCalledWith(
+      3,
+      expect.stringContaining('Error removing session cookie:'),
+      expect.any(Error)
+    );
   });
+  
 });
