d8dbd37d9705d3c5c550edf29f448edd86a5c5bc
BMB <bruno.marquez@ucb.edu.bo>
2023-11-19 22:47:56 -0400

    Added new module TDDCycle with basic folder structure

---
+++ server/src/modules/TDDCycles/Domain/CommitDataObject.ts
@@ -0,0 +1,18 @@
+export interface CommitDTO {
+  html_url: string;
+  stats: Stats;
+  commit: Commit;
+  sha: string;
+  coverage: string;
+}
+export interface Stats {
+  total: number;
+  additions: number;
+  deletions: number;
+}
+export interface Commit {
+  date: Date;
+  message: string;
+  url: string;
+  comment_count: number;
+}
