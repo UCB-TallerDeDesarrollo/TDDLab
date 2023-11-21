0d5741a799b91074390f2ae479d2d1f78cafd323
BMB <bruno.marquez@ucb.edu.bo>
2023-11-20 22:13:20 -0400

    Refactor TDDCyclesController and use GithubRepository in CommitsUseCase and JobsUseCase

--- server/src/routes/TDDCyclesRoutes.ts
+++ server/src/routes/TDDCyclesRoutes.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { CommitRepository } from "../modules/TDDCycles/Repositories/TDDCyclesCommitsRepository";
 import { JobRepository } from "../modules/TDDCycles/Repositories/TDDCyclesJobsRepository";
+import { GithubRepository } from "../modules/TDDCycles/Repositories/TDDCyclesGithubRepository";
 import TDDCyclesController from "../controllers/TDDCycles/TDDCyclesController";
 
 const commitsRepository = new CommitRepository();
 const jobsRepository = new JobRepository();
+const githubRepository = new GithubRepository();
 
-const _TDDCyclesController = new TDDCyclesController(jobsRepository, commitsRepository);
+
+const _TDDCyclesController = new TDDCyclesController(commitsRepository, jobsRepository, githubRepository);
 
 const TDDCycles = express.Router();
 
