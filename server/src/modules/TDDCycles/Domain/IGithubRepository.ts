5a83b9a6bcd9ef6a1716ad8fbccb97363c08baa8
BMB <bruno.marquez@ucb.edu.bo>
2023-11-22 20:20:50 -0400

    Add IGithubRepository interface and implement it in GithubRepository class

---
+++ server/src/modules/TDDCycles/Domain/IGithubRepository.ts
@@ -0,0 +1,9 @@
+import { CommitDataObject, CommitInformationDataObject } from "./commitInterfaces";
+
+export interface IGithubRepository {
+    getCommits(owner: string, repoName: string): Promise<CommitDataObject[]>;
+    getCommitInfoForTDDCycle(owner: string, repoName: string, sha: string): Promise<CommitInformationDataObject>;
+    timeout(ms: number): Promise<void>;
+    obtainRunsOfGithubActions(owner: string, repoName: string): Promise<any>;
+    obtainJobsOfACommit(owner: string, repoName: string, jobId: number, attempt: number): Promise<any>;
+}
