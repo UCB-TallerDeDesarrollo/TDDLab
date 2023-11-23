091a614bcfbb743f6e4eedc3cf4956f22f32ac13
Christian <christianriveroarnez@gmail.com>
2023-11-23 19:11:07 -0400

    modified register port to be an use case

--- Web Ui/src/modules/Auth/application/RegisterUserOnDb.ts
+++ Web Ui/src/modules/Auth/application/RegisterUserOnDb.ts
@@ -1,7 +1,7 @@
 import UserOnDb from "../domain/userOnDb.interface";
 import AuthRepository from "../repository/LoginRepository";
 
-export class RegisterPort {
+export class RegisterUserOnDb {
   adapter: AuthRepository;
   constructor(loginRepository: AuthRepository = new AuthRepository()) {
     this.adapter = loginRepository;
