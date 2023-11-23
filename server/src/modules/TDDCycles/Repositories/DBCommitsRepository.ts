b2354d217be13d2c711a76d431d19a879a90604d
BMB <bruno.marquez@ucb.edu.bo>
2023-11-22 21:31:13 -0400

    Refactor DBCommitsRepository to use try-catch-finally blocks for better error handling

--- server/src/modules/TDDCycles/Repositories/DBCommitsRepository.ts
+++ server/src/modules/TDDCycles/Repositories/DBCommitsRepository.ts
@@ -10,51 +10,75 @@ export class DBCommitRepository implements ICommitRepository {
   }
   async saveCommit(owner: string, repoName: string, commit: CommitDTO) {
     const client = await this.pool.connect();
-    const query =
-      "INSERT INTO commitsTable (owner, repoName, html_url, sha, total,additions,deletions,message,url,comment_count,commit_date, coverage) VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10,$11, $12)";
-    const values = [
-      owner,
-      repoName,
-      commit.html_url,
-      commit.sha,
-      commit.stats.total,
-      commit.stats.additions,
-      commit.stats.deletions,
-      commit.commit.message,
-      commit.commit.url,
-      commit.commit.comment_count,
-      commit.commit.date,
-      commit.coverage,
-    ];
-    const result = await client.query(query, values);
-    client.release();
-    return result.rows;
+    try {
+      const query =
+        "INSERT INTO commitsTable (owner, repoName, html_url, sha, total,additions,deletions,message,url,comment_count,commit_date, coverage) VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10,$11, $12)";
+      const values = [
+        owner,
+        repoName,
+        commit.html_url,
+        commit.sha,
+        commit.stats.total,
+        commit.stats.additions,
+        commit.stats.deletions,
+        commit.commit.message,
+        commit.commit.url,
+        commit.commit.comment_count,
+        commit.commit.date,
+        commit.coverage,
+      ];
+      const result = await client.query(query, values);
+      return result.rows;
+    } catch (error) {
+      console.error("Error saving commit to the database:", error);
+      throw error;
+    } finally {
+      client.release();
+    }
   }
   async getCommits(owner: string, repoName: string) {
     const client = await this.pool.connect();
-    const query =
-      "SELECT * FROM commitsTable WHERE owner = $1 AND reponame = $2 ORDER BY commit_date DESC";
-    const values = [owner, repoName];
-    const result = await client.query(query, values);
-    client.release();
-    return result.rows;
+    try {
+      const query =
+        "SELECT * FROM commitsTable WHERE owner = $1 AND reponame = $2 ORDER BY commit_date DESC";
+      const values = [owner, repoName];
+      const result = await client.query(query, values);
+      return result.rows;
+    } catch (error) {
+      console.error("Error getting commits from the database:", error);
+      throw error;
+    } finally {
+      client.release();
+    }
   }
   async commitExists(owner: string, repoName: string, sha: string) {
     const client = await this.pool.connect();
-    const query =
-      "SELECT * FROM commitstable WHERE owner = $1 AND reponame = $2 AND sha=$3";
-    const values = [owner, repoName, sha];
-    const result = await client.query(query, values);
-    client.release();
-    return result.rows;
+    try {
+      const query =
+        "SELECT * FROM commitstable WHERE owner = $1 AND reponame = $2 AND sha=$3";
+      const values = [owner, repoName, sha];
+      const result = await client.query(query, values);
+      return result.rows;
+    } catch (error) {
+      console.error("Error checking if commit exists in the database:", error);
+      throw error;
+    } finally {
+      client.release();
+    }
   }
   async repositoryExists(owner: string, repoName: string) {
     const client = await this.pool.connect();
-    const query =
-      "SELECT COUNT(*) FROM commitstable WHERE owner = $1 AND reponame = $2";
-    const values = [owner, repoName];
-    const result = await client.query(query, values);
-    client.release();
-    return result.rows[0].count > 0;
+    try {
+      const query =
+        "SELECT COUNT(*) FROM commitstable WHERE owner = $1 AND reponame = $2";
+      const values = [owner, repoName];
+      const result = await client.query(query, values);
+      return result.rows[0].count > 0;
+    } catch (error) {
+      console.error("Error checking if repository exists in the database:", error);
+      throw error;
+    } finally {
+      client.release();
+    }
   }
 }
