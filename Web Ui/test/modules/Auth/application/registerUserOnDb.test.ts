6f6bee220c3adaf93fa74086cb46f87bc1d0e119
Christian <christianriveroarnez@gmail.com>
2023-11-23 19:44:02 -0400

    added a test in the user registration use case

---
+++ Web Ui/test/modules/Auth/application/registerUserOnDb.test.ts
@@ -0,0 +1,32 @@
+import { RegisterUserOnDb } from "../../../../src/modules/Auth/application/registerUserOnDb";
+import UserOnDb from "../../../../src/modules/Auth/domain/userOnDb.interface";
+import AuthRepository from "../../../../src/modules/Auth/repository/LoginRepository";
+
+// Mock the AuthRepository
+jest.mock("../../../../src/modules/Auth/repository/LoginRepository", () => {
+  return jest.fn().mockImplementation(() => ({
+    registerAccount: jest.fn(),
+  }));
+});
+
+describe("RegisterUserOnDb class", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should register a user successfully", async () => {
+    const mockUser: UserOnDb = {
+      email: "test@example.com",
+      course: "2",
+    };
+
+    const mockRepository = new AuthRepository() as jest.Mocked<AuthRepository>;
+    mockRepository.registerAccount.mockResolvedValue(undefined);
+
+    const registerUser = new RegisterUserOnDb(mockRepository);
+    await registerUser.register(mockUser);
+
+    expect(mockRepository.registerAccount).toHaveBeenCalledTimes(1);
+    expect(mockRepository.registerAccount).toHaveBeenCalledWith(mockUser);
+  });
+});
