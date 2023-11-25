432554bf114eb303d627d2a739274baa257bc70e
BMB <bruno.marquez@ucb.edu.bo>
2023-11-25 01:47:34 -0400

    Refactor jobRepository to dbJobRepository in GetTestResultsUseCase constructor

--- server/src/modules/TDDCycles/Repositories/GithubRepository.ts
+++ server/src/modules/TDDCycles/Repositories/GithubRepository.ts
@@ -1,12 +1,11 @@
 import { Octokit } from "octokit";
 import { IGithubRepository } from "../Domain/IGithubRepository";
-import {
-  CommitDataObject,
-  CommitInformationDataObject,
-} from "../Domain/commitInterfaces";
-import dotenv from "dotenv"; // Import 'dotenv' as a module
+import { CommitDataObject, CommitInformationDataObject } from "../Domain/commitInterfaces";
+import dotenv from "dotenv";
+import { JobDataObject } from "../Domain/jobInterfaces";
+
 dotenv.config();
-export class GithubRepository implements IGithubRepository{
+export class GithubRepository implements IGithubRepository {
   octokit: Octokit;
   constructor() {
     const { REACT_APP_AUTH_TOKEN } = process.env;
@@ -38,48 +37,48 @@ export class GithubRepository implements IGithubRepository{
             comments_url: githubCommit.comments_url,
             author: githubCommit.author
               ? {
-                  login: githubCommit.author.login,
-                  id: githubCommit.author.id,
-                  node_id: githubCommit.author.node_id,
-                  avatar_url: githubCommit.author.avatar_url,
-                  gravatar_id: githubCommit.author.gravatar_id,
-                  url: githubCommit.author.url,
-                  html_url: githubCommit.author.html_url,
-                  followers_url: githubCommit.author.followers_url,
-                  following_url: githubCommit.author.following_url,
-                  gists_url: githubCommit.author.gists_url,
-                  starred_url: githubCommit.author.starred_url,
-                  subscriptions_url: githubCommit.author.subscriptions_url,
-                  organizations_url: githubCommit.author.organizations_url,
-                  repos_url: githubCommit.author.repos_url,
-                  events_url: githubCommit.author.events_url,
-                  received_events_url: githubCommit.author.received_events_url,
-                  type: githubCommit.author.type,
-                  site_admin: githubCommit.author.site_admin,
-                }
+                login: githubCommit.author.login,
+                id: githubCommit.author.id,
+                node_id: githubCommit.author.node_id,
+                avatar_url: githubCommit.author.avatar_url,
+                gravatar_id: githubCommit.author.gravatar_id,
+                url: githubCommit.author.url,
+                html_url: githubCommit.author.html_url,
+                followers_url: githubCommit.author.followers_url,
+                following_url: githubCommit.author.following_url,
+                gists_url: githubCommit.author.gists_url,
+                starred_url: githubCommit.author.starred_url,
+                subscriptions_url: githubCommit.author.subscriptions_url,
+                organizations_url: githubCommit.author.organizations_url,
+                repos_url: githubCommit.author.repos_url,
+                events_url: githubCommit.author.events_url,
+                received_events_url: githubCommit.author.received_events_url,
+                type: githubCommit.author.type,
+                site_admin: githubCommit.author.site_admin,
+              }
               : null,
             committer: githubCommit.committer
               ? {
-                  login: githubCommit.committer.login,
-                  id: githubCommit.committer.id,
-                  node_id: githubCommit.committer.node_id,
-                  avatar_url: githubCommit.committer.avatar_url,
-                  gravatar_id: githubCommit.committer.gravatar_id,
-                  url: githubCommit.committer.url,
-                  html_url: githubCommit.committer.html_url,
-                  followers_url: githubCommit.committer.followers_url,
-                  following_url: githubCommit.committer.following_url,
-                  gists_url: githubCommit.committer.gists_url,
-                  starred_url: githubCommit.committer.starred_url,
-                  subscriptions_url: githubCommit.committer.subscriptions_url,
-                  organizations_url: githubCommit.committer.organizations_url,
-                  repos_url: githubCommit.committer.repos_url,
-                  events_url: githubCommit.committer.events_url,
-                  received_events_url:
-                    githubCommit.committer.received_events_url,
-                  type: githubCommit.committer.type,
-                  site_admin: githubCommit.committer.site_admin,
-                }
+                login: githubCommit.committer.login,
+                id: githubCommit.committer.id,
+                node_id: githubCommit.committer.node_id,
+                avatar_url: githubCommit.committer.avatar_url,
+                gravatar_id: githubCommit.committer.gravatar_id,
+                url: githubCommit.committer.url,
+                html_url: githubCommit.committer.html_url,
+                followers_url: githubCommit.committer.followers_url,
+                following_url: githubCommit.committer.following_url,
+                gists_url: githubCommit.committer.gists_url,
+                starred_url: githubCommit.committer.starred_url,
+                subscriptions_url: githubCommit.committer.subscriptions_url,
+                organizations_url: githubCommit.committer.organizations_url,
+                repos_url: githubCommit.committer.repos_url,
+                events_url: githubCommit.committer.events_url,
+                received_events_url:
+                  githubCommit.committer.received_events_url,
+                type: githubCommit.committer.type,
+                site_admin: githubCommit.committer.site_admin,
+              }
               : null,
             parents: githubCommit.parents.map((parent: any) => {
               return {
@@ -162,16 +161,16 @@ export class GithubRepository implements IGithubRepository{
   }
   async obtainRunsOfGithubActions(owner: string, repoName: string) {
     try {
-        const response : any = await Promise.race([
-            this.octokit.request(`GET /repos/${owner}/${repoName}/actions/runs`),
-            this.timeout(10000),
-        ]);
-        return response;
+      const response: any = await Promise.race([
+        this.octokit.request(`GET /repos/${owner}/${repoName}/actions/runs`),
+        this.timeout(10000),
+      ]);
+      return response;
     } catch (error) {
-        console.error("Error obtaining runs:", error);
-        throw error;
+      console.error("Error obtaining runs:", error);
+      throw error;
     }
-}
+  }
   async obtainJobsOfACommit(
     owner: string,
     repoName: string,
@@ -202,5 +201,24 @@ export class GithubRepository implements IGithubRepository{
         return [workFlowRun.head_commit.id, workFlowRun.id];
       });
     return commitsWithActions;
+  }
+  async getJobsDataFromGithub(
+    owner: string,
+    repoName: string,
+    listOfCommitsWithActions: [string, number][]
+  ) {
+    const jobs: Record<string, JobDataObject> = {};
+    await Promise.all(
+      listOfCommitsWithActions.map(async (workflowInfo) => {
+        const jobInfo = await this.obtainJobsOfACommit(
+          owner,
+          repoName,
+          workflowInfo[1],
+          1
+        );
+        jobs[workflowInfo[0]] = jobInfo;
+      })
+    );
+    return jobs;
   }
 }
