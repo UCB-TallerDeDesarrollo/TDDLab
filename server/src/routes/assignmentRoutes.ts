a2d5993cb51a8014c55b2ae45072339e48a6737b
Gabriel Ichiro <gabriel582084@gmail.com>
2023-11-25 16:32:59 -0400

    saved the world

--- server/src/routes/assignmentRoutes.ts
+++ server/src/routes/assignmentRoutes.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import AssignmentController from "../controllers/assignments/assignmentController"; // Import your controller class
+import AssignmentsController from "../controllers/assignments/assignmentController"; // Import your controller class
 import AssignmentRepository from "../modules/Assignments/repositories/AssignmentRepository";
 const repository = new AssignmentRepository(); // Create an instance of your repository
-const assignmentController = new AssignmentController(repository); // Pass the repository instance to the controller
+const assignmentController = new AssignmentsController(repository); // Pass the repository instance to the controller
 
 const assignmentsRouter = express.Router();
 
