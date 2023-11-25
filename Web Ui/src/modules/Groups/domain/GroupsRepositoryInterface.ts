21e19c3c63e9ad56087eccd4d858f4bd7753aed5
Anaelise Huaniri <aanesita@gmail.com>
2023-11-25 03:07:31 -0400

    added groups repository

--- Web Ui/src/modules/Groups/domain/GroupsRepositoryInterface.ts
+++ Web Ui/src/modules/Groups/domain/GroupsRepositoryInterface.ts
@@ -4,8 +4,6 @@ interface GroupsRepositoryInterface {
   getGroups(): Promise<GroupDataObject[]>;
   getGroupById(id: number): Promise<GroupDataObject | null>;
   createGroup(groupData: GroupDataObject): Promise<void>;
-  updateGroup(id: number, groupData: GroupDataObject): Promise<void>;
-  deleteGroup(id: number): Promise<void>;
 }
 
 export default GroupsRepositoryInterface;
---
+++ Web Ui/src/modules/Groups/repository/GroupsRepository.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { GroupDataObject } from '../domain/GroupInterface';
+import GroupsRepositoryInterface from '../domain/GroupsRepositoryInterface';
+
+const API_URL = "https://tdd-lab-api-gold.vercel.app/api/groups"; 
+
+class GroupsRepository implements GroupsRepositoryInterface {
+  async getGroups(): Promise<GroupDataObject[]> {
+    try {
+      const response = await axios.get(API_URL);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching groups:', error);
+      throw error;
+    }
+  }
+
+  async getGroupById(id: number): Promise<GroupDataObject | null> {
+    try {
+      const response = await axios.get(`${API_URL}/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching group by ID:', error);
+      throw error;
+    }
+  }
+
+  async createGroup(groupData: GroupDataObject): Promise<void> {
+    try {
+      await axios.post(API_URL, groupData);
+    } catch (error) {
+      console.error('Error creating group:', error);
+      throw error;
+    }
+  }
+}
+
+export default GroupsRepository;
