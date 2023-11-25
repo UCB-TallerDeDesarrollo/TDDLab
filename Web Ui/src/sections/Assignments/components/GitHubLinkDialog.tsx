034cdded02a97045a276c4e22e4d7b7bd85c789d
Rodrigo Guardia <rodrigo.guardia316@gmail.com>
2023-11-25 18:05:59 -0400

    Deleted code smell

--- Web Ui/src/sections/Assignments/components/GitHubLinkDialog.tsx
+++ Web Ui/src/sections/Assignments/components/GitHubLinkDialog.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
