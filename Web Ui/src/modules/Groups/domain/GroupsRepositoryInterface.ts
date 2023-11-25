0baa025b969dc8e821520b5eb30afeb38b31ee62
Anaelise Huaniri <aanesita@gmail.com>
2023-11-25 02:14:47 -0400

    Group interfaces in domain have been created

---
+++ Web Ui/src/modules/Groups/domain/GroupsRepositoryInterface.ts
@@ -0,0 +1,11 @@
+import { GroupDataObject } from './GroupInterface';
+
+interface GroupsRepositoryInterface {
+  getGroups(): Promise<GroupDataObject[]>;
+  getGroupById(id: number): Promise<GroupDataObject | null>;
+  createGroup(groupData: GroupDataObject): Promise<void>;
+  updateGroup(id: number, groupData: GroupDataObject): Promise<void>;
+  deleteGroup(id: number): Promise<void>;
+}
+
+export default GroupsRepositoryInterface;
