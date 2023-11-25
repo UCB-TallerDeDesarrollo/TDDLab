298226be622692d0b9c3e7555cc6f3fba96f613a
lufercho <luisfernandogilgareca@gmail.com>
2023-11-25 15:42:43 -0400

    GithubAuthPort.test.ts was succesfully created

---
+++ Web Ui/test/modules/auth/GithubAuthPort.test.ts
@@ -0,0 +1,32 @@
+import { GithubAuthPort } from "../../../src/modules/Auth/application/GithubAuthPort";
+import { AssignmentDataObject } from "../../../../src/modules/Assignments/domain/assignmentInterfaces";
+import { MockAssignmentsRepository } from "../../__mocks__/assignments/mockAssignmentsRepository";
+
+let mockRepository: MockAssignmentsRepository;
+let getAssignment: GithubAuthPort;
+/*
+beforeEach(() => {
+  mockRepository = new MockAssignmentsRepository();
+  getAssignment = new GetAssignments(mockRepository);
+});
+
+describe("Get assignments", () => {
+    it("Should successfully obtain assignments list", async () => {
+      const assignmentId = 1;
+      const assignment: AssignmentDataObject = {
+        id: assignmentId,
+        title: "Tarea 1",
+        description: "Esta es la primera tarea",
+        start_date: new Date("2023-01-01"),
+        end_date: new Date("2023-01-10"),
+        state: "inProgress",
+        link: "Enlace",
+        comment: "Comentario",
+      };
+      mockRepository.createAssignment(assignment);
+      const obtainedAssignment = await getAssignment.obtainAllAssignments();
+      console.log(obtainedAssignment);
+      expect(obtainedAssignment).toEqual([assignment]);
+    });
+  });
+  */
