0cde0c54767460f7f9579fd30bf409bc52708463
BMB <bruno.marquez@ucb.edu.bo>
2023-11-27 16:26:18 -0400

    Add test for saving a job in DBJobsRepository

---
+++ server/test/modules/TDDCycles/Repositories/DBJobsRepository.test.ts
@@ -0,0 +1,30 @@
+import { DBJobsRepository } from '../../../../src/modules/TDDCycles/Repositories/DBJobsRepository';
+import { Pool } from "pg";
+
+let repository: DBJobsRepository;
+let poolConnectMock: jest.Mock;
+let clientQueryMock: jest.Mock;
+
+beforeEach(() => {
+    poolConnectMock = jest.fn();
+    clientQueryMock = jest.fn();
+    poolConnectMock.mockResolvedValue({
+        query: clientQueryMock,
+        release: jest.fn(),
+    });
+    jest.spyOn(Pool.prototype, "connect").mockImplementation(poolConnectMock);
+    repository = new DBJobsRepository();
+});
+
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
+
+describe("Obtain assignments", () => {
+    it("should save a job", async () => {
+        await repository.saveJob({ id: 1, sha: 'sha', owner: 'owner', reponame: 'repo', conclusion: 'success' });
+        expect(poolConnectMock).toBeCalledTimes(1);
+        expect(clientQueryMock).toBeCalledWith('INSERT INTO jobsTable (id, sha, owner, repoName, conclusion) VALUES ($1, $2, $3, $4, $5)', [1, 'sha', 'owner', 'repo', 'success']);
+    });
+});
