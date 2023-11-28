27a7e2b8ec69a275b9ca4fafc4760fa10b7c2aa2
luferchoo <luisfernandogilgareca@gmail.com>
2023-11-28 11:50:28 -0400

    some issues resolved (refactors)

--- server/test/modules/groups/application/createGroupUseCase.test.ts
+++ server/test/modules/groups/application/createGroupUseCase.test.ts
@@ -1,7 +1,6 @@
 import CreateGroupUseCase from '../../../../src/modules/Groups/application/GroupUseCases/createGroupUseCase';
 import GroupRepository from '../../../../src/modules/Groups/repositories/GroupRepository';
 
-// Mock the GroupRepository to isolate the tests
 jest.mock('../../../../src/modules/Groups/repositories/GroupRepository');
 
 describe('CreateGroupUseCase', () => {
@@ -14,31 +13,19 @@ describe('CreateGroupUseCase', () => {
   });
 
   it('should create a new group successfully', async () => {
-    // Arrange
     const groupDetail = 'Test Group';
     const createGroupDTO = { groupDetail };
-
-    // Mock the implementation of createGroup in GroupRepository
     groupRepository.createGroup.mockResolvedValue({ id: 1, ...createGroupDTO });
-
-    // Act
     const result = await createGroupUseCase.execute(createGroupDTO);
-
-    // Assert
     expect(result.id).toBeDefined();
     expect(result.groupDetail).toBe(createGroupDTO.groupDetail);
   });
 
   it('should handle errors when creating a group', async () => {
-    // Arrange
     const groupDetail = 'Test Group';
     const createGroupDTO = { groupDetail };
     const errorMessage = 'Failed to create group';
-
-    // Mock the implementation of createGroup in GroupRepository to simulate an error
     groupRepository.createGroup.mockRejectedValue(new Error(errorMessage));
-
-    // Act and Assert
     await expect(createGroupUseCase.execute(createGroupDTO)).rejects.toThrowError(errorMessage);
   });
 });
