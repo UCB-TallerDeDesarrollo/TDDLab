810ac98b7dba96e232522100b835febb9aa255a7
Christian Rivero <christianriveroarnez@gmail.com>
2023-11-28 17:27:05 -0400

    fixed expiration time of the cookie

--- Web Ui/src/modules/Auth/application/setSessionCookie.ts
+++ Web Ui/src/modules/Auth/application/setSessionCookie.ts
@@ -1,9 +1,9 @@
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 export const setSessionCookie = (userData: any) => {
   try {
-    Cookies.set('userSession', JSON.stringify(userData), { expires: 1 });
+    Cookies.set("userSession", JSON.stringify(userData), { expires: 30 });
   } catch (error) {
-    console.error('Error setting session cookie:', error);
+    console.error("Error setting session cookie:", error);
   }
 };
