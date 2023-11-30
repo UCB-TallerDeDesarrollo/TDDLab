b100c4801a7fb0e22a28731a94658362397984ed
BMB <bruno.marquez@ucb.edu.bo>
2023-11-29 23:02:24 -0400

    duplication aaa

--- server/test/__mocks__/TDDCycles/dataTypeMocks/commitData.ts
+++ server/test/__mocks__/TDDCycles/dataTypeMocks/commitData.ts
@@ -55,38 +55,4 @@ export const tddCycleDataObjectMock: TDDCycleDataObject = {
     test_count: '50'
 };
 
-export const unsavedCommits: CommitDataObject[] = [{
-    sha: 'abc123',
-    node_id: 'node1',
-    commit: {
-        message: 'test commit',
-        author: {
-            name: 'John Doe',
-            email: 'johndoe@example.com',
-            date: new Date('2022-01-01T00:00:00Z')
-        },
-        committer: {
-            name: 'Jane Smith',
-            email: 'janesmith@example.com',
-            date: new Date('2022-01-01T00:00:00Z')
-        },
-        tree: {
-            sha: 'tree1',
-            url: 'tree_url'
-        },
-        url: 'commit_url',
-        comment_count: 0,
-        verification: {
-            verified: false,
-            reason: 'reason',
-            signature: null,
-            payload: null
-        }
-    },
-    url: 'url',
-    html_url: 'https://github.com/user/repo/commit/abc123',
-    comments_url: 'comments_url',
-    author: null,
-    committer: null,
-    parents: []
-}];
+export const unsavedCommits: CommitDataObject[] = commitsFromGithub;
