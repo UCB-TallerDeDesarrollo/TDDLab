7e7bbdc7f9b2282a862f10cd0564a055499483db
Christian Rivero <christianriveroarnez@gmail.com>
2023-11-30 00:03:14 -0400

    deleted share link from home page

--- Web Ui/src/sections/Assignments/AssignmentsPage.tsx
+++ Web Ui/src/sections/Assignments/AssignmentsPage.tsx
@@ -1,28 +1,29 @@
 import Form from "./components/AssignmentForm";
 import Assignments from "./components/AssignmentsList";
 import InvitationComponent from "./components/EnrollmentLink";
-import { styled } from '@mui/system'; 
+import { styled } from "@mui/system";
 import { useState } from "react";
 
-const AssignmentManagerContainer = styled('section')({
-  display: 'flex',
-  width: '100%',
+const AssignmentManagerContainer = styled("section")({
+  display: "flex",
+  width: "100%",
 });
 
-const AssignmentsContainer = styled('div')({
-  flex: '1',
-  marginLeft: '16px',  
-  marginRight: '20px', 
+const AssignmentsContainer = styled("div")({
+  flex: "1",
+  marginLeft: "16px",
+  marginRight: "20px",
 });
 
-const FormsContainer = styled('div')({
-  flex: '1',
-  marginLeft: '8px',  
-  marginRight: '2px', 
-  marginTop: '68px',
+const FormsContainer = styled("div")({
+  flex: "1",
+  marginLeft: "8px",
+  marginRight: "2px",
+  marginTop: "68px",
 });
 function AssignmentManager() {
-  const [createAssignmentPopupOpen, setCreateAssignmentPopupOpen] = useState(false);
+  const [createAssignmentPopupOpen, setCreateAssignmentPopupOpen] =
+    useState(false);
 
   const handleCreateAssignmentClick = () => {
     setCreateAssignmentPopupOpen(true);
@@ -34,8 +35,13 @@ function AssignmentManager() {
         <Assignments ShowForm={handleCreateAssignmentClick} />
       </AssignmentsContainer>
       <FormsContainer>
-        {createAssignmentPopupOpen && <Form data-testid="form-container" open={createAssignmentPopupOpen} handleClose={() => setCreateAssignmentPopupOpen(false)} />}
-        <InvitationComponent data-testid="invitation-component"/>
+        {createAssignmentPopupOpen && (
+          <Form
+            data-testid="form-container"
+            open={createAssignmentPopupOpen}
+            handleClose={() => setCreateAssignmentPopupOpen(false)}
+          />
+        )}
       </FormsContainer>
     </AssignmentManagerContainer>
   );
