17b4be616a38123dda34b4661e538d2287fb2612
BMB <bruno.marquez@ucb.edu.bo>
2023-11-29 23:30:34 -0400

    duplicationhelp

--- server/test/modules/TDDCycles/Repositories/DBCommitsRepository.test.ts
+++ server/test/modules/TDDCycles/Repositories/DBCommitsRepository.test.ts
@@ -3,26 +3,26 @@ import { Pool } from "pg";
 import { DBCommitsRepository } from "../../../../src/modules/TDDCycles/Repositories/DBCommitsRepository";
 import { commitsFromGithub, tddCycleDataObjectMock, unsavedCommits } from "../../../__mocks__/TDDCycles/dataTypeMocks/commitData";
 
-let repository: DBCommitsRepository;
-let poolConnectMock: jest.Mock;
-let clientQueryMock: jest.Mock;
+let commitRepository: DBCommitsRepository;
+let poolCommitConnectMock: jest.Mock;
+let clientCommitQueryMock: jest.Mock;
 
 function testErrorHandling(methodUnderTest: () => Promise<any>) {
     it("should handle errors", async () => {
-        clientQueryMock.mockRejectedValue(new Error());
+        clientCommitQueryMock.mockRejectedValue(new Error());
         await expect(methodUnderTest()).rejects.toThrow();
     });
 }
 
 beforeEach(() => {
-    poolConnectMock = jest.fn();
-    clientQueryMock = jest.fn();
-    poolConnectMock.mockResolvedValue({
-        query: clientQueryMock,
+    poolCommitConnectMock = jest.fn();
+    clientCommitQueryMock = jest.fn();
+    poolCommitConnectMock.mockResolvedValue({
+        query: clientCommitQueryMock,
         release: jest.fn(),
     });
-    jest.spyOn(Pool.prototype, "connect").mockImplementation(poolConnectMock);
-    repository = new DBCommitsRepository();
+    jest.spyOn(Pool.prototype, "connect").mockImplementation(poolCommitConnectMock);
+    commitRepository = new DBCommitsRepository();
 });
 
 afterEach(() => {
@@ -32,7 +32,7 @@ afterEach(() => {
 
 describe("Save Commits", () => {
     it("should save a commit", async () => {
-        await repository.saveCommit('owner', 'repo', tddCycleDataObjectMock);
+        await commitRepository.saveCommit('owner', 'repo', tddCycleDataObjectMock);
         const values = [
             "owner",
             "repo",
@@ -48,57 +48,57 @@ describe("Save Commits", () => {
             tddCycleDataObjectMock.coverage,
             tddCycleDataObjectMock.test_count
         ];
-        expect(poolConnectMock).toBeCalledTimes(1);
-        expect(clientQueryMock).toBeCalledWith('INSERT INTO commitsTable (owner, repoName, html_url, sha, total, additions, deletions, message,url, comment_count, commit_date, coverage, test_count) VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, $12, $13)', values);
+        expect(poolCommitConnectMock).toBeCalledTimes(1);
+        expect(clientCommitQueryMock).toBeCalledWith('INSERT INTO commitsTable (owner, repoName, html_url, sha, total, additions, deletions, message,url, comment_count, commit_date, coverage, test_count) VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, $12, $13)', values);
     });
-    testErrorHandling(() => repository.saveCommit('owner', 'repo', tddCycleDataObjectMock));
+    testErrorHandling(() => commitRepository.saveCommit('owner', 'repo', tddCycleDataObjectMock));
 });
 
 describe("Get Commits", () => {
     it('should get commits', async () => {
-        clientQueryMock.mockResolvedValueOnce({ rows: [{ id: 1 }] });
-        const jobs = await repository.getCommits('owner', 'repo');
-        expect(poolConnectMock).toBeCalledTimes(1);
-        expect(clientQueryMock).toBeCalledWith('SELECT * FROM commitsTable WHERE owner = $1 AND reponame = $2 ORDER BY commit_date DESC', ['owner', 'repo']);
+        clientCommitQueryMock.mockResolvedValueOnce({ rows: [{ id: 1 }] });
+        const jobs = await commitRepository.getCommits('owner', 'repo');
+        expect(poolCommitConnectMock).toBeCalledTimes(1);
+        expect(clientCommitQueryMock).toBeCalledWith('SELECT * FROM commitsTable WHERE owner = $1 AND reponame = $2 ORDER BY commit_date DESC', ['owner', 'repo']);
         expect(jobs).toEqual([{ id: 1 }]);
     });
-    testErrorHandling(() => repository.getCommits('owner', 'repo'));
+    testErrorHandling(() => commitRepository.getCommits('owner', 'repo'));
 });
 
 describe("Commit Exists", () => {
     it('should check if commit exists', async () => {
-        clientQueryMock.mockResolvedValueOnce({ rows: [{ exists: true }] });
-        const exists = await repository.commitExists('owner', 'repo', 'sha');
-        expect(poolConnectMock).toBeCalledTimes(1);
-        expect(clientQueryMock).toBeCalledWith('SELECT * FROM commitstable WHERE owner = $1 AND reponame = $2 AND sha=$3', ['owner', 'repo', 'sha']);
+        clientCommitQueryMock.mockResolvedValueOnce({ rows: [{ exists: true }] });
+        const exists = await commitRepository.commitExists('owner', 'repo', 'sha');
+        expect(poolCommitConnectMock).toBeCalledTimes(1);
+        expect(clientCommitQueryMock).toBeCalledWith('SELECT * FROM commitstable WHERE owner = $1 AND reponame = $2 AND sha=$3', ['owner', 'repo', 'sha']);
         expect(exists).toEqual(true);
     });
-    testErrorHandling(() => repository.commitExists('owner', 'repo',  'sha'));
+    testErrorHandling(() => commitRepository.commitExists('owner', 'repo',  'sha'));
 });
 
 
 describe("Repository Existence", () => {
     it('should check if repository exists', async () => {
-        clientQueryMock.mockResolvedValueOnce({ rows: [{ count: "1" }] });
-        const exists = await repository.repositoryExists('owner', 'repo');
-        expect(poolConnectMock).toBeCalledTimes(1);
-        expect(clientQueryMock).toBeCalledWith('SELECT COUNT(*) FROM commitstable WHERE owner = $1 AND reponame = $2', ['owner', 'repo']);
+        clientCommitQueryMock.mockResolvedValueOnce({ rows: [{ count: "1" }] });
+        const exists = await commitRepository.repositoryExists('owner', 'repo');
+        expect(poolCommitConnectMock).toBeCalledTimes(1);
+        expect(clientCommitQueryMock).toBeCalledWith('SELECT COUNT(*) FROM commitstable WHERE owner = $1 AND reponame = $2', ['owner', 'repo']);
         expect(exists).toEqual(true);
     });
-    testErrorHandling(() => repository.repositoryExists('owner', 'repo'));
+    testErrorHandling(() => commitRepository.repositoryExists('owner', 'repo'));
 });
 describe("getCommitsNotSaved", () => {
     it("should return commits that are not saved in the database", async () => {
-        clientQueryMock.mockResolvedValueOnce({ rows: [] });
-        const result = await repository.getCommitsNotSaved("owner", "repoName", commitsFromGithub);
-        expect(poolConnectMock).toBeCalledTimes(1);
-        expect(clientQueryMock).toHaveBeenNthCalledWith(1, 'SELECT * FROM commitstable WHERE owner = $1 AND reponame = $2 AND sha=$3', ["owner", "repoName", commitsFromGithub[0].sha]);
+        clientCommitQueryMock.mockResolvedValueOnce({ rows: [] });
+        const result = await commitRepository.getCommitsNotSaved("owner", "repoName", commitsFromGithub);
+        expect(poolCommitConnectMock).toBeCalledTimes(1);
+        expect(clientCommitQueryMock).toHaveBeenNthCalledWith(1, 'SELECT * FROM commitstable WHERE owner = $1 AND reponame = $2 AND sha=$3', ["owner", "repoName", commitsFromGithub[0].sha]);
         expect(result).toEqual(unsavedCommits);
     });
 });
 describe("Commit Saving Commits List", () => {
     it('should save commits list', async () => {
-        await repository.saveCommitsList('owner', 'repo', [tddCycleDataObjectMock]);
+        await commitRepository.saveCommitsList('owner', 'repo', [tddCycleDataObjectMock]);
         const values = [
             "owner",
             "repo",
@@ -114,8 +114,8 @@ describe("Commit Saving Commits List", () => {
             tddCycleDataObjectMock.coverage,
             tddCycleDataObjectMock.test_count
         ];
-        expect(poolConnectMock).toBeCalledTimes(1);
-        expect(clientQueryMock).toHaveBeenNthCalledWith(1, 'INSERT INTO commitsTable (owner, repoName, html_url, sha, total, additions, deletions, message,url, comment_count, commit_date, coverage, test_count) VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, $12, $13)', values);
+        expect(poolCommitConnectMock).toBeCalledTimes(1);
+        expect(clientCommitQueryMock).toHaveBeenNthCalledWith(1, 'INSERT INTO commitsTable (owner, repoName, html_url, sha, total, additions, deletions, message,url, comment_count, commit_date, coverage, test_count) VALUES ($1, $2, $3, $4, $5,$6, $7, $8, $9, $10, $11, $12, $13)', values);
     });
 });
 
