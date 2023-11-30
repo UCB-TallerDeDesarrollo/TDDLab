6b9190291fd4ace41b82e73ea1f0c2f2169f613a
BMB <bruno.marquez@ucb.edu.bo>
2023-11-29 23:49:08 -0400

    Fix return value in userHasAnAcount method

--- Web Ui/src/modules/Auth/application/checkIfUserHasAccount.ts
+++ Web Ui/src/modules/Auth/application/checkIfUserHasAccount.ts
@@ -10,7 +10,7 @@ export class CheckIfUserHasAccount {
   async userHasAnAcount(email: string) {
     let answerData: UserOnDb = await this.adapter.getAccountInfo(email);
     if (answerData.course) {
-      return answerData;
+      return answerData.course;
     }
     return null;
   }
