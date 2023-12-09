06d33361fc69099b164a9dee5fc2f5113d65b571
Rene <ddcrene@gmail.com>
2023-12-08 21:16:36 -0400

    Refactoring graph

--- Web Ui/src/sections/TDDCycles-Visualization/components/TDDChart.tsx
+++ Web Ui/src/sections/TDDCycles-Visualization/components/TDDChart.tsx
@@ -1,39 +1,13 @@
 import { CommitDataObject } from "../../../modules/TDDCycles-Visualization/domain/githubCommitInterfaces";
 import { JobDataObject } from "../../../modules/TDDCycles-Visualization/domain/jobInterfaces";
-import { Line, getElementAtEvent } from "react-chartjs-2";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import "../styles/TDDChartStyles.css";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler,
-  LineController,
-  LineElement,
-} from "chart.js";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler,
-  LineController,
-  LineElement
-);
+import TDDLineCharts from "./TDDLineChart";
 
 interface CycleReportViewProps {
   commits: CommitDataObject[] | null;
@@ -42,6 +16,7 @@ interface CycleReportViewProps {
 
 function TDDCharts({ commits, jobsByCommit }: Readonly<CycleReportViewProps>) {
   const maxLinesInGraph = 100;
+  const [metricSelected, setMetricSelected] = useState("Cobertura de Código");
 
   const filteredCommitsObject = (() => {
     if (commits != null) {
@@ -53,179 +28,7 @@ function TDDCharts({ commits, jobsByCommit }: Readonly<CycleReportViewProps>) {
     return commits;
   })();
 
-  function getDataLabels() {
-    if (filteredCommitsObject != null) {
-      const commitsArray = filteredCommitsObject.map(
-        (commit) => `Commit ${filteredCommitsObject.indexOf(commit) + 1}`
-      );
-      return commitsArray;
-    } else {
-      return [];
-    }
-  }
 
-  function getCommitName() {
-    if (filteredCommitsObject != null) {
-      const commitsArray = filteredCommitsObject.map(
-        (commit) => commit.commit.message
-      );
-      return commitsArray.reverse();
-    } else {
-      return [];
-    }
-  }
-
-  function getColorConclusion() {
-    if (filteredCommitsObject != null && jobsByCommit != null) {
-      const conclusions = filteredCommitsObject.map((commit) => {
-        let job = jobsByCommit.find((job) => job.sha === commit.sha);
-        if (job != null && job.conclusion === "success") return "green";
-        else if (job === undefined) return "black";
-        else return "red";
-      });
-      return conclusions.reverse();
-    } else {
-      return ["white"];
-    }
-  }
-
-  function getCommitStats() {
-    if (filteredCommitsObject != null) {
-      const additions = filteredCommitsObject
-        .map((commit) => commit.stats.additions)
-        .reverse();
-      const deletions = filteredCommitsObject
-        .map((commit) => commit.stats.deletions)
-        .reverse();
-      const total = filteredCommitsObject
-        .map((commit) => commit.stats.total)
-        .reverse();
-      return [additions, deletions, total];
-    } else {
-      return [[], [], []];
-    }
-  }
-
-  function getCommitCoverage() {
-    if (filteredCommitsObject != null) {
-      const coverage = filteredCommitsObject
-        .map((commit) => commit.coverage)
-        .reverse();
-      return coverage;
-    } else {
-      return [];
-    }
-  }
-
-  function getTestsCount() {
-    if (filteredCommitsObject != null) {
-      const testCount = filteredCommitsObject
-        .map((commit) => commit.test_count)
-        .reverse();
-      return testCount;
-    } else {
-      return [];
-    }
-  }
-
-  function getCommitLink() {
-    if (filteredCommitsObject != null) {
-      const urls = filteredCommitsObject.map((commit) => commit.html_url);
-      return urls.reverse();
-    } else {
-      return [];
-    }
-  }
-
-  let dataChart: any = {};
-
-  function getDataChart(dataChartSelected: any, dataLabel: string) {
-    dataChart = {
-      labels: getDataLabels(),
-      datasets: [
-        {
-          label: dataLabel,
-          backgroundColor: getColorConclusion(),
-          data: dataChartSelected,
-          links: getCommitLink(),
-        },
-      ],
-    };
-    return dataChart;
-  }
-
-  function getOptionsChart(axisText: string) {
-    const optionsLineChart = {
-      responsive: true,
-      pointRadius: 12,
-      pointHoverRadius: 15,
-      scales: {
-        x: {
-          title: {
-            display: true,
-            text: "Commits Realizados",
-            font: {
-              size: 20,
-              lineHeight: 1.2,
-            },
-          },
-        },
-        y: {
-          title: {
-            display: true,
-            text: axisText,
-            font: {
-              size: 20,
-              lineHeight: 1.2,
-            },
-          },
-        },
-      },
-      plugins: {
-        tooltip: {
-          callbacks: {
-            title: function (context: any) {
-              return `${getDataLabels()[context[0].dataIndex]}: ${
-                getCommitName()[context[0].dataIndex]
-              }`;
-            },
-            afterBody: function (context: any) {
-              const afterBodyContent: any = [];
-              afterBodyContent.push(
-                `Líneas de Código Añadido: ${
-                  getCommitStats()[0][context[0].dataIndex]
-                }`
-              );
-              afterBodyContent.push(
-                `Líneas de Código Eliminado: ${
-                  getCommitStats()[1][context[0].dataIndex]
-                }`
-              );
-              return afterBodyContent;
-            },
-          },
-        },
-      },
-    };
-    return optionsLineChart;
-  }
-
-  const chartRef = useRef<any>();
-
-  const onClick = (event: any) => {
-    if (getElementAtEvent(chartRef.current, event).length > 0) {
-      const dataSetIndexNum = getElementAtEvent(chartRef.current, event)[0]
-        .datasetIndex;
-      const dataPoint = getElementAtEvent(chartRef.current, event)[0].index;
-      console.log(dataChart.datasets[dataSetIndexNum].links[dataPoint]);
-      window.open(
-        dataChart.datasets[dataSetIndexNum].links[dataPoint],
-        "_blank"
-      );
-    }
-  };
-
-  const [metricSelected, setMetricSelected] = useState("Cobertura de Código");
 
   const handleSelectChange = (event: SelectChangeEvent) => {
     setMetricSelected(event.target.value);
@@ -257,45 +60,11 @@ function TDDCharts({ commits, jobsByCommit }: Readonly<CycleReportViewProps>) {
         </FormControl>
       </Box>
 
-      {metricSelected === "Cobertura de Código" ? 
-      (
-        <Line
-          height="100"
-          data={getDataChart(
-            getCommitCoverage(),
-            "Porcentaje de Cobertura de Código"
-          )}
-          options={getOptionsChart("Cobertura de Código")}
-          onClick={onClick}
-          ref={chartRef}
-          data-testid="graph-coverage"
-        />
-      ): metricSelected === "Líneas de Código Modificadas" ?
-      (
-        <Line
-          height="100"
-          data={getDataChart(
-            getCommitStats()[2],
-            "Total de Líneas de Código Modificadas"
-          )}
-          options={getOptionsChart("Líneas de Código Modificadas")}
-          onClick={onClick}
-          ref={chartRef}
-          data-testid="graph-modified-lines"
-        />
-      ):(
-        <Line
-          height="100"
-          data={getDataChart(
-            getTestsCount(),
-            "Total Número de Tests"
-          )}
-          options={getOptionsChart("Número de Tests")}
-          onClick={onClick}
-          ref={chartRef}
-          data-testid="graph-test-count"
-        />
-      )}
+      <TDDLineCharts 
+        filteredCommitsObject={filteredCommitsObject} 
+        jobsByCommit={jobsByCommit}
+        optionSelected={metricSelected}
+      ></TDDLineCharts>
     </div>
   );
 }
