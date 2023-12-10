2ad855833eac059afc6d432a03288e8dbccc961a
Gabriel Ichiro <gabriel582084@gmail.com>
2023-12-09 23:45:37 -0400

    Revert "any to react.reactnode"
    This reverts commit 3065c9b4a6c25528d83d6b7b32a49561726fedd8.
    
    roll back

--- Web Ui/src/sections/GroupInvitation/components/SuccessfulEnrollmentPopUp.tsx
+++ Web Ui/src/sections/GroupInvitation/components/SuccessfulEnrollmentPopUp.tsx
@@ -1,32 +1,30 @@
-import * as React from "react";
-import DialogContentText from "@mui/material/DialogContentText";
-import PopUp from "./PopUp";
+import * as React from 'react';
+import DialogContentText from '@mui/material/DialogContentText';
+import PopUp from './PopUp';
 
 function SuccessfulEnrollmentPopUp() {
   const [open, setOpen] = React.useState(true);
 
   const handleClose = () => {
     setOpen(false);
-    window.location.href = "/";
-    console.log(open);
+    window.location.href = '/';
+    console.log(open)
   };
 
-  const dialogContent: React.ReactNode = (
+  const dialogContent:any = (
     <DialogContentText>
-      Ahora eres parte del Curso "TDD II-2023 de Ingeniería de Software". Ya
-      puedes aprender y mejorar tus skills de programación con las tareas del
-      curso.
+        Ahora eres parte del Curso "TDD II-2023 de Ingeniería de Software". Ya puedes aprender y mejorar tus skills de programación con las tareas del curso.
     </DialogContentText>
   );
 
   return (
     <div>
-      <PopUp
-        handleClose={handleClose}
-        open={open}
-        dialogTitle="Inscripción Exitosa"
-        dialogContent={dialogContent}
-      ></PopUp>
+        <PopUp 
+            handleClose={handleClose} 
+            open={open} 
+            dialogTitle="Inscripción Exitosa" 
+            dialogContent={dialogContent}>
+        </PopUp>
     </div>
   );
 }
