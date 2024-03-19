eb809faf4288ef02145bdd9f9f832743deaac450
Osber <osberriojalopez@gmail.com>
2024-03-18 22:33:20 -0400

    se recupera la lista de grupos

--- Web Ui/src/sections/Assignments/components/EditAssignmentForm.tsx
+++ Web Ui/src/sections/Assignments/components/EditAssignmentForm.tsx
@@ -5,12 +5,17 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import { Box, TextField } from "@mui/material";
 import { useState } from 'react';
-import { MenuItem, Select, FormControl, InputLabel } from '@mui/material';
+import { MenuItem, Select } from '@mui/material';
 import { SelectChangeEvent } from '@mui/material/Select';
+import GroupsRepository from "../../../modules/Groups/repository/GroupsRepository"
+import { GroupDataObject } from "../../../modules/Groups/domain/GroupInterface";
+import { useEffect } from 'react';
+import GetGroups from "../../../modules/Groups/application/GetGroups";
 
 interface EditAssignmentDialogProps {
   readonly assignmentId: number;
   readonly currentGroupName: string; // Agrega una propiedad para el nombre del grupo actual
+  //readonly currentGroupId: number;
   //readonly assignmentGroupId: number;
   //readonly groups: { id: number; groupName: string }[];
   readonly onClose: () => void;
@@ -20,6 +25,7 @@ interface EditAssignmentDialogProps {
 function EditAssignmentDialog({
   assignmentId,
   currentGroupName,
+  //currentGroupId,
   //assignmentGroupId,
   //groups,
   onClose,
@@ -29,6 +35,38 @@ function EditAssignmentDialog({
   const handleGroupChange = (event: React.ChangeEvent<{ value: unknown }>) => {
     setSelectedGroup(event.target.value as number);
   };
+
+  const groupRepository = new GroupsRepository();
+  const [groups, setGroups] = useState<GroupDataObject[]>([]);
+  useEffect(() => {
+    const fetchGroups = async () => {
+      const getGroups = new GetGroups(groupRepository);
+      const allGroups = await getGroups.getGroups();
+      setGroups(allGroups);
+    };
+
+    fetchGroups();
+  });
+//const [selectedGroup, setSelectedGroup] = useState<number>(0);
+  //const [selectedGroup, setSelectedGroup] = useState<number>(currentGroupId);
+  // const [groups, setGroups] = useState<GroupDataObject[]>([]);
+
+  // useEffect(() => {
+  //   const fetchGroups = async () => {
+  //     try {
+  //       const groups = await GroupsRepository.getGroups();
+  //       setGroups(groups);
+  //     } catch (error) {
+  //       console.error('Error fetching groups:', error);
+  //     }
+  //   };
+
+  //   fetchGroups();
+  // }, []);
+
+  // const handleGroupChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+  //   setSelectedGroup(event.target.value as number);
+  // };
 
   // const [selectedGroup, setSelectedGroup] = useState(assignmentGroupId);
 
@@ -80,10 +118,11 @@ function EditAssignmentDialog({
             required
           >
             <MenuItem value={0}>{currentGroupName}</MenuItem>
-            <MenuItem value={1}>Grupo 1</MenuItem>
-            <MenuItem value={2}>Grupo 2</MenuItem>
-            <MenuItem value={3}>Grupo 3</MenuItem>
-            {/* Agrega más elementos de menú según tus grupos */}
+            {groups.map((group) => (
+              <MenuItem key={group.id} value={group.id}>
+                {group.groupName}
+              </MenuItem>
+            ))}
           </Select>
           /* <section>
             <FormControl fullWidth variant="outlined">
