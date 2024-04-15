983f717605a128b9e9d919e33f7d799235869e24
Torrixo <santy.torrico@gmail.com>
2024-04-14 22:30:35 -0400

    bug fixes and refactor

--- Web Ui/src/modules/Groups/repository/GroupsRepository.ts
+++ Web Ui/src/modules/Groups/repository/GroupsRepository.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { GroupDataObject } from "../domain/GroupInterface";
 import GroupsRepositoryInterface from "../domain/GroupsRepositoryInterface";
 
-const API_URL = "http://localhost:3000/api/assignments";
+const API_URL = "http://localhost:3000/api/groups";
 
 class GroupsRepository implements GroupsRepositoryInterface {
   async getGroups(): Promise<GroupDataObject[]> {
--- Web Ui/src/sections/Assignments/components/GroupFilter.tsx
+++ Web Ui/src/sections/Assignments/components/GroupFilter.tsx
@@ -2,33 +2,40 @@ import React from "react";
 import { MenuItem, SelectChangeEvent, Select } from "@mui/material";
 import { GroupDataObject } from "../../../modules/Groups/domain/GroupInterface";
 
-interface GroupFilterProps{
-    selectedGroup: number;
-    groupList: GroupDataObject[];
-    onChangeHandler: (event: SelectChangeEvent<number>) => void;
+interface GroupFilterProps {
+  selectedGroup: number;
+  groupList: GroupDataObject[];
+  onChangeHandler: (event: SelectChangeEvent<number>) => void;
 }
+
 const GroupFilter: React.FC<GroupFilterProps> = ({
-    selectedGroup,
-    groupList,
-    onChangeHandler,
+  selectedGroup,
+  groupList,
+  onChangeHandler,
 }) => {
-    return (
-        <Select
-            value={selectedGroup}
-            onChange={onChangeHandler}
-            displayEmpty
-            style={{ fontSize: "14px", height: "36px" }}
-        >
-            <MenuItem value={0} disabled>
-                Seleccione un grupo
-            </MenuItem>
-            {groupList.map((group) => (
-                <MenuItem key={group.id} value={group.id}>
-                    {group.groupName}
-                </MenuItem>
-            ))}
-        </Select>
-    );
+  return (
+    <Select
+      value={selectedGroup}
+      onChange={onChangeHandler}
+      displayEmpty
+      style={{ fontSize: "14px", height: "36px" }}
+    >
+      <MenuItem value={0} disabled>
+        Seleccione un grupo
+      </MenuItem>
+      {groupList.length > 0 ? (
+        groupList.map((group) => (
+          <MenuItem key={group.id} value={group.id}>
+            {group.groupName}
+          </MenuItem>
+        ))
+      ) : (
+        <MenuItem value={0} disabled>
+          No hay grupos disponibles
+        </MenuItem>
+      )}
+    </Select>
+  );
 };
 
 export default GroupFilter;
