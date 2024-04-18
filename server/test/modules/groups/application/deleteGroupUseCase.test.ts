7326a13fe0abe9a9e161f8babc5ebfa424bee8c6
fabi-cm <dahofcmmejia@gmail.com>
2024-04-17 23:52:16 -0400

    Pruebas unitarias para la eliminacion de todas las tareas al eliminar un grupo

--- server/test/modules/groups/application/deleteGroupUseCase.test.ts
+++ server/test/modules/groups/application/deleteGroupUseCase.test.ts
@@ -1,9 +1,20 @@
 import DeleteGroupUseCase from '../../../../src/modules/Groups/application/GroupUseCases/deleteGroupUseCase';
 import { getGroupsRepositoryMock } from '../../../__mocks__/groups/repositoryMock';
 import { getDataGroupMock, getDataListOfGroupsMock } from '../../../__mocks__/groups/dataTypeMocks/groupData';
+import { getAssignmentListMock } from '../../../__mocks__/assignments/dataTypeMocks/assignmentData';
+import { getAssignmentRepositoryMock } from '../../../__mocks__/assignments/repositoryMock';
+import DeleteAssignment from '../../../../src/modules/Assignments/application/AssignmentUseCases/deleteAssignmentUseCase';
 
 const groupRepositoryMock = getGroupsRepositoryMock();
-const deleteGroupUseCase = new DeleteGroupUseCase(groupRepositoryMock);
+let deleteGroupUseCase: DeleteGroupUseCase;
+const assignmentRepositoryMock = getAssignmentRepositoryMock();
+let deleteAssignment: DeleteAssignment;
+
+beforeEach(() => {
+  deleteGroupUseCase = new DeleteGroupUseCase(groupRepositoryMock);
+  deleteAssignment = new DeleteAssignment(assignmentRepositoryMock);
+});
+
 
 describe('DeleteGroupUseCase', () => {
   it('should delete an existing group successfully', async () => {
@@ -25,10 +36,36 @@ describe('DeleteGroupUseCase', () => {
 
   it('should handle errors during group deletion', async () => {
     const groupid = 3;
+    const mockError = new Error("Deletion failed due to internal error");
     groupRepositoryMock.obtainGroupById.mockResolvedValueOnce(getDataGroupMock);
-    groupRepositoryMock.deleteGroup.mockRejectedValueOnce(new Error());
-    await expect(deleteGroupUseCase.execute(groupid)).rejects.toThrowError(Error);
+    groupRepositoryMock.deleteGroup.mockRejectedValueOnce(mockError);
+
+    // Test the specific error throw and error message
+    await expect(deleteGroupUseCase.execute(groupid)).rejects.toThrowError("Deletion failed due to internal error");
+
     expect(groupRepositoryMock.obtainGroupById).toHaveBeenCalledWith(groupid);
+    expect(groupRepositoryMock.deleteGroup).toHaveBeenCalledWith(groupid);
+
+    // Verify no unnecessary methods were called
+    expect(groupRepositoryMock.updateGroup).not.toHaveBeenCalled();
+    expect(groupRepositoryMock.createGroup).not.toHaveBeenCalled();
+  });
+
+  it('should delete all related assignments when a group is deleted', async ()=>{
+    const groupid = 1;
+    const assignmentsToDelete = getAssignmentListMock().filter(assignment => assignment.groupid === groupid);
+
+    groupRepositoryMock.obtainGroupById.mockResolvedValueOnce(getDataListOfGroupsMock.find(group => group.id === groupid));
+    assignmentRepositoryMock.obtainAssignmentsByGroupId.mockResolvedValueOnce(assignmentsToDelete)
+
+    for (const assignment of assignmentsToDelete) {
+      await deleteAssignment.execute(assignment.id);
+    }
+    await deleteGroupUseCase.execute(groupid);
+
+    assignmentsToDelete.forEach(assignment => {
+      expect(assignmentRepositoryMock.deleteAssignment).toHaveBeenCalledWith(assignment.id);
+    });
     expect(groupRepositoryMock.deleteGroup).toHaveBeenCalledWith(groupid);
   });
 });
