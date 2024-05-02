08655f200e9c1d310101199e13d9b0162b3ec328
Angy Encinas <victorm@sintesis.com.bo>
2024-05-01 23:32:46 -0400

    añadir update al usuario

--- server/src/modules/Users/Repositories/UserRepository.ts
+++ server/src/modules/Users/Repositories/UserRepository.ts
@@ -2,10 +2,23 @@ import { Pool } from "pg"; // Import the Pool from 'pg'
 import config from "../../../config/db";
 import { User } from "../Domain/User";
 
+interface UserCreationObject {
+  email: string;
+  groupid: number;
+  role: string;
+}
 export class UserRepository {
   pool: Pool;
   constructor() {
     this.pool = new Pool(config);
+  }
+
+  public mapRowToUser(row: any): User {
+    return {
+      email: row.email,
+      groupid: row.groupid,
+      role: row.role
+    };
   }
   public async executeQuery(query: string, values?: any[]): Promise<any[]> {
     const client = await this.pool.connect();
@@ -45,4 +58,19 @@ export class UserRepository {
     const rows = await this.executeQuery(query);
     return rows.length > 0 ? rows : null;
   }
+  async updateGroup(
+    email: string,
+    updatedGroup: UserCreationObject
+  ): Promise<User | null> {
+    const { groupid } = updatedGroup; 
+    const query =
+      "UPDATE Users SET groupid = $1 WHERE email = $2 RETURNING *"; // Actualizado para modificar solo el ID del grupo
+    const values = [groupid, email]; // Ajustado para reflejar el nuevo ID del grupo y el ID del usuario
+    const rows = await this.executeQuery(query, values);
+    if (rows.length === 1) {
+      return this.mapRowToUser(rows[0]); // Actualizado para reflejar el mapeo de usuario
+    }
+    return null;
+  }
+  
 }
