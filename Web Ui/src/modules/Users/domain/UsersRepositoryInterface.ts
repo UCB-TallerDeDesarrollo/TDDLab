743f871f9df59eb03505376c454dd3e134e986af
Angy Encinas <victorm@sintesis.com.bo>
2024-05-03 12:11:49 -0400

    updateGroupUI

--- Web Ui/src/modules/Users/domain/UsersRepositoryInterface.ts
+++ Web Ui/src/modules/Users/domain/UsersRepositoryInterface.ts
@@ -3,6 +3,9 @@ import { UserDataObject } from "./UsersInterface";
 interface UsersRepositoryInterface {
   getUsers(): Promise<UserDataObject[]>;
   getUsersByGroupid(groupid: number): Promise<UserDataObject[]>;
+  getUserByEmail(email: string): Promise<UserDataObject | null>;
+  updateUser(email: string,
+    userData: UserDataObject):Promise<void>;
 }
 
 export default UsersRepositoryInterface;
