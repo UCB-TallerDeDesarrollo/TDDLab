916258dc759bf5cbe3d4fb0ce8ede43cd7fa4694
Torrixo <santy.torrico@gmail.com>
2024-05-04 21:21:07 -0400

    added tests for the functionality

---
+++ server/test/__mocks__/users/dataTypeMocks/mockUsers.ts
@@ -0,0 +1,7 @@
+import { User } from "../../../../src/modules/Users/Domain/User";
+
+export const mockUsers: User[] = [
+    { email: 'user1@example.com', groupid: 70, role: 'admin' },
+    { email: 'user2@example.com', groupid: 70, role: 'user' },
+    { email: 'user3@example.com', groupid: 71, role: 'user' },
+  ];
