c034ae46ac3c818da2faa65abd85e31ea36784cf
Osber <osberriojalopez@gmail.com>
2024-05-06 17:26:04 -0400

    el id del usuario esta disponible en la sesion para ser usado

--- Web Ui/src/modules/User-Authentication/domain/userOnDb.interface.ts
+++ Web Ui/src/modules/User-Authentication/domain/userOnDb.interface.ts
@@ -1,7 +1,12 @@
-interface UserOnDb {
+export interface UserOnDb {
   email: string;
   groupid: number;
   role: string;
 }
 
-export default UserOnDb;
+export interface UserId{
+  id: number;
+  email: string;
+  groupid: number;
+  role: string;
+}
