11c3a68b9242fbcf80fc7015cbba0812dbb3223a
Osber <osberriojalopez@gmail.com>
2024-05-07 01:40:26 -0400

    ahora se se guarda realmente el id del usuario en la sesion

--- Web Ui/src/modules/User-Authentication/application/setCookieAndGlobalStateForValidUser.ts
+++ Web Ui/src/modules/User-Authentication/application/setCookieAndGlobalStateForValidUser.ts
@@ -2,17 +2,15 @@ import { User } from "firebase/auth";
 import { setGlobalState } from "../domain/authStates";
 import { setSessionCookie } from "./setSessionCookie";
 import { UserOnDb } from "../domain/userOnDb.interface";
-import { UserId } from "../domain/userOnDb.interface";
 
 export function setCookieAndGlobalStateForValidUser(
   userData: User,
   usergroupid: UserOnDb | null,
-  userid: UserId,
   positiveCallback = () => {},
 ) {
   if (usergroupid && userData.photoURL && userData.email) {
     setGlobalState("authData", {
-      userid: userid.id,
+      userid: usergroupid.id,
       userProfilePic: userData.photoURL,
       userEmail: userData.email,
       usergroupid: usergroupid.groupid,
@@ -20,7 +18,7 @@ export function setCookieAndGlobalStateForValidUser(
     });
     setSessionCookie({
       userData,
-      id: userid.id,
+      id: usergroupid.id,
       role: usergroupid.role,
       groupid: usergroupid.groupid,
     });
