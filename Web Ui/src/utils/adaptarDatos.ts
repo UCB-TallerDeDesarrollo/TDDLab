de6fa43f57e7146d0d5ad602ec7950f9854a4ab3
Angy Encinas <victorm@sintesis.com.bo>
2024-05-08 01:04:19 -0400

    guardar grupo en base de datos

--- Web Ui/src/utils/adaptarDatos.ts
+++ Web Ui/src/utils/adaptarDatos.ts
@@ -1,12 +1,14 @@
-import { UserOnDb } from "../modules/User-Authentication/domain/userOnDb.interface";
+import { UserDataObject } from "../modules/Users/domain/UsersInterface";
 
 export function adaptarDatos(authData: {
+  userid: number | undefined;
     usergroupid: number;
     userProfilePic: string | undefined;
     userEmail: string | undefined;
     userRole: string | undefined;
-  }): UserOnDb {
+  }): UserDataObject {
     return {
+        id: authData.userid !== undefined ? authData.userid : 0,
         email: authData.userEmail !== undefined ? authData.userEmail : " ",
         groupid: authData.usergroupid,
         role: authData.userRole !== undefined ? authData.userRole : " " // Ajusta según tu necesidad, no hay una correspondencia directa en AuthData
