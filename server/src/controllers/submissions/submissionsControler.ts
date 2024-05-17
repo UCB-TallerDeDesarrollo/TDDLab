e23a1e06f2e83d0232a37632f7a01d39ad0b64ea
Osber <osberriojalopez@gmail.com>
2024-05-16 21:07:13 -0400

    servicio para obtener todos los submissions

--- server/src/controllers/submissions/submissionsControler.ts
+++ server/src/controllers/submissions/submissionsControler.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from "express";
 import SubmissionRepository from "../../modules/Submissions/Repository/SubmissionsRepository";
 import CreateSubmission from "../../modules/Submissions/Aplication/CreateSubmissionUseCase";
+import GetSubmissionsUseCase from "../../modules/Submissions/Aplication/getSubmissionsUseCase";
 
 
 class SubmissionController{
     private createSubmissionUseCase: CreateSubmission;
-
+    private getSubmissionsUseCase: GetSubmissionsUseCase;
 
     constructor (repository: SubmissionRepository) {
         this.createSubmissionUseCase = new CreateSubmission(repository);
+        this.getSubmissionsUseCase = new GetSubmissionsUseCase(repository);
     }
 
     async CreateSubmission(req: Request, res: Response): Promise<void>{
@@ -28,6 +30,15 @@ class SubmissionController{
             res.status(500).json({ error: "Server error" });
         }
     }
+
+    async getSubmissions(_req: Request, res: Response): Promise<void> {
+        try {
+          const assignments = await this.getSubmissionsUseCase.execute();
+          res.status(200).json(assignments);
+        } catch (error) {
+          res.status(500).json({ error: "Server error" });
+        }
+      }
 }
 
 export default SubmissionController;
