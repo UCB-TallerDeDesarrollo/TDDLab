5cd61d049deb9f1901489f1a127f6e5f604fab0c
Osber <osberriojalopez@gmail.com>
2024-05-17 20:03:32 -0400

    test to SubmissionController

---
+++ server/test/__mocks__/submissions/repositoryMock.ts
@@ -0,0 +1,13 @@
+
+export function getSubmissionRepositoryMock() {
+    return {
+        CreateSubmission: jest.fn(),
+        executeQuery: jest.fn(),
+        mapRowToSubmissions: jest.fn(),
+        ObtainSubmissions: jest.fn(),
+        UpdateSubmission: jest.fn(),
+        deleteSubmission: jest.fn(),
+        assignmentidExistsForSubmission: jest.fn(),
+        useridExistsForSubmission: jest.fn()
+    }
+}
