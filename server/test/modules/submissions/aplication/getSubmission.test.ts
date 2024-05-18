1c558d46cdde4299720b7a2d6db7b7a56128b3ca
Osber <osberriojalopez@gmail.com>
2024-05-17 21:41:00 -0400

    test getSubmission - should obtain submission

---
+++ server/test/modules/submissions/aplication/getSubmission.test.ts
@@ -0,0 +1,20 @@
+import GetSubmissionsUseCase from "../../../../src/modules/Submissions/Aplication/getSubmissionsUseCase";
+import { getSubmissionListMock } from "../../../__mocks__/submissions/dataTypeMocks/submissionData";
+import { getSubmissionRepositoryMock } from "../../../__mocks__/submissions/repositoryMock";
+
+const submissionRepositoryMock = getSubmissionRepositoryMock();
+let getSubmissions: GetSubmissionsUseCase;
+
+beforeEach(() => {
+    getSubmissions = new GetSubmissionsUseCase(submissionRepositoryMock);
+});
+
+describe("Get Submissions", () => {
+    it("should obtain submissions successfully", async () => {
+        submissionRepositoryMock.ObtainSubmissions.mockResolvedValueOnce(getSubmissionListMock());
+      const result = await getSubmissions.execute();
+      expect(result).toEqual(getSubmissionListMock());
+      expect(submissionRepositoryMock.ObtainSubmissions).toHaveBeenCalledTimes(1);
+    });
+});
+  
