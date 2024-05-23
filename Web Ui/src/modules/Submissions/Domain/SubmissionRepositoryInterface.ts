cdc485e4bc46f3212f046d5b3f9aac9e847cb5ce
Osber <osberriojalopez@gmail.com>
2024-05-23 18:32:53 -0400

    Se recupera el repository_link del submission

--- Web Ui/src/modules/Submissions/Domain/SubmissionRepositoryInterface.ts
+++ Web Ui/src/modules/Submissions/Domain/SubmissionRepositoryInterface.ts
@@ -4,6 +4,7 @@ interface SubmissionRepositoryInterface {
     createSubmission(submissionData: SubmissionCreationObject): Promise<void>;
     checkSubmissionExists(assignmentid: number, userid: number): Promise<{ hasStarted: boolean }>;
     getSubmissionsByAssignmentId(assignmentid: number): Promise<SubmissionDataObject[]>;
+    getSubmissionbyUserandSubmissionId(assignmentid: number, userid:number): Promise<SubmissionDataObject>;
     finishSubmission(id: number, submissionData: SubmissionUpdateObject): Promise<void>;
 }
 
