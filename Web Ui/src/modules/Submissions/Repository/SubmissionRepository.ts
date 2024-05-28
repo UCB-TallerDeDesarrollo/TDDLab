8f1015847f680a0ac16cb0c7470a06e5331b6ff5
Angy Encinas <victorm@sintesis.com.bo>
2024-05-28 16:49:16 -0400

    modificar para trabajar con dev

--- Web Ui/src/modules/Submissions/Repository/SubmissionRepository.ts
+++ Web Ui/src/modules/Submissions/Repository/SubmissionRepository.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { SubmissionCreationObject, SubmissionDataObject, SubmissionUpdateObject } from "../Domain/submissionInterfaces";
 import SubmissionRepositoryInterface from "../Domain/SubmissionRepositoryInterface";
 
-const API_URL = "https://tdd-lab-api-gold.vercel.app/api/submissions"; //https://localhost:3000/api/ -> https://tdd-lab-api-gold.vercel.app/api/
+const API_URL = "http://localhost:3000/api/submissions"; //https://localhost:3000/api/ -> https://tdd-lab-api-gold.vercel.app/api/
 
 class SubmissionRepository implements SubmissionRepositoryInterface {
 
