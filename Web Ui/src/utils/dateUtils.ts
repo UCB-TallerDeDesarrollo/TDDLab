43a9df97d1d2e7fcda43889aa60ab7fa2f8e925c
fabi-cm <dahofcmmejia@gmail.com>
2024-06-03 16:15:02 -0400

    Correccion de la fecha, ahora se muestra la fecha correcta del assignment y del submission

--- Web Ui/src/utils/dateUtils.ts
+++ Web Ui/src/utils/dateUtils.ts
@@ -1,8 +1,6 @@
-// dateUtils.ts
-
-// Function to format a date string as "MM/DD/YYYY"
 export function formatDate(dateString: string): string {
-  const date = new Date(dateString).toLocaleDateString();
-  return date;
-  //new Date(date).toLocaleDateString();
+  const datePart = dateString.substring(0, 10);
+  const [year, month, day] = datePart.split('-');
+  const formattedDate = `${day}/${month}/${year}`;
+  return formattedDate;
 }
