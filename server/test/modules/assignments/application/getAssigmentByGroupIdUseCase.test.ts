9aebb25c51c2ffffc93d9e22157ca3d04ed547b7
Torrixo <santy.torrico@gmail.com>
2024-06-08 20:26:01 -0400

    added test to handle errors

--- server/test/modules/assignments/application/getAssigmentByGroupIdUseCase.test.ts
+++ server/test/modules/assignments/application/getAssigmentByGroupIdUseCase.test.ts
@@ -9,12 +9,26 @@ beforeEach(() => {
 });
 
 describe("Get assignments by group ID", () => {
-  it("should obtain assignments successfully for a given group ID", async () => {
-    const groupid = 1;
-    assignmentRepositoryMock.obtainAssignmentsByGroupId.mockResolvedValueOnce(getAssignmentListMock());
-    const result = await getAssignmentsByGroupidUseCase.execute(groupid);
-    expect(result).toEqual(getAssignmentListMock());
-    expect(assignmentRepositoryMock.obtainAssignmentsByGroupId).toHaveBeenCalledTimes(1);
-    expect(assignmentRepositoryMock.obtainAssignmentsByGroupId).toHaveBeenCalledWith(groupid);
-  });
+    beforeEach(() => {
+        jest.clearAllMocks();
+      });
+    it("should obtain assignments successfully for a given group ID", async () => {
+        const groupid = 1;
+        assignmentRepositoryMock.obtainAssignmentsByGroupId.mockResolvedValueOnce(getAssignmentListMock());
+        const result = await getAssignmentsByGroupidUseCase.execute(groupid);
+        expect(result).toEqual(getAssignmentListMock());
+        expect(assignmentRepositoryMock.obtainAssignmentsByGroupId).toHaveBeenCalledTimes(1);
+        expect(assignmentRepositoryMock.obtainAssignmentsByGroupId).toHaveBeenCalledWith(groupid);
+    });
+    it("should handle errors when obtaining assignments by group ID", async () => {
+        assignmentRepositoryMock.obtainAssignmentsByGroupId.mockRejectedValue(new Error());
+        
+        try {
+        await getAssignmentsByGroupidUseCase.execute(1);
+        } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+        }
+    
+        expect(assignmentRepositoryMock.obtainAssignmentsByGroupId).toHaveBeenCalledTimes(1);
+    });
 });
