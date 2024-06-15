27e4c0821636c8f1694032a83a60af8cfa6ec6a7
Torrixo <santy.torrico@gmail.com>
2024-06-15 16:09:47 -0400

    Added test for getusersbyid

---
+++ Web Ui/test/modules/Users/repository/UsersRepository.test.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import UsersRepository from '../../../../src/modules/Users/repository/UsersRepository';
+import { UserDataObject } from '../../../../src/modules/Users/domain/UsersInterface';
+
+const axiosGetSpy = jest.spyOn(axios, 'get');
+
+const repository = new UsersRepository();
+const API_URL = 'https://tdd-lab-api-gold.vercel.app/api/user/users';
+
+describe('UsersRepository', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('getUserById', () => {
+    it('should fetch user by ID successfully', async () => {
+      const mockUser: UserDataObject = { id: 1, email: 'john@example.com', role: 'admin', groupid: 70 };
+      axiosGetSpy.mockResolvedValue({ status: 200, data: mockUser });
+
+      const result = await repository.getUserById(1);
+      expect(result).toEqual(mockUser);
+      expect(axiosGetSpy).toHaveBeenCalledWith(`${API_URL}/1`);
+    });
+  });
+
+  
+});
