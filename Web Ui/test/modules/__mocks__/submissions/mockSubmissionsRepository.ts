89eb278f8fb6b9f4bee4fc8a98857c6a8f3e732e
Israel Antezana Rojas <israelantezana@gmail.com>
2024-10-10 14:49:23 -0400

    Para resolver conflicto de sonar: Fields that are only assigned in the constructor should be 'readonly'

--- Web Ui/test/modules/__mocks__/submissions/mockSubmissionsRepository.ts
+++ Web Ui/test/modules/__mocks__/submissions/mockSubmissionsRepository.ts
@@ -1,15 +1,14 @@
 import SubmissionRepositoryInterface from "../../../../src/modules/Submissions/Domain/SubmissionRepositoryInterface";
-import { SubmissionDataObject} from "../../../../src/modules/Submissions/Domain/submissionInterfaces";
+import { SubmissionDataObject } from "../../../../src/modules/Submissions/Domain/submissionInterfaces";
 
-export class MockSubmissionRepository implements SubmissionRepositoryInterface{
-    private submissions: SubmissionDataObject[] = [];
+export class MockSubmissionRepository implements SubmissionRepositoryInterface {
+  private readonly submissions: SubmissionDataObject[] = [];
 
-    createSubmission = jest.fn(async (submissionData: SubmissionDataObject) => {
-        this.submissions.push(submissionData);
-      });
-    checkSubmissionExists = jest.fn();
-    getSubmissionsByAssignmentId = jest.fn();
-    getSubmissionbyUserandSubmissionId = jest.fn();
-    finishSubmission = jest.fn();
-
+  createSubmission = jest.fn(async (submissionData: SubmissionDataObject) => {
+    this.submissions.push(submissionData);
+  });
+  checkSubmissionExists = jest.fn();
+  getSubmissionsByAssignmentId = jest.fn();
+  getSubmissionbyUserandSubmissionId = jest.fn();
+  finishSubmission = jest.fn();
 }
--- server/src/controllers/submissions/submissionsController.ts
+++ server/src/controllers/submissions/submissionsController.ts
@@ -5,109 +5,117 @@ import GetSubmissionsUseCase from "../../modules/Submissions/Aplication/getSubmi
 import UpdateSubmission from "../../modules/Submissions/Aplication/updateSubmissionUSeCase";
 import DeleteSubmission from "../../modules/Submissions/Aplication/DeleteSubmissionUseCase";
 import GetSubmissionUseCase from "../../modules/Submissions/Aplication/getSubmissionUseCase";
-import GetSubmissionsByAssignmentIdUseCase from '../../modules/Submissions/Aplication/getSubmissionsByAssignmentIdUseCase';
+import GetSubmissionsByAssignmentIdUseCase from "../../modules/Submissions/Aplication/getSubmissionsByAssignmentIdUseCase";
 
-class SubmissionController{
-    private createSubmissionUseCase: CreateSubmission;
-    private getSubmissionsUseCase: GetSubmissionsUseCase;
-    private updateSubmissionUseCase: UpdateSubmission;
-    private deleteSubmissionUSeCase: DeleteSubmission;
-    private getSubmissionUseCase: GetSubmissionUseCase;
-    private getSubmissionsByAssignmentIdUseCase: GetSubmissionsByAssignmentIdUseCase;
+class SubmissionController {
+  private readonly createSubmissionUseCase: CreateSubmission;
+  private readonly getSubmissionsUseCase: GetSubmissionsUseCase;
+  private readonly updateSubmissionUseCase: UpdateSubmission;
+  private readonly deleteSubmissionUSeCase: DeleteSubmission;
+  private readonly getSubmissionUseCase: GetSubmissionUseCase;
+  private readonly getSubmissionsByAssignmentIdUseCase: GetSubmissionsByAssignmentIdUseCase;
 
-    constructor (repository: SubmissionRepository) {
-        this.createSubmissionUseCase = new CreateSubmission(repository);
-        this.getSubmissionsUseCase = new GetSubmissionsUseCase(repository);
-        this.getSubmissionUseCase = new GetSubmissionUseCase(repository)
-        this.updateSubmissionUseCase = new UpdateSubmission(repository);
-        this.deleteSubmissionUSeCase = new DeleteSubmission(repository);
-        this.getSubmissionsByAssignmentIdUseCase = new GetSubmissionsByAssignmentIdUseCase(repository);
+  constructor(repository: SubmissionRepository) {
+    this.createSubmissionUseCase = new CreateSubmission(repository);
+    this.getSubmissionsUseCase = new GetSubmissionsUseCase(repository);
+    this.getSubmissionUseCase = new GetSubmissionUseCase(repository);
+    this.updateSubmissionUseCase = new UpdateSubmission(repository);
+    this.deleteSubmissionUSeCase = new DeleteSubmission(repository);
+    this.getSubmissionsByAssignmentIdUseCase =
+      new GetSubmissionsByAssignmentIdUseCase(repository);
+  }
+
+  async CreateSubmission(req: Request, res: Response): Promise<void> {
+    try {
+      const { assignmentid, userid, status, repository_link, start_date } =
+        req.body;
+      const newSubmission = await this.createSubmissionUseCase.execute({
+        assignmentid,
+        userid,
+        status,
+        repository_link,
+        start_date,
+      });
+      res.status(201).json(newSubmission);
+    } catch (error) {
+      res.status(500).json({ error: "Server error" });
     }
+  }
 
-    async CreateSubmission(req: Request, res: Response): Promise<void>{
-        try{
-            const { assignmentid, userid, status, repository_link, start_date} = req.body;
-            const newSubmission = await this.createSubmissionUseCase.execute({
-                assignmentid,
-                userid,
-                status,
-                repository_link,
-                start_date
-            });
-            res.status(201).json(newSubmission);
-        } catch (error){
-            res.status(500).json({ error: "Server error" });
-        }
+  async getSubmissions(_req: Request, res: Response): Promise<void> {
+    try {
+      const assignments = await this.getSubmissionsUseCase.execute();
+      res.status(200).json(assignments);
+    } catch (error) {
+      res.status(500).json({ error: "Server error" });
     }
-
-    async getSubmissions(_req: Request, res: Response): Promise<void> {
-        try {
-          const assignments = await this.getSubmissionsUseCase.execute();
-          res.status(200).json(assignments);
-        } catch (error) {
-          res.status(500).json({ error: "Server error" });
-        }
-    }
-    async getSubmissionByAssignmentAndUser(req: Request, res: Response): Promise<void> {
-        try {
-            const assignmentid = parseInt(req.params.assignmentid);
-            const userid = parseInt(req.params.userid);
-            const submission = await this.getSubmissionUseCase.execute(assignmentid, userid);
-            if (submission) {
-                res.status(200).json(submission);
-            } else {
-                res.status(404).json({ message: "Submission not found" });
-            }
-        } catch (error) {
-            res.status(500).json({ error: "Server error" });
-        }
-    }
-
-    async updateSubmission(req: Request, res: Response): Promise<void> {
-        try{
-            const submissionid = parseInt(req.params.id);
-            const {
-                status,
-                end_date,
-                comment
-            } = req.body;
-            const updatedSubmission = await this.updateSubmissionUseCase.execute(
-                submissionid,
-                {
-                    status,
-                    end_date,
-                    comment                    
-                }
-            );
-            if(updatedSubmission){
-                res.status(200).json(updatedSubmission);
-            }else{
-                res.status(404).json({ error: "Submission not found" });
-            }
-        }catch (error){
-            res.status(500).json({ error: "Server error" });
-        }
-    }
-
-    async deleteSubmission(req: Request, res: Response): Promise<void> {
-        try {
-          const submissionid = parseInt(req.params.id);
-          await this.deleteSubmissionUSeCase.execute(submissionid);
-          res.status(204).send();
-        } catch (error) {
-          res.status(500).json({ error: "Server error" });
-        }
+  }
+  async getSubmissionByAssignmentAndUser(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const assignmentid = parseInt(req.params.assignmentid);
+      const userid = parseInt(req.params.userid);
+      const submission = await this.getSubmissionUseCase.execute(
+        assignmentid,
+        userid
+      );
+      if (submission) {
+        res.status(200).json(submission);
+      } else {
+        res.status(404).json({ message: "Submission not found" });
       }
-
-    async getSubmissionsByAssignmentId(req: Request, res: Response): Promise<void> {
-        try {
-            const assignmentid = parseInt(req.params.assignmentid, 10);
-            const assignments = await this.getSubmissionsByAssignmentIdUseCase.execute(assignmentid);
-            res.status(200).json(assignments);
-          } catch (error) {
-            res.status(500).json({ error: "Error getSubmissionsByAssignmentId" });
-          }
+    } catch (error) {
+      res.status(500).json({ error: "Server error" });
     }
+  }
+
+  async updateSubmission(req: Request, res: Response): Promise<void> {
+    try {
+      const submissionid = parseInt(req.params.id);
+      const { status, end_date, comment } = req.body;
+      const updatedSubmission = await this.updateSubmissionUseCase.execute(
+        submissionid,
+        {
+          status,
+          end_date,
+          comment,
+        }
+      );
+      if (updatedSubmission) {
+        res.status(200).json(updatedSubmission);
+      } else {
+        res.status(404).json({ error: "Submission not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ error: "Server error" });
+    }
+  }
+
+  async deleteSubmission(req: Request, res: Response): Promise<void> {
+    try {
+      const submissionid = parseInt(req.params.id);
+      await this.deleteSubmissionUSeCase.execute(submissionid);
+      res.status(204).send();
+    } catch (error) {
+      res.status(500).json({ error: "Server error" });
+    }
+  }
+
+  async getSubmissionsByAssignmentId(
+    req: Request,
+    res: Response
+  ): Promise<void> {
+    try {
+      const assignmentid = parseInt(req.params.assignmentid, 10);
+      const assignments =
+        await this.getSubmissionsByAssignmentIdUseCase.execute(assignmentid);
+      res.status(200).json(assignments);
+    } catch (error) {
+      res.status(500).json({ error: "Error getSubmissionsByAssignmentId" });
+    }
+  }
 }
 
 export default SubmissionController;
