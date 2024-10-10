363034b9b062c205574bba11c63c9d9c1905a976
Israel Antezana Rojas <israelantezana@gmail.com>
2024-10-10 13:26:29 -0400

    Para corregir regla de sonar: Fields that are only assigned in the constructor should be 'readonly'

--- Web Ui/src/modules/Submissions/Aplication/checkSubmissionExists.ts
+++ Web Ui/src/modules/Submissions/Aplication/checkSubmissionExists.ts
@@ -1,9 +1,17 @@
 import SubmissionRepositoryInterface from "../Domain/SubmissionRepositoryInterface";
 
 export class CheckSubmissionExists {
-    constructor(private submissionRepository: SubmissionRepositoryInterface) {}
+  constructor(
+    private readonly submissionRepository: SubmissionRepositoryInterface
+  ) {}
 
-    async checkSubmissionExists(assignmentid: number, userid: number): Promise<{ hasStarted: boolean }> {
-        return await this.submissionRepository.checkSubmissionExists(assignmentid, userid);
-    }
+  async checkSubmissionExists(
+    assignmentid: number,
+    userid: number
+  ): Promise<{ hasStarted: boolean }> {
+    return await this.submissionRepository.checkSubmissionExists(
+      assignmentid,
+      userid
+    );
+  }
 }
--- Web Ui/src/modules/Users/application/getUsersByGroupid.ts
+++ Web Ui/src/modules/Users/application/getUsersByGroupid.ts
@@ -4,7 +4,7 @@ import { UserDataObject } from "../domain/UsersInterface";
 import UsersRepositoryInterface from "../domain/UsersRepositoryInterface";
 
 class GetUsersByGroupId {
-  constructor(private userRepository: UsersRepositoryInterface) {}
+  constructor(private readonly userRepository: UsersRepositoryInterface) {}
 
   async execute(groupid: number): Promise<UserDataObject[]> {
     return await this.userRepository.getUsersByGroupid(groupid);
