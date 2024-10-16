29f50f0d961b11fddb42c72674ec4c0f6c17a176
Adriana-Tarquino <99218283+Adriana-Tarquino@users.noreply.github.com>
2024-10-16 19:50:32 -0400

    Integrate Button with Timeline

---
+++ VSCodeExtension/src/modules/Button/application/commands.ts
@@ -0,0 +1,9 @@
+import { TerminalRepository } from '../repository/Buttonrepository';
+
+export class CommandService {
+  constructor(private terminalRepository: TerminalRepository) {}
+
+  async runTestCommand() {
+    await this.terminalRepository.createTerminal('TDD Terminal', 'npm --version');
+  }
+}
