e5db629d218c4e851495e17deda31eab4c6bee0e
NicolasVidal03 <nicolas.vidal@ucb.edu.bo>
2024-10-17 00:07:09 -0400

    Timeline se muestra correctamente

--- VSCodeExtension/src/extension.ts
+++ VSCodeExtension/src/extension.ts
@@ -4,6 +4,7 @@ import { CommandService } from './modules/Button/application/commands';
 import { TerminalRepository } from './modules/Button/repository/Buttonrepository';
 import { MyTreeItem } from './modules/Button/domain/treeItem';
 import * as path from 'path';
+import * as fs from 'fs';
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -22,17 +23,29 @@ export function activate(context: vscode.ExtensionContext) {
             timelineViewProvider.showTimeline(timelineViewProvider.currentWebview);
         }
     });
+
+
+    const jsonFilePath = path.join(vscode.workspace.workspaceFolders?.[0]?.uri.fsPath || '', 'script', 'tdd_log.json');
+
+    // Observa el archivo JSON para detectar cambios
+    fs.watch(jsonFilePath, (eventType, filename) => {
+        if (eventType === 'change') {
+            vscode.commands.executeCommand('workbench.view.extension.timelineContainer');
+
+            if (timelineViewProvider.currentWebview) {
+                timelineViewProvider.showTimeline(timelineViewProvider.currentWebview);
+            }
+        }
+    });
+
+
     const terminalRepository = new TerminalRepository();
     const commandService = new CommandService(terminalRepository);
     
     let runTestCommand = vscode.commands.registerCommand('TDD.runTest', async () => {
       await commandService.runTestCommand();
 
-      vscode.commands.executeCommand('workbench.view.extension.timelineContainer');
-
-        if (timelineViewProvider.currentWebview) {
-            timelineViewProvider.showTimeline(timelineViewProvider.currentWebview);
-        }
+      
     });
   
     context.subscriptions.push(runTestCommand);
