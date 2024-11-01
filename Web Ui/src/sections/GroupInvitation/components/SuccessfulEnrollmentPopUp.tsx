cc0244a4f61f1c9ee7ae3891117e139cb55ea259
Adrian Montano <adrian.montano2011@yahoo.com>
2024-11-01 14:34:08 -0400

    Se corrigió la prueba

--- Web Ui/src/sections/GroupInvitation/components/SuccessfulEnrollmentPopUp.tsx
+++ Web Ui/src/sections/GroupInvitation/components/SuccessfulEnrollmentPopUp.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 import DialogContentText from "@mui/material/DialogContentText";
 import PopUp from "./PopUp";
+import {VITE_API} from "../../../../config.ts";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function SuccessfulEnrollmentPopUp() {
   const [open, setOpen] = React.useState(true);
+  const [groupName, setGroupName] = useState<string | null>(null);
 
   const handleClose = () => {
     setOpen(false);
@@ -11,11 +16,27 @@ function SuccessfulEnrollmentPopUp() {
     console.log(open);
   };
 
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const groupId = urlParams.get("groupid");
+
+    if (groupId) {
+      // Realizar una solicitud a la API para obtener el nombre del grupo
+      axios.get(`${VITE_API}/groups/${groupId}`)
+        .then(response => {
+          setGroupName(response.data.groupName);
+        })
+        .catch(error => {
+          console.error("Error al obtener el nombre del grupo:", error);
+        });
+    }
+  }, []);
+
+
   const dialogContent: any = (
     <DialogContentText>
-      Ahora eres parte del Curso "TDD II-2023 de Ingeniería de Software". Ya
-      puedes aprender y mejorar tus skills de programación con las tareas del
-      curso.
+      Ahora eres parte del grupo {groupName}.
+      Ya puedes aprender y mejorar tus skills de programación con las tareas del curso.
     </DialogContentText>
   );
 
