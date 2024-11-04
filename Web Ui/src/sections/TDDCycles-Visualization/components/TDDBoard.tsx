aabd852a6a280c8676b7726128e888e2083c547b
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2024-11-04 19:24:06 -0400

    Agregar alineacion del zoom

--- Web Ui/src/sections/TDDCycles-Visualization/components/TDDBoard.tsx
+++ Web Ui/src/sections/TDDCycles-Visualization/components/TDDBoard.tsx
@@ -199,7 +199,7 @@ const TDDBoard: React.FC<CycleReportViewProps> = ({ commits, jobsByCommit }) => 
       {zoomedChart && (
         <div className="zoom-modal" onClick={closeModal} style={{
           position: "fixed", top: 0, left: 0, width: "100%", height: "100vh", 
-          backgroundColor: "rgba(0, 0, 0, 0.5)", display: "flex", alignItems: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.5)", display: "flex", alignItems: "center", justifyContent:"center",
           zIndex: 1000
         }}>
           <div style={{ width: "80%", backgroundColor: "white", padding: "20px", borderRadius: "8px" }}>
