0364559e3353308b67647481191e88dd5ec91d6d
NicolasVidal03 <nicolas.vidal@ucb.edu.bo>
2024-11-04 23:12:13 -0400

    Timeline se muestra al iniciar la extensión

--- VSCodeExtension/src/modules/Timeline/application/GetTimeline.ts
+++ VSCodeExtension/src/modules/Timeline/application/GetTimeline.ts
@@ -14,12 +14,8 @@ export class GetTimeline  {
         try {
             const timeline = await this.timelineRepository.getTimelines();
             response = timeline;
-        } catch (err) {
-            if (err instanceof Error) {
-                vscode.window.showErrorMessage(`Error al mostrar la línea de tiempo: ${err.message}`);
-            } else {
-                vscode.window.showErrorMessage(`Error desconocido al mostrar la línea de tiempo`);
-            }
+        } catch {
+            vscode.window.showErrorMessage('Error al obtener la linea de tiempo.');
         }
         return response;
     }
