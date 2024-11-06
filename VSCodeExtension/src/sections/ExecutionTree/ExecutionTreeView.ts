20bd6376402a4005e828b608bf3392cef847ee7a
denilsonFiesta <inocentedenil@gmail.com>
2024-11-06 14:03:17 -0400

    Refactorizacion de botones

--- VSCodeExtension/src/sections/ExecutionTree/ExecutionTreeView.ts
+++ VSCodeExtension/src/sections/ExecutionTree/ExecutionTreeView.ts
@@ -1,15 +1,14 @@
 import * as vscode from 'vscode';
-import { TestExecutionButton } from '../../modules/RunTestButton/domain/model/TestExecutionButton';
-import { CloneExecutionButton } from '../../modules/CloneButton/domain/model/CloneExecutionButton';
+import { ExecutionButton } from '../../model/ExecutionButton';
 
-export class TestExecutionTreeDataProvider implements vscode.TreeDataProvider<TestExecutionButton> {
-  getTreeItem(element: TestExecutionButton): vscode.TreeItem {
+export class ExecutionTreeDataProvider implements vscode.TreeDataProvider<ExecutionButton> {
+  getTreeItem(element: ExecutionButton): vscode.TreeItem {
     return element;
   }
 
-  getChildren(element?: TestExecutionButton): Thenable<TestExecutionButton[]> {
+  getChildren(element?: ExecutionButton): Thenable<ExecutionButton[]> {
     return Promise.resolve([
-      new TestExecutionButton(
+      new ExecutionButton(
         'Run Test',
         vscode.TreeItemCollapsibleState.None,
         { command: 'TDD.runTest', title: 'Run Test' },
@@ -17,7 +16,7 @@ export class TestExecutionTreeDataProvider implements vscode.TreeDataProvider<Te
         new vscode.ThemeIcon('play', new vscode.ThemeColor('charts.green')),
         'Execute TDD Tests'
       ),
-      new CloneExecutionButton(
+      new ExecutionButton(
         'Open New Project',
         vscode.TreeItemCollapsibleState.None,
         { command: 'TDD.cloneCommand', title: 'Open New Project'},
@@ -29,12 +28,12 @@ export class TestExecutionTreeDataProvider implements vscode.TreeDataProvider<Te
   }
 }
 
-export class TestExecutionTreeView {
-  private treeView: vscode.TreeView<TestExecutionButton>;
-  private treeDataProvider: TestExecutionTreeDataProvider;
+export class ExecutionTreeView {
+  private treeView: vscode.TreeView<ExecutionButton>;
+  private treeDataProvider: ExecutionTreeDataProvider;
 
   constructor(private context: vscode.ExtensionContext) {
-    this.treeDataProvider = new TestExecutionTreeDataProvider();
+    this.treeDataProvider = new ExecutionTreeDataProvider();
     this.treeView = vscode.window.createTreeView('tddTestExecution', {
       treeDataProvider: this.treeDataProvider
     });
