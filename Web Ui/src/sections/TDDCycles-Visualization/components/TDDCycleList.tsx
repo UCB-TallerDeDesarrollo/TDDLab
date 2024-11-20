87ac988a13ae82794bd7f4b9f9ef954c4ef0391a
Jhoselin Quiroga <jhoselin.quiroga@ucb.edu.bo>
2024-11-20 08:23:03 -0400

    Fecha alineada al card de commits

--- Web Ui/src/sections/TDDCycles-Visualization/components/TDDCycleList.tsx
+++ Web Ui/src/sections/TDDCycles-Visualization/components/TDDCycleList.tsx
@@ -2,8 +2,6 @@ import { useState } from 'react';
 import TDDCycleCard from "./TDDCycleCard";
 import { JobDataObject } from "../../../modules/TDDCycles-Visualization/domain/jobInterfaces";
 import { CommitDataObject } from "../../../modules/TDDCycles-Visualization/domain/githubCommitInterfaces";
-import { Card, CardContent, Typography, Box } from '@mui/material';
-import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
 
 interface CycleReportViewProps {
   commitsInfo: CommitDataObject[] | null;
@@ -35,66 +33,35 @@ function TDDCycleList({
     return sortOrder === 'asc' ? dateA - dateB : dateB - dateA;
   });
 
-
-  const sortedDates = sortedCombinedList.map(([commit]) => {
-    const date = new Date(commit.commit.date);
-    return date.toLocaleString();
-  });
-
   return (
-<div style={{ display: "flex", gap: "5px", alignItems: "flex-start", marginBottom: "40px" }}>
-  <div style={{ flex: "1", maxWidth: "300px" }}>
-    <Box
-      onClick={toggleSortOrder}
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: 0.5,
-        cursor: 'pointer',
-        mb: 1,
-      }}
-    >
-      <Typography variant="body1" fontWeight="bold">
-        Fecha
-      </Typography>
-      {sortOrder === 'asc' ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
-    </Box>
-
-    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-      {sortedDates.map((date, index) => (
-        <Card
-          key={index}
-          sx={{
-            height: '229px', 
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            boxShadow: '0 5px 6px rgba(0, 0, 0, 0.1)',
-            mb: 0.5, 
+    <div style={{ display: "flex", gap: "5px", alignItems: "flex-start", marginBottom: "40px" }}>
+      <div style={{ flex: "1", maxWidth: "300px" }}>
+        <button
+          onClick={toggleSortOrder}
+          style={{
+            cursor: "pointer",
+            fontWeight: "bold",
+            marginTop: "-18px",
+            textAlign: "left",
+            display: "flex",
+            alignItems: "center",
+            gap: "5px",
           }}
         >
-          <CardContent>
-            <Typography 
-              variant="body2" 
-              color="textSecondary"
-              sx={{ textAlign: 'center' }}
-            >
-              {date}
-            </Typography>
-          </CardContent>
-        </Card>
-      ))}
-    </Box>
-  </div>
-
-  <div style={{ flex: "1" }}>
-    {sortedCombinedList.map(([commit, job]) => (
-      <TDDCycleCard key={commit.sha} commit={commit} jobs={job} />
-    ))}
-  </div>
-</div>
+          Fecha <span> {sortOrder === 'asc' ? '↑' : '↓'} </span>
+        </button>
+      </div>
+  
+      <div style={{ flex: "1" }}>
+ 
+        {sortedCombinedList.map(([commit, job]) => (
+          <TDDCycleCard key={commit.sha} commit={commit} jobs={job} />
+        ))}
+      </div>
+    </div>
   );
 }
 
 export default TDDCycleList;
+
 
