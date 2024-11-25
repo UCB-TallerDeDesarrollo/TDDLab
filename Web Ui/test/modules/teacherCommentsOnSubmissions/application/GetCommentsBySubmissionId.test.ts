fdc3eb42ecc0ca13b83e779a78b3a1e4d3038771
DILAN ALVARO ALAVI GUTIERREZ <dilanalavi11@gmail.com>
2024-11-25 18:36:13 -0400

    Configuración inicial de pruebas GetCommentsBySubmissionId

---
+++ Web Ui/test/modules/teacherCommentsOnSubmissions/application/GetCommentsBySubmissionId.test.ts
@@ -0,0 +1,7 @@
+// test/modules/teacherCommentsOnSubmissions/application/GetCommentsBySubmissionId.test.ts
+import { GetCommentsBySubmissionId } from "../../../../src/modules/teacherCommentsOnSubmissions/application/GetComments";
+import { MockTeacherCommentsRepository } from "../../__mocks__/submissions/mockTeacherCommentsRepository";
+import { commentDataMock } from "../../__mocks__/submissions/commentDataMock";
+
+describe('GetCommentsBySubmissionId', () => {
+});
