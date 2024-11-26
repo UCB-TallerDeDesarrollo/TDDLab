781705512203851a9f82e41a6bb4c12d2218e6a5
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2024-11-26 15:42:55 -0400

    Correcciones

--- server/src/modules/TDDCycles/Repositories/DBCommitsRepository.ts
+++ server/src/modules/TDDCycles/Repositories/DBCommitsRepository.ts
@@ -35,7 +35,7 @@ export class DBCommitsRepository implements IDBCommitsRepository {
       ];
       await client.query(query, values);
     } catch (error) {
-      console.error(`Error saving commit ${commit.sha} for ${owner}/${repoName}`);
+      console.error(`Error saving commit`);
       throw error;
     } finally {
       client.release();
@@ -56,7 +56,7 @@ export class DBCommitsRepository implements IDBCommitsRepository {
       const values = [coverage, test_count, owner, repoName, sha];
       await client.query(query, values);
     } catch (error) {
-      console.error(`Error updating coverage and test count for commit ${sha} in ${owner} / ${repoName}`);
+      console.error(`Error updating coverage and test count for commit`);
       throw error;
     } finally {
       client.release();
@@ -76,7 +76,7 @@ export class DBCommitsRepository implements IDBCommitsRepository {
       const values = [tdd_cicle, owner, repoName, sha];
       await client.query(query, values);
     } catch (error) {
-      console.error(`Error updating TDD cycle for commit ${sha} in ${owner}/${repoName}`);
+      console.error(`Error updating TDD cycle for commit`);
       throw error;
     } finally {
       client.release();
@@ -92,7 +92,7 @@ export class DBCommitsRepository implements IDBCommitsRepository {
       const result = await client.query(query, values);
       return result.rows;
     } catch (error) {
-      console.error(`Error retrieving commits for ${owner}/${repoName}`);
+      console.error(`Error retrieving commits`);
       throw error;
     } finally {
       client.release();
@@ -107,7 +107,7 @@ export class DBCommitsRepository implements IDBCommitsRepository {
       const result = await client.query(query, values);
       return result.rows.length > 0;
     } catch (error) {
-      console.error(`Error checking existence of commit ${sha} in ${owner}/${repoName}`);
+      console.error(`Error checking existence of commit`);
       throw error;
     } finally {
       client.release();
@@ -122,7 +122,7 @@ export class DBCommitsRepository implements IDBCommitsRepository {
       const result = await client.query(query, values);
       return result.rows[0].count > 0;
     } catch (error) {
-      console.error(`Error checking existence of repository ${owner}/${repoName}`);
+      console.error(`Error checking existence of repository`);
       throw error;
     } finally {
       client.release();
@@ -154,7 +154,7 @@ export class DBCommitsRepository implements IDBCommitsRepository {
         newCommits.map((commit) => this.saveCommit(owner, repoName, commit))
       );
     } catch (error) {
-      console.error(`Error saving commits list for ${owner}/${repoName}`);
+      console.error(`Error saving commits list`);
       throw error;
     }
   }
