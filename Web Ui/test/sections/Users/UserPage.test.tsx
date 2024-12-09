1844d93964b0ab4aa3df6041b07dca5203808937
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2024-12-09 12:25:44 -0400

    Correccion de CodeSmells y correccion de tests de UserPage.test.ts

--- Web Ui/test/sections/Users/UserPage.test.tsx
+++ Web Ui/test/sections/Users/UserPage.test.tsx
@@ -1,96 +1,36 @@
-import { render, screen, waitFor, fireEvent } from "@testing-library/react";
-import UserPage from "../../../src/sections/User/UserPage";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import InvitationPage from "../../../src/sections/GroupInvitation/InvitationPage";
 import "@testing-library/jest-dom";
-import GetUsers from "../../../src/modules/Users/application/getUsers";
-import GetGroups from "../../../src/modules/Groups/application/GetGroups";
-import { UserDataObject } from "../../../src/modules/Users/domain/UsersInterface";
-import { GroupDataObject } from "../../../src/modules/Groups/domain/GroupInterface";
 
-jest.mock("../../../src/modules/Users/application/getUsers");
-jest.mock("../../../src/modules/Groups/application/GetGroups");
-
-const mockUsers: UserDataObject[] = [
-  { id: 1, email: "user1@example.com", role: "admin", groupid: 1 },
-  { id: 2, email: "user2@example.com", role: "user", groupid: 2 },
-];
-
-const mockGroups: GroupDataObject[] = [
-  {
-    id: 1,
-    groupName: "Group1",
-    groupDetail: "Detail1",
-    creationDate: new Date(),
-  },
-  {
-    id: 2,
-    groupName: "Group2",
-    groupDetail: "Detail2",
-    creationDate: new Date(),
-  },
-];
-
-describe("UserPage component", () => {
-  beforeEach(() => {
-    (GetUsers.prototype.getUsers as jest.Mock).mockResolvedValue(mockUsers);
-    (GetGroups.prototype.getGroups as jest.Mock).mockResolvedValue(mockGroups);
-  });
-
+describe("InvitationPage component", () => {
   afterEach(() => {
-    jest.resetAllMocks();
+    jest.clearAllMocks();
   });
 
-  it("renders users table after successful data fetch", async () => {
-    render(<UserPage />);
-    expect(screen.getByRole("progressbar")).toBeInTheDocument();
+  it("Renders the Sign Up button and press it", async () => {
+    render(<InvitationPage />);
 
-    await waitFor(() => {
-      expect(screen.getByText("user1@example.com")).toBeInTheDocument();
-      expect(screen.getByText("Group1")).toBeInTheDocument();
-      expect(screen.getByText("admin")).toBeInTheDocument();
-      expect(screen.getByText("user2@example.com")).toBeInTheDocument();
-      expect(screen.getByText("Group2")).toBeInTheDocument();
-      expect(screen.getByText("user")).toBeInTheDocument();
-    });
-  });
+    const signUpButton = screen.getByText("Registrarse");
+    expect(signUpButton).toBeInTheDocument();
 
-  it("filters users when selecting a group", async () => {
-    render(<UserPage />);
-    await waitFor(() => {
-      expect(screen.getByText("user1@example.com")).toBeInTheDocument();
-      expect(screen.getByText("user2@example.com")).toBeInTheDocument();
-    });
-
-    const selectElement = screen.getByLabelText(/Grupo/i);
-    fireEvent.mouseDown(selectElement);
-
-    const menuItem = await screen.findAllByRole("option");
-    fireEvent.click(menuItem[1]);
-
-    await waitFor(() => {
-      expect(screen.getByText("user1@example.com")).toBeInTheDocument();
-      expect(screen.queryByText("user2@example.com")).not.toBeInTheDocument();
-    });
-
-    fireEvent.mouseDown(selectElement);
-    const allGroupsItem = await screen.findByText("Todos los grupos");
-    fireEvent.click(allGroupsItem);
-    await waitFor(() => {
-      expect(screen.getByText("user1@example.com")).toBeInTheDocument();
-      expect(screen.getByText("user2@example.com")).toBeInTheDocument();
-    });
-    
-  });
-
-  it("displays error message if fetching users fails", async () => {
-    (GetUsers.prototype.getUsers as jest.Mock).mockRejectedValueOnce(
-      new Error("Failed to fetch users")
-    );
-
-    render(<UserPage />);
+    fireEvent.click(signUpButton);
 
     await waitFor(() => {
       expect(
-        screen.getByText(/Error: Failed to fetch users/i)
+        screen.getByText(/Te has registrado exitosamente/i)
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("Displays error message when registration fails", async () => {
+    render(<InvitationPage />);
+
+    const signUpButton = screen.getByText("Registrarse");
+    fireEvent.click(signUpButton);
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(/Error al registrar/i)
       ).toBeInTheDocument();
     });
   });
