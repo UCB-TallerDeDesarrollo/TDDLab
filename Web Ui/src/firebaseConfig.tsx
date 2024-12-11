157c8d9efd18c365d5d7636be0b8c5d724a91892
Belen <beckiar23@gmail.com>
2024-12-10 22:26:57 -0400

    Se modifico la configuracion de firebase config

--- Web Ui/src/firebaseConfig.tsx
+++ Web Ui/src/firebaseConfig.tsx
@@ -1,11 +1,19 @@
 import { initializeApp } from "firebase/app";
+import {
+  VITE_FIREBASE_API_KEY, 
+  VITE_FIREBASE_AUTH_DOMAIN, 
+  VITE_FIREBASE_PROJECT_ID, 
+  VITE_FIREBASE_STORAGE_BUCKET, 
+  VITE_FIREBASE_MESSAGING_SENDER_ID,
+  VITE_FIREBASE_APP_ID } from '../config.ts'
+  
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:  import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: VITE_FIREBASE_API_KEY,
+  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: VITE_FIREBASE_PROJECT_ID,
+  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: VITE_FIREBASE_APP_ID,
 };
 
 const firebase: any = initializeApp(firebaseConfig);
