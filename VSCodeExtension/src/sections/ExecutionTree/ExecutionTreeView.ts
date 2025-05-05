432540f50caf4c14893f2e4328f4c55870e7d260
JonathanELPRO <jonathan.moruno@ucb.du.bo>
2025-05-05 13:18:47 -0400

    se implementaron los cambios necesarios para feature toggle con un archivo json

--- VSCodeExtension/src/sections/ExecutionTree/ExecutionTreeView.ts
+++ VSCodeExtension/src/sections/ExecutionTree/ExecutionTreeView.ts
@@ -1,47 +1,79 @@
 import * as vscode from 'vscode';
 import { ExecutionButton } from '../../modules/Button/model/ExecutionButton';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export class ExecutionTreeDataProvider implements vscode.TreeDataProvider<ExecutionButton> {
+  private features: { [key: string]: boolean } = {};
+
+  constructor(private readonly context: vscode.ExtensionContext) {
+    this.loadFeatureConfig();
+  }
+
+  private loadFeatureConfig(): void {
+    try {
+      const configPath = path.join(this.context.extensionPath, 'resources', 'features.json');
+      const rawData = fs.readFileSync(configPath, 'utf8');
+      this.features = JSON.parse(rawData);
+    } catch (error) {
+      console.error('Error al cargar features.json:', error);
+    }
+  }
+
   getTreeItem(element: ExecutionButton): vscode.TreeItem {
     return element;
   }
 
   getChildren(element?: ExecutionButton): Thenable<ExecutionButton[]> {
-    return Promise.resolve([
-      new ExecutionButton(
+    const buttons: ExecutionButton[] = [];
+
+    if (this.features.runTest) {
+      buttons.push(new ExecutionButton(
         'Run Test',
         vscode.TreeItemCollapsibleState.None,
         { command: 'TDD.runTest', title: 'Run Test' },
         '',
         new vscode.ThemeIcon('play', new vscode.ThemeColor('charts.green')),
         'Execute TDD Tests'
-      ),
-      new ExecutionButton(
+      ));
+    }
+
+    if (this.features.crearProyecto) {
+      buttons.push(new ExecutionButton(
         'Crear Proyecto',
         vscode.TreeItemCollapsibleState.None,
-        { command: 'TDD.cloneCommand', title: 'Crear Proyecto'},
+        { command: 'TDD.cloneCommand', title: 'Crear Proyecto' },
         '',
         new vscode.ThemeIcon('window', new vscode.ThemeColor('charts.white')),
         ''
-      ),
-      new ExecutionButton(
+      ));
+    }
+
+    if (this.features.asistenteIA) {
+      buttons.push(new ExecutionButton(
         'Asistente de IA',
         vscode.TreeItemCollapsibleState.None,
-        { command: 'TDD.AsistenteCommand', title: 'Asistente de IA'},
+        { command: 'TDD.AsistenteCommand', title: 'Asistente de IA' },
         '',
         new vscode.ThemeIcon('symbol-method', new vscode.ThemeColor('charts.white')),
         ''
-      ),
-      new ExecutionButton(
+      ));
+    }
+
+    if (this.features.exportarSesion) {
+      buttons.push(new ExecutionButton(
         'Exportar sesion TDDLab',
         vscode.TreeItemCollapsibleState.None,
-        { command: 'TDD.exportCommand', title: 'Exportar sesion TDDLab'},
+        { command: 'TDD.exportCommand', title: 'Exportar sesion TDDLab' },
         '',
         new vscode.ThemeIcon('arrow-down', new vscode.ThemeColor('charts.white')),
         ''
-      )
-    ]);
+      ));
+    }
+
+    return Promise.resolve(buttons);
   }
+  
 }
 
 export class ExecutionTreeView {
@@ -49,7 +81,7 @@ export class ExecutionTreeView {
   private readonly treeDataProvider: ExecutionTreeDataProvider;
 
   constructor(private readonly context: vscode.ExtensionContext) {
-    this.treeDataProvider = new ExecutionTreeDataProvider();
+    this.treeDataProvider = new ExecutionTreeDataProvider(context);
     this.treeView = vscode.window.createTreeView('tddTestExecution', {
       treeDataProvider: this.treeDataProvider
     });
