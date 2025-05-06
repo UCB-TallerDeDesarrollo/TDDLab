97275d8c9cc0ab03e8c272a43fe6951b4e4403bd
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2025-05-06 06:39:42 -0700

    Se actualizo el controlador del asistente de IA para analizar tdd desde la extension

--- server/src/controllers/AIAssistant/AIAssistantController.ts
+++ server/src/controllers/AIAssistant/AIAssistantController.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from 'express';
 import { AnalyzeOrRefactorCodeUseCase } from '../../modules/AIAssistant/application/AIAssistantUseCases/analyzeOrRefactorCodeUseCase';
 import { AIAssistantRepository } from '../../modules/AIAssistant/repository/AIAssistantRepositoy';
+import { AnalyzeTDDUseCase } from '../../modules/AIAssistant/application/AIAssistantUseCases/analyzeTDDUseCase';
 
 export default class AIAssistantController {
 
     private readonly analyzeOrRefactorUseCase: AnalyzeOrRefactorCodeUseCase;
+    private readonly analyzeTDDUseCase: AnalyzeTDDUseCase;
 
     constructor(repository: AIAssistantRepository) {
         this.analyzeOrRefactorUseCase = new AnalyzeOrRefactorCodeUseCase(repository);
+        this.analyzeTDDUseCase = new AnalyzeTDDUseCase(repository);
     }
 
     async analyzeOrRefactor(req: Request, res: Response): Promise<void> {
@@ -23,6 +26,23 @@ export default class AIAssistantController {
             res.json(result);
         } catch (err) {
             res.status(500).json({ error: 'Error procesando el prompt' });
+        }
+    }
+
+    async analyzeTDDFromExtension(req: Request, res: Response): Promise<void> {
+        const { tddlog, gitInfo } = req.body;
+
+        if (!tddlog || !gitInfo) {
+            res.status(400).json({ error: 'tddlog and gitInfo are required in the JSON body.' });
+            return;
+        }
+
+        try {
+            const result = await this.analyzeTDDUseCase.execute(tddlog, gitInfo);
+            res.json({ generatedText: result });
+        } catch (error) {
+            console.error("Error generating text:", error);
+            res.status(500).json({ error: "Failed to generate text." });
         }
     }
 }
