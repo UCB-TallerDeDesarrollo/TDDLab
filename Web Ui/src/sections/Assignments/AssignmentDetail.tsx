874350b7d7dfa454e00a390219b3e8c3000b9efe
Guille <guichi.maldo@gmail.com>
2025-05-07 14:26:13 -0400

    se modifico la funcion handleRedirectStudent

--- Web Ui/src/sections/Assignments/AssignmentDetail.tsx
+++ Web Ui/src/sections/Assignments/AssignmentDetail.tsx
@@ -642,7 +642,7 @@ const AssignmentDetail: React.FC<AssignmentDetailProps> = ({
                 onClick={() => {
                   localStorage.setItem("selectedMetric", "Dashboard");
                   if (studentSubmission?.repository_link) {
-                    handleRedirectStudent(studentSubmission.repository_link, navigate);
+                    handleRedirectStudent(studentSubmission.repository_link,studentSubmission.id,navigate)
                   }
                 }}
                 color="primary"
