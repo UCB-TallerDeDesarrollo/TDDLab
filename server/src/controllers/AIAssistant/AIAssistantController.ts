002b01e4b17e6ef872da8bf227743e53f1b78291
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2025-05-07 18:09:49 -0700

    Merge con la rama de Patrick

--- server/src/controllers/AIAssistant/AIAssistantController.ts
+++ server/src/controllers/AIAssistant/AIAssistantController.ts
@@ -1,23 +1,23 @@
 import { Request, Response } from 'express';
 import { AnalyzeOrRefactorCodeUseCase } from '../../modules/AIAssistant/application/AIAssistantUseCases/analyzeOrRefactorCodeUseCase';
 import { AIAssistantRepository } from '../../modules/AIAssistant/repository/AIAssistantRepositoy';
-import { AnalyzeTDDUseCase } from '../../modules/AIAssistant/application/AIAssistantUseCases/analyzeTDDUseCase';
 import { GetPromptsCodeUseCase } from '../../modules/AIAssistant/application/AIAssistantUseCases/getPromptsCodeUseCases';
 import { UpdatePromptsCodeUseCase } from '../../modules/AIAssistant/application/AIAssistantUseCases/updatePromptsCodeUseCase';
 import { AIAssistantDataBaseRepository } from '../../modules/AIAssistant/repository/AiAssistantDataBaseRepository';
+import { AnalyzeTDDCodeUseCase } from '../../modules/AIAssistant/application/AIAssistantUseCases/analyzeTDDCodeUseCase';
 
 export default class AIAssistantController {
 
     private readonly analyzeOrRefactorUseCase: AnalyzeOrRefactorCodeUseCase;
-    private readonly analyzeTDDUseCase: AnalyzeTDDUseCase;
     private readonly getPromptsUseCase: GetPromptsCodeUseCase;
     private readonly updatePromptsUseCase: UpdatePromptsCodeUseCase;
+    private readonly analyzeTDDUseCase: AnalyzeTDDCodeUseCase;
 
     constructor(repository: AIAssistantRepository, repositoryDB: AIAssistantDataBaseRepository) {
         this.analyzeOrRefactorUseCase = new AnalyzeOrRefactorCodeUseCase(repository);
-        this.analyzeTDDUseCase = new AnalyzeTDDUseCase(repository);
         this.getPromptsUseCase = new GetPromptsCodeUseCase(repositoryDB);
         this.updatePromptsUseCase = new UpdatePromptsCodeUseCase(repositoryDB);
+        this.analyzeTDDUseCase = new AnalyzeTDDCodeUseCase(repository);
     }
 
     async analyzeOrRefactor(req: Request, res: Response): Promise<void> {
@@ -34,6 +34,34 @@ export default class AIAssistantController {
         } catch (err) {
             console.error('[CONTROLLER ERROR] analyzeOrRefactor:', err);
             res.status(500).json({ error: 'Error procesando el prompt' });
+        }
+    }
+
+    async getPrompts(_req: Request, res: Response): Promise<void> {
+        try {
+            const prompts = await this.getPromptsUseCase.execute();
+            res.status(200).json(prompts);
+        } catch (error) {
+            res.status(500).json({ error: "Server error" });
+        }
+    }
+
+    async updatePrompts(_req: Request, res: Response): Promise<void> {
+        const {
+            analysis_tdd,
+            refactoring
+        } = _req.body;
+
+        try {
+            const updatePrompts = await this.updatePromptsUseCase.execute(
+                {
+                    analysis_tdd,
+                    refactoring
+                }
+            );
+            res.status(200).json(updatePrompts);
+        } catch (error) {
+            res.status(500).json({ error: "Server error" });
         }
     }
 
@@ -66,31 +94,5 @@ export default class AIAssistantController {
                 error: "Error al analizar el código",
                 details: errorMessage 
             });
-    async getPrompts(_req: Request, res: Response): Promise<void> {
-        try {
-            const prompts = await this.getPromptsUseCase.execute();
-            res.status(200).json(prompts);
-        } catch (error) {
-            res.status(500).json({ error: "Server error" });
-        }
-    }
-
-    async updatePrompts(_req: Request, res: Response): Promise<void> {
-        const {
-            analysis_tdd,
-            refactoring
-        } = _req.body;
-
-        try {
-            const updatePrompts = await this.updatePromptsUseCase.execute(
-                {
-                    analysis_tdd,
-                    refactoring
-                }
-            );
-            res.status(200).json(updatePrompts);
-        } catch (error) {
-            res.status(500).json({ error: "Server error" });
-        }
-    }
+        }}
 }
