ca1cfdc32629711a71421ed8b79c2c38a08c73d1
Your Name <mateo.poquechoque@ucb.edu.bo>
2025-05-27 11:45:15 -0400

    test para ver si retorna el flag correcto cuando se hace una peticion

--- Web Ui/test/modules/FeatureToggles/repository/FeatureFlagRepository.test.ts
+++ Web Ui/test/modules/FeatureToggles/repository/FeatureFlagRepository.test.ts
@@ -30,6 +30,16 @@ describe("FeatureFlagRepository", () => {
       expect(mockedAxios.get).toHaveBeenCalledWith(API_URL);
     });
   });
-  
+   describe("getFlagByName", () => {
+    it("debería retornar el feature flag con el nombre especificado", async () => {
+      mockedAxios.get.mockResolvedValue({ status: 200, data: sampleFlag });
+
+      const result = await repo.getFlagByName("Boton Asistente IA");
+
+      expect(result).toEqual(sampleFlag);
+      expect(mockedAxios.get).toHaveBeenCalledWith(`${API_URL}/name/Boton Asistente IA`);
+    });
+  });
+
   
 });
