98202ce308ec808d775846ae1012ab84e75687bb
PatrickGQ <patrick.gonzales@ucb.edu.bo>
2025-05-28 14:21:05 -0400

    Se cambio a una version anterior

--- Web Ui/src/modules/AIAssistant/repository/ChatBotRepository.ts
+++ Web Ui/src/modules/AIAssistant/repository/ChatBotRepository.ts
@@ -6,12 +6,11 @@ const API_URL = VITE_API + "/AIAssistant/chatbot";
 export class ChatbotRepository {
   async sendMessage(message: string): Promise<string> {
     try {
-      const { data } = await axios.post(API_URL, { input: message });
-      return data.result;
+      const response = await axios.post(API_URL, { input: message });
+      return response.data.response?.result ?? "No hubo respuesta del asistente.";
     } catch (error) {
       console.error("Error en la API del chatbot:", error);
       throw new Error("Error en la conexión con el servidor");
     }
   }
 }
-
