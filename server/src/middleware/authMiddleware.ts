85fd2f855812ba1729ee980e9df1e29abfe68086
SebastianB-RDL <sebastian@rndlabs.ai>
2025-09-22 23:25:13 -0400

    feat: Verificar que devuelve 200 cuando la autenticacion es exitosa

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -1,13 +1,13 @@
-import { Request} from "express";
+import { Request, Response, NextFunction } from "express";
 import { AuthenticateUser } from "../modules/Users/Application/authenticateUser";
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecreto";
 const authUser = new AuthenticateUser(JWT_SECRET);
 
-export function authenticateJWT(
-  req: Request
-) {
+export function authenticateJWT(req: Request, res: Response, next: NextFunction) {
   const token = req.cookies?.userSession;
   const decoded = authUser.verifyToken(token);
   (req as any).user = decoded;
+  next();
+  res.status(200);
 }
--- server/test/middleware/authMiddleware.test.ts
+++ server/test/middleware/authMiddleware.test.ts
@@ -5,26 +5,48 @@ jest.mock("../../src/modules/Users/Application/authenticateUser");
 
 describe("authenticateJWT middleware", () => {
   let mockReq: any;
-
+  let mockRes: any;
+  let mockNext: jest.Mock;
 
   beforeEach(() => {
     mockReq = { cookies: {} };
+    mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    mockNext = jest.fn();
     jest.clearAllMocks();
   });
 
   it("Verificar el token con token_valido", () => {
     mockReq.cookies.userSession = "token_valido";
     const mockDecoded = { userId: 123 };
-    (AuthenticateUser.prototype.verifyToken as jest.Mock).mockReturnValue(mockDecoded);
-    authenticateJWT(mockReq);
-    expect(AuthenticateUser.prototype.verifyToken).toHaveBeenCalledWith("token_valido");
+    (AuthenticateUser.prototype.verifyToken as jest.Mock).mockReturnValue(
+      mockDecoded
+    );
+    authenticateJWT(mockReq,mockRes,mockNext);
+    expect(AuthenticateUser.prototype.verifyToken).toHaveBeenCalledWith(
+      "token_valido"
+    );
   });
 
-    it("Verificar que el token es decifrado correctamente", () => {
+  it("Verificar que el token es decifrado correctamente", () => {
     mockReq.cookies.userSession = "token_valido";
     const mockDecoded = { userId: 123 };
-    (AuthenticateUser.prototype.verifyToken as jest.Mock).mockReturnValue(mockDecoded);
-    authenticateJWT(mockReq);
+    (AuthenticateUser.prototype.verifyToken as jest.Mock).mockReturnValue(
+      mockDecoded
+    );
+    authenticateJWT(mockReq,mockRes,mockNext);
     expect((mockReq as any).user).toEqual(mockDecoded);
+  });
+
+  it("Verificar que devuelve 200 cuando la autenticacion es exitosa", () => {
+    mockReq.cookies.userSession = "token_valido";
+    const mockDecoded = { userId: 123 };
+    (AuthenticateUser.prototype.verifyToken as jest.Mock).mockReturnValue(
+      mockDecoded
+    );
+    authenticateJWT(mockReq,mockRes,mockNext);
+    expect(mockRes.status).toHaveBeenCalledWith(200);
   });
 });
