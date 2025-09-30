f65c1bb1b20d24ddd15b72378b2560e9a44f26b9
SebastianB-RDL <sebastian@rndlabs.ai>
2025-09-30 14:55:30 -0400

    refactor: se agrega la url de frontend en las variables de entorno del backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,7 @@ const port = 3000;
 
 // Enable CORS for all routes
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.VITE_FRONT_URL , 
   credentials: true,
 }));
 app.use(express.json()); 
