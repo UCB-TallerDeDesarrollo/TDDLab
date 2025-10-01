260448497d5aafa6311aecffd0cfaa6dec93bf6e
Wenba1 <balderramawendym@gmail.com>
2025-10-01 14:32:55 -0400

    Agregar prueba en test de authenticateUser

--- server/test/modules/users/application/authenticateUser.test.ts
+++ server/test/modules/users/application/authenticateUser.test.ts
@@ -14,7 +14,9 @@ describe("AuthenticateUser", () => {
   it("Devolver el payload si el token es válido", () => {
     const mockPayload = { userId: 123 };
     (jwt.verify as jest.Mock).mockReturnValue(mockPayload);
+
     const result = authUser.verifyToken("token_valido");
+
     expect(jwt.verify).toHaveBeenCalledWith("token_valido", secret);
     expect(result).toEqual(mockPayload);
   });
@@ -23,7 +25,17 @@ describe("AuthenticateUser", () => {
     (jwt.verify as jest.Mock).mockImplementation(() => {
       throw new Error("invalid token");
     });
+
     expect(() => authUser.verifyToken("token_invalido")).toThrow("Token inválido");
     expect(jwt.verify).toHaveBeenCalledWith("token_invalido", secret);
+  });
+
+  it("Debe devolver null si jwt.verify no retorna nada", () => {
+    (jwt.verify as jest.Mock).mockReturnValue(null);
+
+    const result = authUser.verifyToken("token_vacio");
+
+    expect(jwt.verify).toHaveBeenCalledWith("token_vacio", secret);
+    expect(result).toBeNull();
   });
 });
