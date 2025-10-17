a4ea11007ddee5cf797393e2519e9e4288a3b06a
SebastianB-RDL <sebastian@rndlabs.ai>
2025-10-17 14:23:06 -0400

    Se quita el envio de crenciales en todos los endpoints desde frontend

--- Web Ui/src/main.tsx
+++ Web Ui/src/main.tsx
@@ -1,9 +1,7 @@
-import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-axios.defaults.withCredentials = true;
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
