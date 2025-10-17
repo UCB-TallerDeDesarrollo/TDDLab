f39c2aef8d209c5155f8881783c25f9de3af71fe
SebastianB-RDL <sebastian@rndlabs.ai>
2025-10-17 09:46:37 -0400

    Se agrega para que el front envie los credenciales y compartan el mismo sitio con el backend

--- server/src/modules/Users/Application/saveUserCookie.ts
+++ server/src/modules/Users/Application/saveUserCookie.ts
@@ -2,9 +2,10 @@ import { Response } from "express";
 export const saveUserCookie =  async (
   token:string, res: Response
 ) => { 
-        res.cookie("userSession", token, {
-      httpOnly: true,
-      sameSite: "strict",
-      maxAge: 1000 * 60 * 60 * 24 * 30,
-    });
+  res.cookie("userSession", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 1000 * 60 * 60 * 24 * 30,
+  });
   }
