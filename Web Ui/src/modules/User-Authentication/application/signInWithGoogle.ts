9e3bc822a7700964651e6d0d144e13330eb72d8d
Ale-Saul <alejandro.mont.cesp@gmail.com>
2025-11-25 14:14:47 -0400

    Se elimino bloque try-catch innecesario en signInWithGoogle.ts

--- Web Ui/src/modules/User-Authentication/application/signInWithGoogle.ts
+++ Web Ui/src/modules/User-Authentication/application/signInWithGoogle.ts
@@ -3,12 +3,8 @@ import firebase from "../../../firebaseConfig";
 
 export async function handleSignInWithGoogle() {
   const provider = new GoogleAuthProvider();
-  try {
-    const auth = getAuth(firebase);
-    const result = await signInWithPopup(auth, provider);
-    return result.user;
-  } catch (error) {
-    throw error;
-  }
+  const auth = getAuth(firebase);
+  const result = await signInWithPopup(auth, provider);
+  return result.user;
 }
 
