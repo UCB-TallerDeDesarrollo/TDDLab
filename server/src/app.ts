5f4e92f8d02c8828b3d10408d8fb17301819a42e
Laura Camacho <laura.camacho@ucb.edu.bo>
2026-03-24 21:45:36 -0400

    Fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,7 @@ const port = 3000;
 
 // Enable CORS for all routes
 app.use(cors({
-  origin: "https://tddlab-staging-firebase.web.app", //process.env.VITE_FRONT_URL, 
+  origin: "http://localhost:5173", //process.env.VITE_FRONT_URL, 
   credentials: true,
 }));
 app.use(express.json()); 
