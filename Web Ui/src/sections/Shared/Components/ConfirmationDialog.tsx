ff2b6912eeb1b8eec087555f07452a6f7b80b6f4
Mar14n423 <wara.mercado@ucb.edu.bo>
2026-03-26 10:57:20 -0400

    Se usaron los estilos globales en shared confirmacion

--- Web Ui/src/sections/Shared/Components/ConfirmationDialog.tsx
+++ Web Ui/src/sections/Shared/Components/ConfirmationDialog.tsx
@@ -4,12 +4,7 @@ import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
-import {
-  dialogContentStyle,
-  titleStyle,
-  primaryButtonStyle,
-  secondaryButtonStyle,
-} from "../Styles/DialogBoxStyles";
+import "../../App.css";
 
 interface ConfirmationDialogProps {
   open: boolean;
@@ -31,27 +26,28 @@ export const ConfirmationDialog: React.FC<ConfirmationDialogProps> = ({
   onDelete,
 }) => {
   return (
-    <Dialog open={open}>
-      <DialogTitle style={titleStyle}>{title}</DialogTitle>
-      <DialogContent style={dialogContentStyle}>{content}</DialogContent>
-      <DialogActions>
+    <Dialog open={open} maxWidth="xs" fullWidth>
+      <DialogTitle className="dialog-title-std">
+        {title}
+      </DialogTitle>
+      
+      <DialogContent className="dialog-content-box">
+        <div className="dialog-text-content">
+          {content}
+        </div>
+      </DialogContent>
+
+      <DialogActions className="dialog-footer">
         <Button
           onClick={onCancel}
-          color="primary"
-          style={{ ...dialogContentStyle, ...secondaryButtonStyle, 
-            color: '#d32f2f',
-            borderColor: '#d32f2f', // Color del borde
-            borderWidth: '2px', // Ancho del borde
-            borderStyle: 'solid', // Estilo del borde
-            padding: '5px 20px' // Añade padding para un mayor relieve 
-          }}
+          className="btn-std btn-danger-outline"
         >
           {cancelText}
         </Button>
+
         <Button
           onClick={onDelete}
-          color="primary"
-          style={{ ...dialogContentStyle, ...primaryButtonStyle }}
+          className="btn-std btn-primary"
         >
           {deleteText}
         </Button>
