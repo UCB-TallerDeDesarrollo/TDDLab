29ebe03f5d090b67641b70709e8eee29b4c40e18
Mar14n423 <wara.mercado@ucb.edu.bo>
2026-03-26 10:58:24 -0400

    Se saco todo los estilos del codigo

--- Web Ui/src/sections/Shared/Components/ConfirmationDialog.tsx
+++ Web Ui/src/sections/Shared/Components/ConfirmationDialog.tsx
@@ -4,7 +4,7 @@ import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
-import "../../App.css";
+import "../../../App.css";
 
 interface ConfirmationDialogProps {
   open: boolean;
