43a691eff42f92a1e05503b5b28f35ef4679f351
Libia71421 <baka65311131@gmail.com>
2026-04-25 22:56:13 -0400

    fix backend jest mock

--- server/test/controllers/userController.test.ts
+++ server/test/controllers/userController.test.ts
@@ -13,6 +13,9 @@ jest.mock("../../src/modules/Users/Repositories/UserRepository");
 jest.mock("firebase-admin", () => ({
   initializeApp: jest.fn(),
   auth: jest.fn(),
+  credential: {
+    applicationDefault: jest.fn(),
+  },
 }));
 jest.mock("../../src/modules/Users/Application/getUser", () => ({
   getUser: jest.fn(),
