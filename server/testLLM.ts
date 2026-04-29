eea4d6326600942d5412638f0b37dc51b16752d3
Nathanael Alba <nathanaelalba75@gmail.com>
2026-04-29 15:36:20 -0400

    Revert "Correccion nombre de practicas duplicados"
    This reverts commit 669512c4303160a2dcb62e7dcfceb2cff2287c11.

--- server/testLLM.ts
+++
@@ -1,7 +0,0 @@
-import { AIAssistantRepository } from './src/modules/AIAssistant/repository/AIAssistantRepositoy';
-async function run() {
-  const repo = new AIAssistantRepository();
-  const res = await repo.sendChat("historia", "hola");
-  console.log("RESULT:", res);
-}
-run();
