e21199f8a709951ab5e16a00570755b9dd435abd
christian-ledezma <chris.ledezma.s@gmail.com>
2026-05-06 23:05:38 -0400

    add active link indicator to LateralMenu items

--- Web Ui/src/sections/MainMenu/components/LateralMenu.tsx
+++ Web Ui/src/sections/MainMenu/components/LateralMenu.tsx
@@ -10,6 +10,7 @@ import {
 import { Dispatch, SetStateAction } from "react";
 import LoginIcon from "@mui/icons-material/Login";
 import { NavLink as NavLinkType } from "../../../types/navigation.types";
+import { useLocation } from "react-router-dom";
 
 interface NavLateralMenuProps {
   navArrayLinks: NavLinkType[];
@@ -22,24 +23,41 @@ export default function NavLateralMenu({
   NavLink,
   setOpen,
 }: Readonly<NavLateralMenuProps>) {
+  const location = useLocation();
+
   return (
     <Box sx={{ width: 250 }}>
       <nav>
         <List>
           <Typography sx={{ marginLeft: "14px" }}>TDDLab</Typography>
 
-          {navArrayLinks.map((item) => (
-            <ListItem disablePadding key={item.title}>
-              <ListItemButton
-                component={NavLink}
-                to={item.path}
-                onClick={() => setOpen(false)}
-              >
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText>{item.title}</ListItemText>
-              </ListItemButton>
-            </ListItem>
-          ))}
+          {navArrayLinks.map((item) => {
+            const isActive = location.pathname === item.path;
+            return (
+              <ListItem disablePadding key={item.title}>
+                <ListItemButton
+                  component={NavLink}
+                  to={item.path}
+                  onClick={() => setOpen(false)}
+                  sx={{
+                    borderLeft: isActive ? "4px solid #002345" : "4px solid transparent",
+                    backgroundColor: isActive ? "rgba(0, 35, 69, 0.08)" : "transparent",
+                    "&:hover": {
+                      backgroundColor: "rgba(0, 35, 69, 0.05)",
+                    },
+                  }}
+                >
+                  <ListItemIcon sx={{ color: isActive ? "#002345" : "inherit" }}>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.title}
+                    primaryTypographyProps={{
+                      fontWeight: isActive ? 700 : 400,
+                      color: isActive ? "#002345" : "inherit",
+                    }}></ListItemText>
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+
           <ListItem disablePadding>
             <ListItemButton
               component={NavLink}
