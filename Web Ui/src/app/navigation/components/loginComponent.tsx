aa6cf1580bf4d3ca3b590cc6a58a1355fd31db5f
huascar-riv0126 <huascar.rivero.dev0126@gmail.com>
2026-09-01 11:08:53 -0400

    refact: modificar funciones con el cambio de nombre

--- Web Ui/src/app/navigation/components/loginComponent.tsx
+++ Web Ui/src/app/navigation/components/loginComponent.tsx
@@ -9,8 +9,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { CheckIfUserHasAccount } from "../../../modules/User-Authentication/application/checkIfUserHasAccount";
 import { removeSessionCookie } from "../../../modules/User-Authentication/application/deleteSessionCookie";
-import { handleSignInWithGitHub } from "../../../modules/User-Authentication/application/signInWithGithub";
-import { handleGithubSignOut } from "../../../modules/User-Authentication/application/signOutWithGithub";
+import { handleSignInWithGoogle } from "../../../modules/User-Authentication/application/signInWithGoogle";
+import { handleSignOutFromGoogle } from "../../../modules/User-Authentication/application/signOutFromGoogle";
 import { setCookieAndGlobalStateForValidUser } from "../../../modules/User-Authentication/application/setCookieAndGlobalStateForValidUser";
 import {
   setGlobalState,
@@ -29,7 +29,7 @@ export default function LoginComponent({
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
 
   const handleLogin = async () => {
-    const userData = await handleSignInWithGitHub();
+    const userData = await handleSignInWithGoogle();
     if (userData?.email) {
       const idToken = await userData.getIdToken();
       const loginPort = new CheckIfUserHasAccount();
@@ -40,7 +40,7 @@ export default function LoginComponent({
 
   const handleLogout = async () => {
     setAnchorEl(null);
-    await handleGithubSignOut();
+    await handleSignOutFromGoogle();
     setGlobalState("authData", {
       userid: -1,
       userProfilePic: "",
